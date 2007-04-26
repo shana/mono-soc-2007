25e0642a0438c38953374579a9686e15fccf762f
MarcosMarin <MarcosMarin@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-04-26 15:23:39 +0000

    Forgot this >_<

--- trunk/mdavid/CBinding/Gui/CodeGenerationPanel.cs
+++ trunk/mdavid/CBinding/Gui/CodeGenerationPanel.cs
@@ -10,82 +10,16 @@ namespace CBinding
 	public partial class CodeGenerationPanel : Gtk.Bin
 	{
 		//private CProject project;
-		private CProjectConfiguration configuration;
-		private CCompilationParameters compilationParameters;
+		//private CProjectConfiguration configuration;
+		//private CCompilationParameters compilationParameters;
 		
 		public CodeGenerationPanel (IProperties customizationObject)
 		{
 			this.Build();
-			
-			//project = (CProject)customizationObject.GetProperty ("Project");
-			configuration = (CProjectConfiguration)customizationObject.GetProperty ("Config");
-			compilationParameters = (CCompilationParameters)configuration.CompilationParameters;
-			
-			switch (configuration.CompileTarget)
-			{
-			case CBinding.CompileTarget.Bin:
-				targetCombo.Active = 0;
-				break;
-			case CBinding.CompileTarget.StaticLibrary:
-				targetCombo.Active = 1;
-				break;
-			case CBinding.CompileTarget.SharedLibrary:
-				targetCombo.Active = 2;
-				break;
-			}
-			
-			compilationParameters.GenWarnings = warningAllRadio.Active;
-			
-			if (compilationParameters.Libs != null)
-				foreach (string lib in compilationParameters.Libs)
-					librariesEntry.Buffer.Text += lib + "\n";
-			
-			targetCombo.Changed += OnTargetChanged;
-			addButton.Clicked += OnLibAdded;
-		}
-		
-		private void OnLibAdded (object sender, EventArgs e)
-		{
-			if (libToAddEntry.Buffer != null &&
-			    libToAddEntry.Buffer.Text != string.Empty)
-			{
-				librariesEntry.Buffer.Text += libToAddEntry.Buffer.Text + "\n";
-			}
-		}
-		
-		private void OnTargetChanged (object sender, EventArgs e)
-		{
-			switch (targetCombo.ActiveText)
-			{
-			case "Executable":
-				configuration.CompileTarget = CBinding.CompileTarget.Bin;
-				break;
-			case "Static Library":
-				configuration.CompileTarget = CBinding.CompileTarget.StaticLibrary;
-				break;
-			case "Shared Object":
-				configuration.CompileTarget = CBinding.CompileTarget.SharedLibrary;
-				break;
-			}
 		}
 		
 		public bool Store ()
-		{
-			if (compilationParameters == null)
-				return true;
-			
-			compilationParameters.GenWarnings = warningAllRadio.Active;
-			
-			if (compilationParameters.Libs == null)
-				compilationParameters.Libs = new ArrayList ();
-			
-			StringReader libReader = new StringReader (librariesEntry.Buffer.Text);
-			string lib;
-			while ((lib = libReader.ReadLine()) != null) {
-				compilationParameters.Libs.Add (lib);
-			}
-			libReader.Close ();
-			
+		{			
 			return true;
 		}
 	}
