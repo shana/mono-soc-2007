86a00c47a75012d06c3a569c64ebfc7add762d30
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-13 22:02:13 +0000

    Initial import.
    I've written a simple rule for check the Attribute suffix in Attribute
    subclasses.  I'm learning some Cecil and Gendarme stuff.  When I have a bit more
    experience I'll write more complex rules.

---
+++ trunk/nestor/rules/Gendarme.Rules.Naming/Test/AttributeEndsWithAttributeSuffixTest.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Reflection;
+
+using Gendarme.Framework;
+using Gendarme.Rules.Naming;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace Test.Rules.Naming {
+
+    public class CorrectAttribute : Attribute {
+    }
+    
+    public class Incorrect : Attribute {
+    }
+
+    public class OtherAttribute : CorrectAttribute {
+    }
+    
+    public class Other : CorrectAttribute {
+    }
+    
+    [TestFixture]
+    public class AttributeEndsWithAttributeSuffixTest {
+		private ITypeRule rule;
+		private AssemblyDefinition assembly;
+		private TypeDefinition type;
+       
+        [TestFixtureSetUp]
+        public void FixtureSetUp ()
+        {
+            string unit = Assembly.GetExecutingAssembly ().Location;
+            assembly = AssemblyFactory.GetAssembly (unit);
+			rule = new AttributeEndsWithAttributeSuffixRule ();
+        }
+        
+        [Test]
+        public void TestOneLevelInheritanceIncorrectName () 
+        {
+            type = assembly.MainModule.Types ["Test.Rules.Naming.Incorrect"];
+            Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
+            Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
+        }
+        
+        [Test]
+        public void TestOneLevelInheritanceCorrectName () 
+        {
+            type = assembly.MainModule.Types ["Test.Rules.Naming.CorrectAttribute"];
+            Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
+        }
+        
+        [Test]
+        public void TestVariousLevelInheritanceCorrectName () {
+            type = assembly.MainModule.Types ["Test.Rules.Naming.OtherAttribute"];
+            Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
+        }
+        
+        [Test]
+        public void TestVariousLevelInheritanceIncorrectName () 
+        {
+            type = assembly.MainModule.Types ["Test.Rules.Naming.Other"];
+            Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
+            Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
+        }
+    }
+}
