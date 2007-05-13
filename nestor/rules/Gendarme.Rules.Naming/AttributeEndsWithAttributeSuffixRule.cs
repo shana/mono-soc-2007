5d01d9afc62fb4aac962de087dbc985e5de61c8e
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-13 22:02:13 +0000

    Initial import.
    I've written a simple rule for check the Attribute suffix in Attribute
    subclasses.  I'm learning some Cecil and Gendarme stuff.  When I have a bit more
    experience I'll write more complex rules.

---
+++ nestor/rules/Gendarme.Rules.Naming/AttributeEndsWithAttributeSuffixRule.cs
@@ -0,0 +1,34 @@
+using System;
+
+using Mono.Cecil;
+using Gendarme.Framework;
+
+namespace Gendarme.Rules.Naming {
+    public class AttributeEndsWithAttributeSuffixRule : ITypeRule {
+        private bool InheritsFromAttribute (TypeDefinition typeDefinition)
+        {
+            TypeDefinition current =  typeDefinition;
+            while (current.FullName != "System.Object") {
+                if (current.BaseType.FullName == "System.Attribute")
+                    return true;
+                current = (TypeDefinition)current.BaseType;
+            }
+            return false;
+        }
+        
+        public MessageCollection CheckType (TypeDefinition typeDefinition, Runner runner)
+        {
+            MessageCollection messageCollection = new MessageCollection ();
+            if (InheritsFromAttribute (typeDefinition)) {
+                if (!typeDefinition.Name.EndsWith ("Attribute")) {
+                    Location location = new Location (typeDefinition.FullName, typeDefinition.Name, 0);
+                    Message message = new Message ("The class name doesn't end with Attribute Suffix", location, MessageType.Warning);
+                    messageCollection.Add (message);                        
+                }
+            }
+            if (messageCollection.Count == 0)
+                return null;
+            return messageCollection;
+        }
+    }
+}
