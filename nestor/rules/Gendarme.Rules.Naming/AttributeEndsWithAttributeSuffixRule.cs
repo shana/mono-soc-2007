000774564edcde1ce9e09351af823a2344584e84
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-21 19:28:23 +0000

    * Check the MessageType in the tests.
    * Fixed InheritsFromAttribute private method.  Now it return a
    
    * InheritanceResult enumeration, that allows notify if the checking type
    inherits from an external type.
    
    * Then we can determine if the check is complete, or if the check is undetermined
    only adds a warning because we don't want to produce false positives and whe
    warn for potential defects.

--- nestor/rules/Gendarme.Rules.Naming/AttributeEndsWithAttributeSuffixRule.cs
+++ nestor/rules/Gendarme.Rules.Naming/AttributeEndsWithAttributeSuffixRule.cs
@@ -1,3 +1,29 @@
+//
+// Gendarme.Rules.Naming.AttributeEndsWithAttributeSuffixRule class
+//
+// Authors:
+//	Néstor Salceda <nestor.salceda@gmail.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using System;
 
 using Mono.Cecil;
@@ -5,26 +31,49 @@ using Gendarme.Framework;
 
 namespace Gendarme.Rules.Naming {
 	public class AttributeEndsWithAttributeSuffixRule : ITypeRule {
-		private bool InheritsFromAttribute (TypeDefinition typeDefinition)
+		private enum InheritanceResult {
+			InheritsFromAttribute,
+			NoInheritsFromAttribute,
+			Undetermined
+		}
+	
+		private InheritanceResult InheritsFromAttribute (TypeReference typeReference)
 		{
-			TypeDefinition current =  typeDefinition;
-			while (current.FullName != "System.Object") {
-				if (current.BaseType.FullName == "System.Attribute")
-					return true;
-				current = (TypeDefinition)current.BaseType;
+			if (typeReference is TypeDefinition) {
+				TypeDefinition current =  (TypeDefinition)typeReference;
+				while (current.FullName != "System.Object") {
+					if (current.BaseType.FullName == "System.Attribute")
+						return InheritanceResult.InheritsFromAttribute;
+					if (current.BaseType is TypeDefinition)
+						current = (TypeDefinition)current.BaseType;
+					else
+						return InheritanceResult.Undetermined;
+				}
+				return InheritanceResult.NoInheritsFromAttribute;
 			}
-			return false;
+			return InheritanceResult.Undetermined;
 		}
 		
 		public MessageCollection CheckType (TypeDefinition typeDefinition, Runner runner)
 		{
 			MessageCollection messageCollection = new MessageCollection ();
-			if (InheritsFromAttribute (typeDefinition)) {
+			switch (InheritsFromAttribute (typeDefinition)) {
+			case InheritanceResult.InheritsFromAttribute:
 				if (!typeDefinition.Name.EndsWith ("Attribute")) {
 					Location location = new Location (typeDefinition.FullName, typeDefinition.Name, 0);
-					Message message = new Message ("The class name doesn't end with Attribute Suffix", location, MessageType.Warning);
+					Message message = new Message ("The class name doesn't end with Attribute Suffix", location, MessageType.Error);
 					messageCollection.Add (message);                        
 				}
+				break;
+			case InheritanceResult.Undetermined:
+				Location location = new Location (typeDefinition.FullName, typeDefinition.Name, 0);
+				Message message = new Message ("I can't determine if this type inherits from Attribute.", location, MessageType.Warning);
+				messageCollection.Add (message);
+				break;
+			case InheritanceResult.NoInheritsFromAttribute:
+				break;
+			default:
+				break;
 			}
 			if (messageCollection.Count == 0)
 				return null;
