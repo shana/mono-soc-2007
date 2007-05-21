0a426adbb4901fca29b0cea702a1a7929595ecfb
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-21 18:55:03 +0000

    Added the propset eol:native. Changed spaces to tabs for guidelines. Add a new test inheriting an attribute which is contained in other assembly.

--- trunk/nestor/rules/Gendarme.Rules.Naming/Test/AttributeEndsWithAttributeSuffixTest.cs
+++ trunk/nestor/rules/Gendarme.Rules.Naming/Test/AttributeEndsWithAttributeSuffixTest.cs
@@ -8,59 +8,69 @@ using NUnit.Framework;
 
 namespace Test.Rules.Naming {
 
-    public class CorrectAttribute : Attribute {
-    }
-    
-    public class Incorrect : Attribute {
-    }
+	public class CorrectAttribute : Attribute {
+	}
 
-    public class OtherAttribute : CorrectAttribute {
-    }
-    
-    public class Other : CorrectAttribute {
-    }
-    
-    [TestFixture]
-    public class AttributeEndsWithAttributeSuffixTest {
+	public class Incorrect : Attribute {
+	}
+
+	public class OtherAttribute : CorrectAttribute {
+	}
+	
+	public class Other : CorrectAttribute {
+	}
+	
+	public class CorrectContextStaticAttribute : ContextStaticAttribute {
+	}
+	
+	[TestFixture]
+	public class AttributeEndsWithAttributeSuffixTest {
 		private ITypeRule rule;
 		private AssemblyDefinition assembly;
 		private TypeDefinition type;
-       
-        [TestFixtureSetUp]
-        public void FixtureSetUp ()
-        {
-            string unit = Assembly.GetExecutingAssembly ().Location;
-            assembly = AssemblyFactory.GetAssembly (unit);
+	
+		[TestFixtureSetUp]
+		public void FixtureSetUp ()
+		{
+			string unit = Assembly.GetExecutingAssembly ().Location;
+			assembly = AssemblyFactory.GetAssembly (unit);
 			rule = new AttributeEndsWithAttributeSuffixRule ();
-        }
-        
-        [Test]
-        public void TestOneLevelInheritanceIncorrectName () 
-        {
-            type = assembly.MainModule.Types ["Test.Rules.Naming.Incorrect"];
-            Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
-            Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
-        }
-        
-        [Test]
-        public void TestOneLevelInheritanceCorrectName () 
-        {
-            type = assembly.MainModule.Types ["Test.Rules.Naming.CorrectAttribute"];
-            Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
-        }
-        
-        [Test]
-        public void TestVariousLevelInheritanceCorrectName () {
-            type = assembly.MainModule.Types ["Test.Rules.Naming.OtherAttribute"];
-            Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
-        }
-        
-        [Test]
-        public void TestVariousLevelInheritanceIncorrectName () 
-        {
-            type = assembly.MainModule.Types ["Test.Rules.Naming.Other"];
-            Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
-            Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
-        }
-    }
+		}
+		
+		[Test]
+		public void TestOneLevelInheritanceIncorrectName () 
+		{
+			type = assembly.MainModule.Types ["Test.Rules.Naming.Incorrect"];
+			Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
+			Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
+		}
+		
+		[Test]
+		public void TestOneLevelInheritanceCorrectName () 
+		{
+			type = assembly.MainModule.Types ["Test.Rules.Naming.CorrectAttribute"];
+			Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
+		}
+		
+		[Test]
+		public void TestVariousLevelInheritanceCorrectName () {
+			type = assembly.MainModule.Types ["Test.Rules.Naming.OtherAttribute"];
+			Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
+		}
+		
+		[Test]
+		public void TestVariousLevelInheritanceIncorrectName () 
+		{
+			type = assembly.MainModule.Types ["Test.Rules.Naming.Other"];
+			Assert.IsNotNull (rule.CheckType (type, new MinimalRunner ()));
+			Assert.AreEqual (rule.CheckType (type, new MinimalRunner ()).Count, 1);
+		}
+		
+		[Test]
+		public void TestVariousLevelInheritanceExternalTypeCorrectName () 
+		{
+		    type = assembly.MainModule.Types ["Test.Rules.Naming.CorrectContextStaticAttribute"];
+		    Assert.IsNull (rule.CheckType (type, new MinimalRunner ()));
+		}
+	}
 }
