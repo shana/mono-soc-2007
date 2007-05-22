2f762eaaad1da8c237906415b110f45735f1efeb
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-22 22:39:19 +0000

    Ï've forgotten the Copyright.

--- nestor/rules/Gendarme.Rules.Naming/AttributeEndsWithAttributeSuffixRule.cs
+++ nestor/rules/Gendarme.Rules.Naming/AttributeEndsWithAttributeSuffixRule.cs
@@ -4,6 +4,8 @@
 // Authors:
 //	Néstor Salceda <nestor.salceda@gmail.com>
 //
+//  (C) 2007 Néstor Salceda
+//
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
