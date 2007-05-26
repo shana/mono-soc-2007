f2ef014fae8ad203e340de9382317c27c2cf7d92
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-26 15:08:01 +0000

    2007-05-26  George Giolfan  <georgegiolfan@yahoo.com>
    	Changed some file names to match WPF rules.

---
+++ george/Source/PresentationFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Windows;
+
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.ExternalAssembly, ResourceDictionaryLocation.SourceAssembly)]
