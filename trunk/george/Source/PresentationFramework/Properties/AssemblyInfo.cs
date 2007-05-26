6f0ddfa502994b61cbd208b57a8779da2108e26e
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-26 17:28:08 +0000

    2007-05-26  George Giolfan  <georgegiolfan@yahoo.com>
    	* AssemblyInfo.cs: Disabled unused attribute.

--- trunk/george/Source/PresentationFramework/Properties/AssemblyInfo.cs
+++ trunk/george/Source/PresentationFramework/Properties/AssemblyInfo.cs
@@ -3,4 +3,6 @@ using System.Windows;
 
 [assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: AssemblyVersion("3.0.0.0")]
+#if !Implementation
 [assembly: ThemeInfo(ResourceDictionaryLocation.ExternalAssembly, ResourceDictionaryLocation.SourceAssembly)]
+#endif
