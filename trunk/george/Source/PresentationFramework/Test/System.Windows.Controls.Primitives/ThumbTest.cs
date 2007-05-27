995b7998c90c162312a2315971990b594620bb35
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-05-27 11:49:22 +0000

    2007-05-25  George Giolfan  <georgegiolfan@yahoo.com>
    	* Various test files: Added some tests.

--- trunk/george/Source/PresentationFramework/Test/System.Windows.Controls.Primitives/ThumbTest.cs
+++ trunk/george/Source/PresentationFramework/Test/System.Windows.Controls.Primitives/ThumbTest.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Windows.Input;
 using System.Windows.Media;
 #if Implementation
 using System;
@@ -29,5 +31,38 @@ namespace System.Windows.Controls.Primitives {
 			Thumb t = new Thumb();
 			Assert.IsNull(t.Style, "Style");
 		}
+
+		#region OrderOfEvents
+		[Test]
+		public void OrderOfEvents() {
+			new OrderOfEventsThumb();
+		}
+
+		class OrderOfEventsThumb : Thumb {
+			List<string> events = new List<string>();
+
+			public OrderOfEventsThumb() {
+				DragStarted += new DragStartedEventHandler(OrderOfEventsThumb_DragStarted);
+				DragDelta += new DragDeltaEventHandler(OrderOfEventsThumb_DragDelta);
+				DragCompleted += new DragCompletedEventHandler(OrderOfEventsThumb_DragCompleted);
+				MouseButtonEventArgs e = new MouseButtonEventArgs(Mouse.PrimaryDevice, Environment.TickCount, MouseButton.Left);
+				e.RoutedEvent = MouseLeftButtonDownEvent;
+				OnMouseLeftButtonDown(e);
+				Assert.AreEqual(events.Count, 1);
+			}
+
+			void OrderOfEventsThumb_DragStarted(object sender, DragStartedEventArgs e) {
+				events.Add("DragStarted");
+			}
+
+			void OrderOfEventsThumb_DragDelta(object sender, DragDeltaEventArgs e) {
+				events.Add("DragDelta");
+			}
+
+			void OrderOfEventsThumb_DragCompleted(object sender, DragCompletedEventArgs e) {
+				events.Add("DragCompleted");
+			}
+		}
+		#endregion
     }
 }
