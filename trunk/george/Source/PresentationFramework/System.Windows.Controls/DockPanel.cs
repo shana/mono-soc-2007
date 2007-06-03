66f4d9bb8e5f15e5de112022374e5781a68ed5df
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-03 18:33:02 +0000

    2007-06-03  George Giolfan  <georgegiolfan@yahoo.com>
    	* DockPanel.cs: Fixed MeasureOverride, ArrangeOverride.

--- trunk/george/Source/PresentationFramework/System.Windows.Controls/DockPanel.cs
+++ trunk/george/Source/PresentationFramework/System.Windows.Controls/DockPanel.cs
@@ -44,52 +44,59 @@ namespace System.Windows.Controls {
 
 		#region Protected Methods
 		protected override Size ArrangeOverride(Size finalSize) {
-			Rect remaining_rect = new Rect(new Point(0, 0), finalSize);
-			int children_to_lay_out_normally = Children.Count - (LastChildFill ? 1 : 0);
-			for (int child_index = 0; child_index < children_to_lay_out_normally; child_index ++) {
-				UIElement element = Children[child_index];
-				switch (GetDock(element)) {
-				case Dock.Left:
-					element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Top, element.DesiredSize.Width, remaining_rect.Height));
-					remaining_rect.X += element.DesiredSize.Width;
-					remaining_rect.Width -= element.DesiredSize.Width;
-					break;
-				case Dock.Top:
-					element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Top, remaining_rect.Width, element.DesiredSize.Height));
-					remaining_rect.Y += element.DesiredSize.Height;
-					remaining_rect.Height -= element.DesiredSize.Height;
-					break;
-				case Dock.Right:
-					element.Arrange(new Rect(remaining_rect.Right - element.DesiredSize.Width, remaining_rect.Top, element.DesiredSize.Width, remaining_rect.Height));
-					remaining_rect.Width -= element.DesiredSize.Width;
-					break;
-				default:
-					element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Bottom - element.DesiredSize.Height, remaining_rect.Width, element.DesiredSize.Height));
-					remaining_rect.Height -= element.DesiredSize.Height;
-					break;
+			if (Children.Count != 0) {
+				Rect remaining_rect = new Rect(new Point(0, 0), finalSize);
+				int children_to_lay_out_normally = Children.Count - (LastChildFill ? 1 : 0);
+				for (int child_index = 0; child_index < children_to_lay_out_normally; child_index++) {
+					UIElement element = Children[child_index];
+					switch (GetDock(element)) {
+					case Dock.Left:
+						element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Top, element.DesiredSize.Width, remaining_rect.Height));
+						remaining_rect.X += element.DesiredSize.Width;
+						remaining_rect.Width -= element.DesiredSize.Width;
+						break;
+					case Dock.Top:
+						element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Top, remaining_rect.Width, element.DesiredSize.Height));
+						remaining_rect.Y += element.DesiredSize.Height;
+						remaining_rect.Height -= element.DesiredSize.Height;
+						break;
+					case Dock.Right:
+						element.Arrange(new Rect(remaining_rect.Right - element.DesiredSize.Width, remaining_rect.Top, element.DesiredSize.Width, remaining_rect.Height));
+						remaining_rect.Width -= element.DesiredSize.Width;
+						break;
+					default:
+						element.Arrange(new Rect(remaining_rect.Left, remaining_rect.Bottom - element.DesiredSize.Height, remaining_rect.Width, element.DesiredSize.Height));
+						remaining_rect.Height -= element.DesiredSize.Height;
+						break;
+					}
 				}
+				if (LastChildFill)
+					Children[Children.Count - 1].Arrange(remaining_rect);
 			}
-			if (LastChildFill)
-				Children[Children.Count - 1].Arrange(remaining_rect);
 			return finalSize;
 		}
 
 		protected override Size MeasureOverride(Size availableSize) {
-			//TODO: Handle infinite availableSize.
-			Size remaining_size = new Size(availableSize.Width, availableSize.Height);
-			foreach (UIElement element in Children) {
-				element.Measure(remaining_size);
-				switch (GetDock(element)) {
-				case Dock.Left:
-				case Dock.Right:
-					remaining_size.Width -= element.DesiredSize.Width;
-					break;
-				default:
-					remaining_size.Height -= element.DesiredSize.Height;
-					break;
+			if (Children.Count == 0)
+				return new Size(0, 0);
+			if (double.IsInfinity(availableSize.Width))
+				return new Size(0, 0);
+			else {
+				Size remaining_size = new Size(availableSize.Width, availableSize.Height);
+				foreach (UIElement element in Children) {
+					element.Measure(remaining_size);
+					switch (GetDock(element)) {
+					case Dock.Left:
+					case Dock.Right:
+						remaining_size.Width -= element.DesiredSize.Width;
+						break;
+					default:
+						remaining_size.Height -= element.DesiredSize.Height;
+						break;
+					}
 				}
+				return availableSize;
 			}
-			return availableSize;
 		}
 		#endregion
 	}
