ddbd4975ca6e69556306c35c5899aa51c7017721
debackerl <debackerl@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-05 11:04:05 +0000

    * SmartWindow.cs: Optimized with a caching mechanism to find children a lot faster.

--- laurent/src/Ribbons/Ribbons/SmartWindow.cs
+++ laurent/src/Ribbons/Ribbons/SmartWindow.cs
@@ -6,11 +6,11 @@ namespace Ribbons
 {
 	public class SmartWindow : Window
 	{
-		private List<Container> lastHoveredContainers;
+		private List<Widget> lastHoveredWidgets;
 		
-		public SmartWindow(WindowType type) : base(type)
+		public SmartWindow (WindowType type) : base (type)
 		{
-			lastHoveredContainers = new List<Gtk.Container> ();
+			lastHoveredWidgets = new List<Gtk.Widget> ();
 		}
 		
 		protected override bool OnWidgetEvent (Gdk.Event evnt)
@@ -37,29 +37,58 @@ namespace Ribbons
 		
 		private bool PropagateEventGivenCoordinate (Gdk.Event evnt, int x, int y)
 		{
-			Container current = this;
-			Widget match = this;
+			Container current = this;	// Current container containing the coordinate
+			Widget match = this;	// Current match for the position
+			int pos = 0;	// Current position in lastHoveredWidgets
 			do
 			{
 				Container next = null;
+				
+				if(lastHoveredWidgets.Count > pos)	// Does it has a candidate ?
+				{
+					Widget candidate = lastHoveredWidgets[pos];
+					if(candidate.Parent == current)	// Is it still a child of the current container ?
+					{
+						Gdk.Rectangle alloc = candidate.Allocation;
+						if(alloc.Contains (x, y))	// Does it contain the coordinate ?
+						{
+							match = candidate;	// The candiate is the winner!
+							next = candidate as Container;
+						}
+					}
+				}
+				
 				foreach(Widget child in current.Children)
 				{
 					Gdk.Rectangle alloc = child.Allocation;
-					if(alloc.Contains(x, y))
+					if(alloc.Contains (x, y))
 					{
 						match = child;
 						next = child as Container;
 						break;
 					}
 				}
+				
+				if(match != current)	// Has a child been found ?
+				{
+					if(lastHoveredWidgets.Count > pos)	// Is there enough room ?
+						lastHoveredWidgets[pos] = match;
+					else
+						lastHoveredWidgets.Add(match);
+					++pos;
+				}
+				
 				current = next;
 			} while (current != null);
 			
-			if(match == this)
+			// Remove excess widgets kept from last time
+			lastHoveredWidgets.RemoveRange (pos, lastHoveredWidgets.Count - pos);
+			
+			if(match == this)	// A widget has been found, let's send it the event
 			{
 				return base.OnWidgetEvent (evnt);
 			}
-			else
+			else	// No widget found, the window keeps the event
 			{
 				match.ProcessEvent (evnt);
 				return true;
