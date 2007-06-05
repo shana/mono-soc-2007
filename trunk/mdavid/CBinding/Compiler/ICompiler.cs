0ce330b705e7f7388c64cfe64fcbe468e3ee8f72
MarcosMarin <MarcosMarin@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-05 18:05:43 +0000

    Packages node now appears on top of every other node and the references node is only removed if this is a C/C++ project, also cleaned up a bit

--- trunk/mdavid/CBinding/Compiler/ICompiler.cs
+++ trunk/mdavid/CBinding/Compiler/ICompiler.cs
@@ -1,3 +1,35 @@
+//
+// ICompiler.cs: interface that must be implemented by any class that wants
+// to provide a compiler for the CBinding addin.
+//
+// Authors:
+//   Marcos David Marin Amador <MarcosMarin@gmail.com>
+//
+// Copyright (C) 2007 Marcos David Marin Amador
+//
+//
+// This source code is licenced under The MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
 using Mono.Addins;
 
 using MonoDevelop.Core;
@@ -18,7 +50,7 @@ namespace CBinding
 		
 		ICompilerResult Compile (
 			ProjectFileCollection projectFiles,
-		    ProjectReferenceCollection references,
+		    ProjectPackageCollection packages,
 		    CProjectConfiguration configuration,
 		    IProgressMonitor monitor);
 	}
