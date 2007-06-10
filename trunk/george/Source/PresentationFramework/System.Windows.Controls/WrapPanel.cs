b19dc2d7c2ff5e2d3400a1d8ed097fafd1dfb24d
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-10 23:34:29 +0000

    2007-06-11  George Giolfan  <georgegiolfan@yahoo.com>
    	* WrapPanel.cs: Implemented ArrangeOverride, MeasureOverride.

--- trunk/george/Source/PresentationFramework/System.Windows.Controls/WrapPanel.cs
+++ trunk/george/Source/PresentationFramework/System.Windows.Controls/WrapPanel.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.ComponentModel;
 #if Implementation
+using System;
 using System.Windows;
 using System.Windows.Controls;
 namespace Mono.System.Windows.Controls {
@@ -43,11 +45,98 @@ namespace System.Windows.Controls {
 
 		#region Protected Methods
 		protected override Size ArrangeOverride(Size finalSize) {
-			return base.ArrangeOverride(finalSize);
+			// In the following code the meaning of width, height, row, top in local variable names is height, width, column, left when the Orientation is Vertical. 
+			bool horizontal = Orientation == Orientation.Horizontal;
+			double used_width = 0;
+			double used_height = 0;
+			double available_row_width = horizontal ? finalSize.Width : finalSize.Height;
+			double current_row_height = 0;
+			List<UIElement> current_row_elements = new List<UIElement>();
+			for(int child_index = 0; child_index < Children.Count; child_index++) {
+				UIElement child = Children[child_index];
+				Size desired_child_size = GetDesiredChildSize(child);
+				current_row_elements.Add(child);
+				used_width += desired_child_size.Width;
+				current_row_height = Math.Max(current_row_height, desired_child_size.Height);
+				if ((used_width + desired_child_size.Width > available_row_width && used_width != 0) || child_index == Children.Count - 1) {
+					used_width = 0;
+					foreach (UIElement current_row_element in current_row_elements) {
+						desired_child_size = GetDesiredChildSize(current_row_element);
+						double top = used_height;
+						FrameworkElement framework_element = current_row_element as FrameworkElement;
+						if (framework_element != null)
+							if (horizontal)
+								switch (framework_element.VerticalAlignment) {
+								case VerticalAlignment.Top:
+									break;
+								case VerticalAlignment.Bottom:
+									top += current_row_height - desired_child_size.Height;
+									break;
+								case VerticalAlignment.Center:
+									top += (current_row_height - desired_child_size.Height) / 2;
+									break;
+								default:
+									desired_child_size.Height = Math.Min(current_row_height, finalSize.Height);
+									break;
+								} else
+								switch (framework_element.HorizontalAlignment) {
+								case HorizontalAlignment.Left:
+									break;
+								case HorizontalAlignment.Right:
+									top += current_row_height - desired_child_size.Height;
+									break;
+								case HorizontalAlignment.Center:
+									top += (current_row_height - desired_child_size.Height) / 2;
+									break;
+								default:
+									desired_child_size.Height = Math.Min(current_row_height, finalSize.Width);
+									break;
+								}
+						current_row_element.Arrange(horizontal ? new Rect(used_width, top, desired_child_size.Width, desired_child_size.Height) : new Rect(top, used_width, desired_child_size.Height, desired_child_size.Width));
+						used_width += desired_child_size.Width;
+					}
+					current_row_elements.Clear();
+					used_width = 0;
+					used_height += current_row_height;
+					current_row_height = 0;
+				}
+			}
+			return finalSize;
 		}
 
 		protected override Size MeasureOverride(Size availableSize) {
-			return base.MeasureOverride(availableSize);
+			bool horizontal = Orientation == Orientation.Horizontal;
+			if (double.IsPositiveInfinity(availableSize.Width) || double.IsPositiveInfinity(availableSize.Height)) {
+				Size result = new Size();
+				foreach (UIElement child in Children) {
+					child.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+					if (horizontal)
+						result.Width += child.DesiredSize.Width;
+					else
+						result.Height += child.DesiredSize.Height;
+				}
+				return result;
+			} else {
+				foreach (UIElement child in Children)
+					child.Measure(availableSize);
+				return availableSize;
+			}
+		}
+		#endregion
+
+		#region Private Methods
+		Size GetDesiredChildSize(UIElement element) {
+			Size result = new Size(element.DesiredSize.Width, element.DesiredSize.Height);
+			FrameworkElement framework_element = element as FrameworkElement;
+			if (framework_element != null) {
+				if (!double.IsNaN(ItemWidth) && double.IsNaN(framework_element.Width)) 
+					result.Width = ItemWidth;
+				if (!double.IsNaN(ItemHeight) && double.IsNaN(framework_element.Height)) 
+					result.Height = ItemHeight;
+			}
+			if (Orientation == Orientation.Vertical)
+				result = new Size(result.Height, result.Width);
+			return result;
 		}
 		#endregion
 	}
