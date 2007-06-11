a88da326b68835f2f5754a9eb8364f2ca19f96b5
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-11 19:44:54 +0000

    * MonodocEditor/Driver.cs, MonodocEditor/MainWindow.cs,   MonodocEditor/EditorWindow.cs, MonodocEditor/gtk-gui/MainWindow.cs,   MonodocEditor/gtk-gui/gui.stetic,   MonodocEditor/gtk-gui/Monodoc.Editor.EditorWindow.cs: Changed   MainWindow to EditorWindow. Changed the namespace to   Monodoc.Editor. * MonodocEditor/MonodocEditor.mdp,   MonodocEditor/gtk-gui/Monodoc.Editor.OpenDocDialog.cs,   MonodocEditor/OpenDocDialog.cs: Added a FileSelector to open XML   file to edit.

---
+++ hector/MonodocEditor/MonodocEditor/OpenDocDialog.cs
@@ -0,0 +1,21 @@
+//
+// MainWindow.cs: Main window of the app.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+//
+
+using System;
+using Gtk;
+
+namespace Monodoc.Editor {
+public partial class OpenDocDialog : Gtk.Dialog {
+	
+	public OpenDocDialog()
+	{
+		this.Build();
+	}
+}
+}
