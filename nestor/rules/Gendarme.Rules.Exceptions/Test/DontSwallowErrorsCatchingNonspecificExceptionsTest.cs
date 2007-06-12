c0577235fff3dea0b4ff1572342f084861af1f7d
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-12 09:18:31 +0000

    Changed the file name. Modified the makefile because it was generating Test.Rules.exception.dll Added test cases.

---
+++ nestor/rules/Gendarme.Rules.Exceptions/Test/DontSwallowErrorsCatchingNonspecificExceptionsTest.cs
@@ -0,0 +1,82 @@
+//
+// Unit Test for DontSwallowErrorsCatchingNonspecificExceptions Rule
+//
+// Authors:
+//	Néstor Salceda <nestor.salceda@gmail.com>
+//
+// 	(C) 2007 Néstor Salceda
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections;
+using System.Reflection;
+using System.IO;
+
+using Gendarme.Framework;
+using Gendarme.Rules.Exceptions;
+using Mono.Cecil;
+using NUnit.Framework;
+
+namespace Test.Rules.Exceptions {
+	
+
+	[TestFixture]
+	public class DontSwallowErrorsCatchingNonspecificExceptionsText {
+	    
+	    //Methods for make the tests
+	    public void SwallowErrorsCatchingException () 
+	    {
+	        try { 
+	            File.Open ("foo.txt", FileMode.Open);
+	        }
+	        catch (Exception exception) {
+	        }
+	    }
+	    
+	    public void SwallowErrorsCatchingSystemException () 
+	    {
+	        try {
+	        	File.Open ("foo.txt", FileMode.Open);
+	        }
+	        catch (SystemException exception) {
+	        }
+	    }
+	    
+	    public void SwallowErrorsCatchingTypeException () 
+	    {
+	    	try {
+	    		File.Open ("foo.txt", FileMode.Open);
+	    	}
+	    	catch (Exception) {
+	    	}
+	    }
+	    
+	    public void SwallowErrorsCatchingAll () 
+	    {
+	        try { 
+	            File.Open ("foo.txt", FileMode.Open);
+	        }
+	        catch {
+	        }
+	    }
+	}
+}
