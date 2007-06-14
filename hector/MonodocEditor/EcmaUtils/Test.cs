a6a38c22224fc0479f4bb52e3f00d8d7491d81d7
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-14 17:22:22 +0000

    * EcmaUtils/Test, EcmaUtils/Test/TestEcmaReader.cs,   EcmaUtils/Test/Examples, EcmaUtils/Test/Examples/Accel.xml,   EcmaUtils/Test.cs, EcmaUtils/EcmaUtils.mdp: Adding unit test   infrastructure. * EcmaUtils/EcmaReader.cs: Changed the form of getting the EcmaUtils   assembly to get the XSD stream.

---
+++ hector/MonodocEditor/EcmaUtils/Test.cs
@@ -0,0 +1,28 @@
+//
+// EcmaReader.cs: Class that reads an Monodoc XML Documentation.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+
+using System;
+using System.IO;
+
+namespace Monodoc.EcmaUtils {
+public class Test {
+		
+	public static string PathOfTestFiles ()
+	{
+		// FIXME: Este es un hack para correr los casos que depende de la
+		// locacion cuando se corre el test.
+		string path;
+		path = Environment.CurrentDirectory;
+		path = path.Replace ("bin" + Path.DirectorySeparatorChar + "Debug", String.Empty);
+		path = path.Replace ("bin" + Path.DirectorySeparatorChar + "Release", String.Empty);
+		path = Path.Combine (path, "Test");
+		
+		return path;
+	}
+}
+}
---
+++ hector/MonodocEditor/EcmaUtils/Test/TestEcmaReader.cs
@@ -0,0 +1,55 @@
+///
+// TestEcmaReader.cs: Test unit for EcmaReader class.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+
+using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Monodoc.EcmaUtils {
+[TestFixture()]
+public class TestEcmaReader {
+	
+	private string pathTest;
+	private Type type;
+	
+	[SetUp()]
+	public void Initialize ()
+	{
+		string filePath;
+		
+		filePath = Test.PathOfTestFiles ();
+		pathTest = Path.Combine (filePath, "Examples");
+		type = Type.GetType ("Monodoc.EcmaUtils.EcmaReader");
+	}
+	
+	[Test()]
+	public void ConstructorInvalid ()
+	{
+		EcmaReader reader = null;
+		
+		try {
+			reader = new EcmaReader (String.Empty);
+		} catch {
+		}
+		
+		Assert.IsNotInstanceOfType (type, reader, "CI01");
+		Assert.IsNull (reader, "CI02");
+	}
+	
+	[Test()]
+	public void ConstructorValid ()
+	{
+		string fileName;
+		
+		fileName = Path.Combine (pathTest, "Accel.xml");
+		EcmaReader reader = new EcmaReader (fileName);
+		Assert.IsInstanceOfType (type, reader, "CV02");
+		Assert.IsNotNull (reader, "CV02");
+	}
+}
+}
