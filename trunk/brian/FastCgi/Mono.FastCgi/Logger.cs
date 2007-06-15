4a5419408c5ece4c4d97878e5bc089cb969850d4
brian.nickel <brian.nickel@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-15 19:14:16 +0000

    2007-06-15 Brian Nickel <brian.nickel@gmail.com>
    	* Mono.FastCgi/Logger.cs: Made singleton to take advantage of
    	destructor. Documented completely.

--- trunk/brian/FastCgi/Mono.FastCgi/Logger.cs
+++ trunk/brian/FastCgi/Mono.FastCgi/Logger.cs
@@ -33,54 +33,206 @@ using System.Globalization;
 
 namespace Mono.FastCgi
 {
+	/// <summary>
+	///   Specifies what type of message to log.
+	/// </summary>
 	[Flags]
 	public enum LogLevel
 	{
+		/// <summary>
+		///    No messages will be logged.
+		/// </summary>
 		None    = 0x00,
+		
+		/// <summary>
+		///    Error messages will be logged.
+		/// </summary>
 		Error   = 0x01,
+		
+		/// <summary>
+		///    Warning message will be logged.
+		/// </summary>
 		Warning = 0x02,
+		
+		/// <summary>
+		///    Notice messages will be logged.
+		/// </summary>
 		Notice  = 0x04,
-		All     = 0xFF
+		
+		/// <summary>
+		///    All messages will be logged.
+		/// </summary>
+		All     = Error | Warning | Notice
 	}
 	
+	
+	
 	/// <summary>
-	///   Logs contents to a specified location.
+	///    Stores log messages in a specified file.
 	/// </summary>
-	public static class Logger
+	public class Logger
 	{
-		private static StreamWriter writer;
-		private static LogLevel     level;
-		private static object       write_lock = new object ();
+		#region Private Fields
 		
-		public static LogLevel Level {
-			get {return level;}
-			set {level = value;}
+		private StreamWriter writer;
+		private LogLevel     level;
+		private object write_lock = new object ();
+		
+		#endregion
+		
+		
+		
+		#region Singleton Implementation
+		
+		/// This class is a singleton class to take advantage of the
+		/// destructor.
+		
+		private static Logger logger = new Logger ();
+		
+		~Logger ()
+		{
+			Close ();
 		}
 		
+		#endregion
+		
+		
+		
+		#region Public Static Properties
+		
+		/// <summary>
+		///    Gets and sets the levels of messages to log.
+		/// </summary>
+		/// <value>
+		///    A bitwise combined <see cref="LogLevel" /> specifying the
+		///    levels of events to log.
+		/// </value>
+		public static LogLevel Level {
+			get {return logger.level;}
+			set {logger.level = value;}
+		}
+		
+		#endregion
+		
+		
+		
+		#region Public Static Methods
+		
+		/// <summary>
+		///    Opens a file to log to.
+		/// </summary>
+		/// <param name="path">
+		///    <para>A <see cref="String" /> containing the path of the
+		///    file to open.</para>
+		///    <para>This value is the same as the parameter that would
+		///    be passed to <see cref="File.OpenWrite" />.</para>
+		/// </param>
+		/// <remarks>
+		///    For information on what exceptions are thrown by this
+		///    method, see <see cref="FileInfo.OpenWrite" />.
+		/// </remarks>
 		public static void Open (string path)
 		{
-			lock (write_lock) {
+			if (path == null)
+				throw new ArgumentNullException ("path");
+			
+			lock (logger.write_lock) {
 				Close ();
 				FileInfo info = new FileInfo (path);
 				Stream stream = info.OpenWrite ();
 				stream.Seek (0, SeekOrigin.End);
-				writer = new StreamWriter (stream);
+				logger.writer = new StreamWriter (stream);
 			}
 		}
 		
-		public static void Write (LogLevel level, IFormatProvider provider, string format, params object [] args)
+		/// <summary>
+		///    Writes a formatted string with a specified warning level
+		///    to the log file, if one exists.
+		/// </summary>
+		/// <param name="level">
+		///    A <see cref="LogLevel" /> containing the severity of the
+		///    message.
+		/// </param>
+		/// <param name="provider">
+		///    A <see cref="IFormatProvider" /> object to use when
+		///    formatting values for the message.
+		/// </param>
+		/// <param name="format">
+		///    A <see cref="string" /> containing the format to use for
+		///    the message.
+		/// </param>
+		/// <param name="args">
+		///    A <see cref="object[]" /> containing values to insert
+		///    into the format.
+		/// </param>
+		/// <remarks>
+		///    <para>The message will only be written to the log if
+		///    <see cref="Level" /> contains <paramref name="level" />.
+		///    </para>
+		///    <para>See <see
+		///    cref="string.Format(IFormatProvider,string,object[])" />
+		///    for more details on this method's arguments.</para>
+		/// </remarks>
+		public static void Write (LogLevel level,
+		                          IFormatProvider provider,
+		                          string format, params object [] args)
 		{
 			Write (level, string.Format (provider, format, args));
 		}
 				
-		public static void Write (LogLevel level, string format, params object [] args)
+		/// <summary>
+		///    Writes a formatted string with a specified warning level
+		///    to the log file, if one exists.
+		/// </summary>
+		/// <param name="level">
+		///    A <see cref="LogLevel" /> containing the severity of the
+		///    message.
+		/// </param>
+		/// <param name="format">
+		///    A <see cref="string" /> containing the format to use for
+		///    the message.
+		/// </param>
+		/// <param name="args">
+		///    A <see cref="object[]" /> containing values to insert
+		///    into the format.
+		/// </param>
+		/// <remarks>
+		///    <para>The message will only be written to the log if
+		///    <see cref="Level" /> contains <paramref name="level" />.
+		///    </para>
+		///    <para>This method outputs using the current culture of
+		///    the assembly. To use a different culture, use
+		///    <see
+		///    cref="Write(LogLevel,IFormatProvider,string,object[])"
+		///    />.</para>
+		///    <para>See <see cref="string.Format(string,object[])" />
+		///    for more details on this method's arguments.</para>
+		/// </remarks>
+		public static void Write (LogLevel level, string format,
+		                          params object [] args)
 		{
 			Write (level, CultureInfo.CurrentCulture, format, args);
 		}
 		
+		/// <summary>
+		///    Writes a formatted string with a specified warning level
+		///    to the log file, if one exists.
+		/// </summary>
+		/// <param name="level">
+		///    A <see cref="LogLevel" /> containing the severity of the
+		///    message.
+		/// </param>
+		/// <param name="message">
+		///    A <see cref="string" /> containing the message to write.
+		/// </param>
+		/// <remarks>
+		///    <para>The message will only be written to the log if
+		///    <see cref="Level" /> contains <paramref name="level" />.
+		///    </para>
+		/// </remarks>
 		public static void Write (LogLevel level, string message)
 		{
-			if (writer == null)
+			if (logger.writer == null)
 				return;
 			
 			if ((Level & level) == LogLevel.None)
@@ -92,21 +244,30 @@ namespace Mono.FastCgi
 				level,
 				message);
 			
-			lock (write_lock) {
-				writer.WriteLine (text);
+			lock (logger.write_lock) {
+				logger.writer.WriteLine (text);
 			}
 		}
 		
+		/// <summary>
+		///    Closes the log file and flushes its output.
+		/// </summary>
+		/// <remarks>
+		///    This method is called automatically when the class is
+		///    destroyed.
+		/// </remarks>
 		public static void Close ()
 		{
-			lock (write_lock) {
-				if (writer == null)
+			lock (logger.write_lock) {
+				if (logger.writer == null)
 					return;
 				
-				writer.Flush ();
-				writer.Close ();
-				writer = null;
+				logger.writer.Flush ();
+				logger.writer.Close ();
+				logger.writer = null;
 			}
 		}
+		
+		#endregion
 	}
 }
