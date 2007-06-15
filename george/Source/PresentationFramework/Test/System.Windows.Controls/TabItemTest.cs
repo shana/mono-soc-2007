af78e142adf7460aa9dbf9295a14f0d92308979b
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-15 20:20:58 +0000

    2007-06-15  George Giolfan  <georgegiolfan@yahoo.com>
    	* TabItemTest.cs: Added IsSelected and following tests.

--- george/Source/PresentationFramework/Test/System.Windows.Controls/TabItemTest.cs
+++ george/Source/PresentationFramework/Test/System.Windows.Controls/TabItemTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Windows.Input;
 #if Implementation
 using System;
 using System.Windows;
@@ -62,6 +63,112 @@ namespace System.Windows.Controls {
 		public void TabStripPlacement() {
 			TabItem tab_item = new TabItem();
 			Assert.AreEqual(tab_item.TabStripPlacement, Dock.Top, "1");
+		}
+
+		[Test]
+		public void IsSelected() {
+			TabItem i1 = new TabItem();
+			Assert.IsFalse(i1.IsSelected, "1");
+			i1.IsSelected = true;
+			Assert.IsTrue(i1.IsSelected, "2");
+			i1.IsSelected = false;
+			Assert.IsFalse(i1.IsSelected, "3");
+			TabControl t = new TabControl();
+			t.Items.Add(i1);
+			Assert.IsFalse(i1.IsSelected, "4");
+			Assert.IsNull(t.SelectedItem, "4 1");
+			i1.IsSelected = true;
+			Assert.IsTrue(i1.IsSelected, "5");
+			Assert.AreEqual(t.SelectedItem, i1, "5 1");
+			i1.IsSelected = false;
+			Assert.IsFalse(i1.IsSelected, "6");
+			Assert.IsNull(t.SelectedItem, "6 1");
+			TabItem i2 = new TabItem();
+			t.Items.Add(i2);
+			i2.IsSelected = true;
+			Assert.IsTrue(i2.IsSelected, "7");
+			Assert.AreSame(t.SelectedItem, i2, "7 1");
+			i1.IsSelected = true;
+			Assert.IsTrue(i1.IsSelected, "8");
+			Assert.AreSame(t.SelectedItem, i1, "8 1");
+
+			i1.IsSelected = false;
+			i2.IsSelected = false;
+			Window w = new Window();
+			w.Content = t;
+			w.Show();
+			Assert.IsTrue(i1.IsSelected, "9");
+			Assert.IsFalse(i2.IsSelected, "10");
+			Assert.AreSame(t.SelectedItem, i1, "10 1");
+			i1.IsSelected = true;
+			Assert.IsTrue(i1.IsSelected, "11");
+			Assert.IsFalse(i2.IsSelected, "12");
+			Assert.AreSame(t.SelectedItem, i1, "12 1");
+			i2.IsSelected = true;
+			Assert.IsFalse(i1.IsSelected, "13");
+			Assert.IsTrue(i2.IsSelected, "14");
+			Assert.AreSame(t.SelectedItem, i2, "14 1");
+			i1.IsSelected = false;
+			Assert.IsFalse(i1.IsSelected, "15");
+			Assert.IsTrue(i2.IsSelected, "16");
+			Assert.AreSame(t.SelectedItem, i2, "16 1");
+		}
+
+		#region OnApplyTemplateSetsIsSelected
+		[Test]
+		public void OnApplyTemplateSetsIsSelected() {
+			new OnApplyTemplateSetsIsSelectedTabControl();
+		}
+
+		class OnApplyTemplateSetsIsSelectedTabControl : TabControl {
+			public OnApplyTemplateSetsIsSelectedTabControl() {
+				TabItem t = new TabItem();
+				Items.Add(t);
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.IsTrue(t.IsSelected);
+			}
+
+			public override void OnApplyTemplate() {
+			}
+		}
+		#endregion
+
+		#region OnMouseLeftButtonDown
+		[Test]
+		public void OnMouseLeftButtonDown() {
+			new OnMouseLeftButtonDownTabItem();
+		}
+
+		class OnMouseLeftButtonDownTabItem : TabItem {
+			public OnMouseLeftButtonDownTabItem() {
+				MouseButtonEventArgs e = new MouseButtonEventArgs(Mouse.PrimaryDevice, Environment.TickCount, MouseButton.Left);
+				e.RoutedEvent = MouseLeftButtonDownEvent;
+				OnMouseLeftButtonDown(e);
+				Assert.IsFalse(IsSelected, "1");
+				Assert.IsTrue(e.Handled, "2");
+				e.Handled = false;
+				TabControl t = new TabControl();
+				t.Items.Add(new TabItem());
+				t.Items.Add(this);
+				OnMouseLeftButtonDown(e);
+				Assert.IsFalse(IsSelected, "3");
+				Assert.IsTrue(e.Handled, "4");
+				e.Handled = false;
+				Window w = new Window();
+				w.Content = t;
+				w.Show();
+				OnMouseLeftButtonDown(e);
+				Assert.IsTrue(IsSelected, "5");
+				Assert.IsTrue(e.Handled, "6");
+			}
+		}
+		#endregion
+
+		[Test]
+		public void Focusable() {
+			Assert.IsTrue(new TabItem().Focusable);
 		}
 	}
 }
