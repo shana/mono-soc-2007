4eb9667932a6c8f593dd16a348a5568be29ea3d8
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-16 18:10:44 +0000

    * MonodocEditor/EditorWindow.cs, MonodocEditor/MonodocEditor.mdp,   EcmaUtils/MonoDocument.cs, EcmaUtils/EcmaReader.cs: Added the   MonoDocument class.

--- hector/MonodocEditor/EcmaUtils/MonoDocument.cs
+++ hector/MonodocEditor/EcmaUtils/MonoDocument.cs
@@ -7,16 +7,73 @@
 // (C) 2007 Hector E. Gomez M
 
 using System;
-using System.IO;
 using System.Xml;
-using System.Xml.Schema;
-using System.Reflection;
 
 namespace Monodoc.EcmaUtils {
 public class MonoDocument {
+	private XmlDocument document;
+	private XmlNode root_element;
+	private bool valid;
 	
-	public MonoDocument ()
+	public MonoDocument (string filePath)
 	{
+		EcmaReader reader = new EcmaReader (filePath);
+		document = reader.Document;
+		root_element = reader.Document.DocumentElement;
+		valid = reader.Valid;
+	}
+	
+	public bool Valid {
+		get {
+			return valid;
+		}
+	}
+	
+	public void Convert ()
+	{
+		Console.WriteLine (ConvertRoot ());
+	}
+	
+	private string ConvertRoot () 
+	{
+		string result = "";
+		string temp;
+		
+		XmlElement root = root_element as XmlElement;
+		temp = "\nBegins Document:\n" + root_element.Name + "\n";
+		
+		if (root.HasAttributes) {
+			foreach (XmlNode node in root.Attributes) {
+				temp += "\tAttribute " + node.Name + ": " + node.InnerText + "\n";
+			}
+		}
+		
+		result = temp + ConvertRootChild (root.ChildNodes) ;
+		
+		return result;
+	}
+	
+	private string ConvertRootChild (XmlNodeList childNodes)
+	{
+		string result = "";
+		
+		foreach (XmlNode node in childNodes) {
+			switch (node.Name) {
+				case "TypeSignature":
+					result += ConvertSignature (node);
+					break;
+				default:
+					break;
+			}
+		}
+		
+		return result;
+	}
+	
+	private string ConvertSignature (XmlNode node)
+	{
+		string result = "";
+		return result;
 	}
 }
 }
