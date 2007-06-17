8f320bc93df28462db6b782189d6e19e916426e1
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-17 07:17:28 +0000

    * Monodoc.Editor.Utils/Test/Examples/WrapMode.xml,   Monodoc.Editor.Utils/Monodoc.Editor.Utils.mdp: Added WrapMode.xml   doc for test purposes. * Monodoc.Editor.Gui/DocumentTagTable.cs: Added basic tags for   DocumentBuffer.

--- hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentTagTable.cs
+++ hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentTagTable.cs
@@ -31,6 +31,127 @@ public class DocumentTagTable : TextTagTable {
 	
 	private void InitCommonTags ()
 	{
+		TextTag tag;
+		
+		tag = new TextTag ("Type");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Type:Attributes");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("TypeSignature");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("TypeSignature:Attributes");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("AssemblyInfo");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("AssemblyName");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("AssemblyPublicKey");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("AssemblyVersion");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("ThreadSafetyStatement");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Base");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("BaseTypeName");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Attributes");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Attribute");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("AttributeName");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Members");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Member");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Member:Attributes");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("MemberSignature");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("MemberSignature:Attributes");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("MemberType");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("ReturnValue");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("ReturnType");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Parameters");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("Docs");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("summary");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("remarks");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("para");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("see");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("link");
+		tag.Editable = false;
+		Add (tag);
+		
+		tag = new TextTag ("since");
+		tag.Editable = false;
+		Add (tag);
 	}
 }
 }
