99b7dcd844b55805854cb25468dc1eed696d19b1
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-18 18:56:43 +0000

    * Monodoc.Editor.Utils/MonoDocument.cs: Deleted Convert methods this   new is done in Deserializer. * Monodoc.Editor.Utils/EcmaReader.cs: Deleted Accesor. * Monodoc.Editor/EditorWindow.cs: Now using MonoDocument and   DocumentBufferArchiver when opening a file.

--- hector/Monodoc.Editor/Monodoc.Editor.Utils/MonoDocument.cs
+++ hector/Monodoc.Editor/Monodoc.Editor.Utils/MonoDocument.cs
@@ -1,5 +1,5 @@
 //
-// MonoDocument.cs: Class that reads an Monodoc XML Documentation.
+// MonoDocument.cs: Class that represents an Monodoc XML Documentation.
 //
 // Author:
 //   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
@@ -12,68 +12,25 @@ using System.Xml;
 namespace Monodoc.Editor.Utils {
 public class MonoDocument {
 	private XmlDocument document;
-	private XmlNode root_element;
 	private bool valid;
 	
 	public MonoDocument (string filePath)
 	{
 		EcmaReader reader = new EcmaReader (filePath);
 		document = reader.Document;
-		root_element = document.DocumentElement;
 		valid = reader.Valid;
 	}
 	
+	public string Text {
+		get {
+			return document.OuterXml;
+		}
+	}
+		
 	public bool Valid {
 		get {
 			return valid;
 		}
-	}
-	
-	public void Convert ()
-	{
-		Console.WriteLine (ConvertRoot ());
-	}
-	
-	private string ConvertRoot () 
-	{
-		string result = "";
-		string temp;
-		
-		XmlElement root = root_element as XmlElement;
-		temp = "\nBegins Document:\n" + root_element.Name + "\n";
-		
-		if (root.HasAttributes) {
-			foreach (XmlNode node in root.Attributes) {
-				temp += "\tAttribute " + node.Name + ": " + node.InnerText + "\n";
-			}
-		}
-		
-		result = temp + ConvertRootChild (root.ChildNodes) ;
-		
-		return result;
-	}
-	
-	private string ConvertRootChild (XmlNodeList childNodes)
-	{
-		string result = "";
-		
-		foreach (XmlNode node in childNodes) {
-			switch (node.Name) {
-				case "TypeSignature":
-					result += ConvertSignature (node);
-					break;
-				default:
-					break;
-			}
-		}
-		
-		return result;
-	}
-	
-	private string ConvertSignature (XmlNode node)
-	{
-		string result = "";
-		return result;
 	}
 }
 }
