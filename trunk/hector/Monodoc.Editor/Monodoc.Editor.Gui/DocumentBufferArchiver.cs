a87bb5b43f697ab917f711907ee7409426a5f571
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-19 05:26:25 +0000

    * DocumentBufferArchiver.cs: Created specific methods to handle the   attributes of differentes elements. Added some temp debug stuff. * Monodoc.Editor.Gui.mdp: Added pango-sharp to the references of the   project. * DocumentTagTable.cs: Added more attribute related tags.

--- trunk/hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentBufferArchiver.cs
+++ trunk/hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentBufferArchiver.cs
@@ -45,43 +45,50 @@ public class DocumentBufferArchiver {
 		while (xmlReader.Read ()) {
 			switch (xmlReader.NodeType) {
 				case XmlNodeType.Element:
+					bool empty = xmlReader.IsEmptyElement;
 					tagStart = new TagStart ();
 					tagStart.Start = offset;
-					
 					tagStart.Tag = buffer.TagTable.Lookup (xmlReader.Name);
+					
+					Console.WriteLine ("Element: {0} Start: {1}", tagStart.Tag.Name, tagStart.Start);
+					
 					if (xmlReader.HasAttributes) {
-						string tagName = xmlReader.Name + ":Attributes";
-						string attributes = "";
-						
-						while (xmlReader.MoveToNextAttribute ()) {
-							attributes += xmlReader.Name + ": " + xmlReader.Value + " ";
-						}
-						
-						attributes += "\n";
-						insert_at = buffer.GetIterAtOffset (offset);
-						buffer.InsertWithTagsByName (ref insert_at, attributes, tagName);
-						
-						offset += attributes.Length;
+						offset = DeserializeAttributes (buffer, offset, xmlReader);
 					}
 					
-					stack.Push (tagStart);
+					if (empty) {
+						TextIter applyStart, applyEnd;
+						applyStart = buffer.GetIterAtOffset (tagStart.Start);
+						applyEnd = buffer.GetIterAtOffset (offset);
+					
+						Console.WriteLine ("TagName: {0}, Start: {1}, End: {2}", xmlReader.Name, tagStart.Start, offset);
+					
+						buffer.ApplyTag (tagStart.Tag, applyStart, applyEnd);
+						break;
+					} else
+						stack.Push (tagStart);
 					break;
 				case XmlNodeType.Text:
 				case XmlNodeType.Whitespace:
 				case XmlNodeType.SignificantWhitespace:
+					Console.WriteLine ("Text: " + xmlReader.Value);
+					
 					insert_at = buffer.GetIterAtOffset (offset);
 					buffer.Insert (ref insert_at, xmlReader.Value);
 					
 					offset += xmlReader.Value.Length;
+					Console.WriteLine ("Offset: " + offset);
 					break;
 				case XmlNodeType.EndElement:
 					tagStart = (TagStart) stack.Pop ();
-					if (tagStart.Tag == null)
-						break;
+					
+					Console.WriteLine ("Element: {0}, End: {1}", tagStart.Tag.Name, offset);
 					
 					TextIter applyStart, applyEnd;
 					applyStart = buffer.GetIterAtOffset (tagStart.Start);
 					applyEnd = buffer.GetIterAtOffset (offset);
+					
+					Console.WriteLine ("TagName: {0}, Start: {1}, End: {2}", xmlReader.Name, tagStart.Start, offset);
 					
 					buffer.ApplyTag (tagStart.Tag, applyStart, applyEnd);
 					break;
@@ -90,8 +97,87 @@ public class DocumentBufferArchiver {
 						    xmlReader.NodeType,
 						    xmlReader.Value);
 					break;
-				}
 			}
 		}
+	}
+	
+	private static int DeserializeAttributes (TextBuffer buffer, int offset, XmlTextReader xmlReader)
+	{
+		int result = offset;
+		switch (xmlReader.Name) {
+			case "Type":
+				result = DeserializeTypeAttributes (buffer, offset, xmlReader);
+				break;
+			case "TypeSignature":
+				result = DeserializeTypeSignatureAttributes (buffer, offset, xmlReader);
+				break;
+			case "link":
+				result = DeserializeLinkAttributes (buffer, offset, xmlReader);
+				break;
+			case "see":
+				result = DeserializeSeeAttributes (buffer, offset, xmlReader);
+				break;
+			default:
+				break;
+		}
+		
+		return result;
+	}
+	
+	private static int DeserializeTypeAttributes (TextBuffer buffer, int offset, XmlTextReader xmlReader)
+	{
+		string tagPrefix = xmlReader.Name + ":";
+		
+		TextIter insertAt = buffer.GetIterAtOffset (offset);
+		while (xmlReader.MoveToNextAttribute ()) {
+			string tagName = tagPrefix + xmlReader.Name;
+			buffer.InsertWithTagsByName (ref insertAt, xmlReader.Value, tagName);
+			buffer.Insert (ref insertAt, "\n");
+		}
+		
+		return insertAt.Offset;
+	}
+	
+	private static int DeserializeTypeSignatureAttributes (TextBuffer buffer, int offset, XmlTextReader xmlReader)
+	{
+		string tagPrefix = xmlReader.Name + ":";
+		
+		TextIter insertAt = buffer.GetIterAtOffset (offset);
+		while (xmlReader.MoveToNextAttribute ()) {
+			string tagName = tagPrefix + xmlReader.Name;
+			buffer.InsertWithTagsByName (ref insertAt, xmlReader.Value, tagName);
+			buffer.Insert (ref insertAt, "\n");
+		}
+		
+		return insertAt.Offset;
+	}
+	
+	private static int DeserializeLinkAttributes (TextBuffer buffer, int offset, XmlTextReader xmlReader)
+	{
+		string tagPrefix = xmlReader.Name + ":";
+		
+		TextIter insertAt = buffer.GetIterAtOffset (offset);
+		while (xmlReader.MoveToNextAttribute ()) {
+			string tagName = tagPrefix + xmlReader.Name;
+			buffer.InsertWithTagsByName (ref insertAt, xmlReader.Value, tagName);
+			buffer.Insert (ref insertAt, " ");
+		}
+		
+		return insertAt.Offset;
+	}
+	
+	private static int DeserializeSeeAttributes (TextBuffer buffer, int offset, XmlTextReader xmlReader)
+	{
+		string tagPrefix = xmlReader.Name + ":";
+		
+		TextIter insertAt = buffer.GetIterAtOffset (offset);
+		while (xmlReader.MoveToNextAttribute ()) {
+			string tagName = tagPrefix + xmlReader.Name;
+			buffer.InsertWithTagsByName (ref insertAt, xmlReader.Value, tagName);
+			buffer.Insert (ref insertAt, " ");
+		}
+		
+		return insertAt.Offset;
+	}
 }
 }
