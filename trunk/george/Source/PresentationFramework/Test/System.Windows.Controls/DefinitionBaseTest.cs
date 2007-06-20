59fd7683354c0f0691be87af2a963a08329eb2b3
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-20 23:41:28 +0000

    2007-06-21  George Giolfan  <georgegiolfan@yahoo.com>
    	* GridTest.cs: Added or expanded a few tests.
    	* DefinitionBaseTest.cs: Added.

---
+++ trunk/george/Source/PresentationFramework/Test/System.Windows.Controls/DefinitionBaseTest.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+#if Implementation
+using System;
+namespace Mono.System.Windows.Controls {
+#else
+namespace System.Windows.Controls {
+#endif
+	[TestFixture]
+	public class DefinitionBaseTest {
+		[Test]
+		public void SharedSizeGroup() {
+			ColumnDefinition c = new ColumnDefinition();
+			Assert.IsNull(c.SharedSizeGroup, "1");
+			try {
+				c.SharedSizeGroup = string.Empty;
+				Assert.Fail("2");
+			} catch (ArgumentException) {
+			}
+			try {
+				c.SharedSizeGroup = " ";
+				Assert.Fail("3");
+			} catch (ArgumentException) {
+			}
+			try {
+				c.SharedSizeGroup = ".";
+				Assert.Fail("4");
+			} catch (ArgumentException) {
+			}
+			try {
+				c.SharedSizeGroup = "1";
+				Assert.Fail("5");
+			} catch (ArgumentException) {
+			}
+			c.SharedSizeGroup = "_";
+			c.SharedSizeGroup = "A";
+			c.SharedSizeGroup = "A1";
+			c.SharedSizeGroup = null;
+			Assert.IsNotNull(DefinitionBase.SharedSizeGroupProperty.ValidateValueCallback, "6");
+		}
+	}
+}
