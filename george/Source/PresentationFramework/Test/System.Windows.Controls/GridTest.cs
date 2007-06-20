513017a0889e9582bf59846dadea19b8287ccad7
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-20 23:41:28 +0000

    2007-06-21  George Giolfan  <georgegiolfan@yahoo.com>
    	* GridTest.cs: Added or expanded a few tests.
    	* DefinitionBaseTest.cs: Added.

--- george/Source/PresentationFramework/Test/System.Windows.Controls/GridTest.cs
+++ george/Source/PresentationFramework/Test/System.Windows.Controls/GridTest.cs
@@ -21,7 +21,9 @@ namespace System.Windows.Controls {
 			public MeasureGrid() {
 				Children.Add(new TestButton());
 				Measure(new Size(100, 100));
-				Assert.AreEqual(measure_constraint.Width, 100);
+				Assert.AreEqual(measure_constraint.Width, 100, "1");
+				Assert.AreEqual(DesiredSize.Width, Utility.GetEmptyButtonSize(), "2");
+				Assert.AreEqual(DesiredSize.Height, Utility.GetEmptyButtonSize(), "2");
 			}
 
 			class TestButton : Button {
@@ -123,9 +125,54 @@ namespace System.Windows.Controls {
 			g.ColumnDefinitions.Add(new ColumnDefinition());
 			g.Children.Add(new Button());
 			g.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+			//LAMESPEC
 			Assert.AreEqual(c.ActualWidth, 0, "1");
 			g.Arrange(new Rect(0, 0, 100, 100));
 			Assert.AreEqual(c.ActualWidth, Utility.GetEmptyButtonSize(), "2");
+			g.RowDefinitions.Add(new RowDefinition());
+			//LAMESPEC
+			Assert.AreEqual(c.ActualWidth, Utility.GetEmptyButtonSize(), "3");
+			g.ColumnDefinitions.Add(new ColumnDefinition());
+			Assert.AreEqual(c.ActualWidth, 0, "4");
 		}
+
+		[Test]
+		public void Button() {
+			Grid g = new Grid();
+			g.RowDefinitions.Add(new RowDefinition());
+			global::System.Windows.Controls.Button b = new global::System.Windows.Controls.Button();
+			g.Children.Add(b);
+			g.Measure(new Size(Utility.GetEmptyButtonSize() / 2, Utility.GetEmptyButtonSize() / 2));
+			g.Arrange(new Rect(0, 0, Utility.GetEmptyButtonSize() / 2, Utility.GetEmptyButtonSize() / 2));
+			Assert.AreEqual(b.ActualWidth, Utility.GetEmptyButtonSize() / 2, "1");
+			Assert.AreEqual(b.ActualHeight, Utility.GetEmptyButtonSize() / 2, "2");
+		}
+
+		#region Arrange
+		[Test]
+		public void Arrange() {
+			new ArrangeGrid();
+		}
+
+		class ArrangeGrid : Grid {
+			Size ArrangeFinalSize;
+
+			public ArrangeGrid() {
+				global::System.Windows.Controls.Button b = new global::System.Windows.Controls.Button();
+				Children.Add(b);
+				RowDefinitions.Add(new RowDefinition());
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				w.Height = 0;
+				Assert.AreEqual(ArrangeFinalSize.Height, 0);
+			}
+
+			protected override Size ArrangeOverride(Size finalSize) {
+				ArrangeFinalSize = finalSize;
+				return base.ArrangeOverride(finalSize);
+			}
+		}
+		#endregion
 	}
 }
