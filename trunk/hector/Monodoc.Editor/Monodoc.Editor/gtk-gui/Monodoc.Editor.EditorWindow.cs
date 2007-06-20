9e9d0e2d5d0eac2b9574f9ad16f1058fcc3d2b04
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-20 04:18:42 +0000

    * EditorWindow.cs: Testing Serialize method. * gtk-gui/Monodoc.Editor.EditorWindow.cs, gtk-gui/gui.stetic: Added a   toolbar to the main window. * gtk-gui/Monodoc.Editor.OpenDocDialog.cs: Minor change.

--- trunk/hector/Monodoc.Editor/Monodoc.Editor/gtk-gui/Monodoc.Editor.EditorWindow.cs
+++ trunk/hector/Monodoc.Editor/Monodoc.Editor/gtk-gui/Monodoc.Editor.EditorWindow.cs
@@ -23,6 +23,8 @@ namespace Monodoc.Editor {
         
         private Gtk.MenuBar menubar1;
         
+        private Gtk.Toolbar toolbar1;
+        
         private Gtk.ScrolledWindow scrolledwindow2;
         
         private Monodoc.Editor.Gui.DocumentEditor docEditView;
@@ -34,7 +36,7 @@ namespace Monodoc.Editor {
             // Widget Monodoc.Editor.EditorWindow
             Gtk.UIManager w1 = new Gtk.UIManager();
             Gtk.ActionGroup w2 = new Gtk.ActionGroup("Default");
-            this.File = new Gtk.Action("File", Mono.Unix.Catalog.GetString("File"), null, null);
+            this.File = new Gtk.Action("File", Mono.Unix.Catalog.GetString("_File"), null, null);
             this.File.ShortLabel = Mono.Unix.Catalog.GetString("File");
             w2.Add(this.File, null);
             this.Quit = new Gtk.Action("Quit", Mono.Unix.Catalog.GetString("_Quit"), null, "gtk-quit");
@@ -51,7 +53,7 @@ namespace Monodoc.Editor {
             // Container child Monodoc.Editor.EditorWindow.Gtk.Container+ContainerChild
             this.vbox1 = new Gtk.VBox();
             this.vbox1.Name = "vbox1";
-            this.vbox1.Spacing = 6;
+            this.vbox1.Spacing = 3;
             // Container child vbox1.Gtk.Box+BoxChild
             w1.AddUiFromString("<ui><menubar name='menubar1'><menu action='File'><menuitem action='Open'/><menuitem action='Quit'/></menu></menubar></ui>");
             this.menubar1 = ((Gtk.MenuBar)(w1.GetWidget("/menubar1")));
@@ -61,6 +63,18 @@ namespace Monodoc.Editor {
             w3.Position = 0;
             w3.Expand = false;
             w3.Fill = false;
+            // Container child vbox1.Gtk.Box+BoxChild
+            w1.AddUiFromString("<ui><toolbar name='toolbar1'><toolitem action='Open'/></toolbar></ui>");
+            this.toolbar1 = ((Gtk.Toolbar)(w1.GetWidget("/toolbar1")));
+            this.toolbar1.Name = "toolbar1";
+            this.toolbar1.ShowArrow = false;
+            this.toolbar1.ToolbarStyle = ((Gtk.ToolbarStyle)(0));
+            this.toolbar1.IconSize = ((Gtk.IconSize)(2));
+            this.vbox1.Add(this.toolbar1);
+            Gtk.Box.BoxChild w4 = ((Gtk.Box.BoxChild)(this.vbox1[this.toolbar1]));
+            w4.Position = 1;
+            w4.Expand = false;
+            w4.Fill = false;
             // Container child vbox1.Gtk.Box+BoxChild
             this.scrolledwindow2 = new Gtk.ScrolledWindow();
             this.scrolledwindow2.CanFocus = true;
@@ -75,17 +89,18 @@ namespace Monodoc.Editor {
             this.docEditView.Editable = false;
             this.scrolledwindow2.Add(this.docEditView);
             this.vbox1.Add(this.scrolledwindow2);
-            Gtk.Box.BoxChild w5 = ((Gtk.Box.BoxChild)(this.vbox1[this.scrolledwindow2]));
-            w5.Position = 1;
+            Gtk.Box.BoxChild w6 = ((Gtk.Box.BoxChild)(this.vbox1[this.scrolledwindow2]));
+            w6.Position = 2;
             // Container child vbox1.Gtk.Box+BoxChild
             this.statusbar1 = new Gtk.Statusbar();
             this.statusbar1.Name = "statusbar1";
             this.statusbar1.Spacing = 6;
+            this.statusbar1.HasResizeGrip = false;
             this.vbox1.Add(this.statusbar1);
-            Gtk.Box.BoxChild w6 = ((Gtk.Box.BoxChild)(this.vbox1[this.statusbar1]));
-            w6.Position = 2;
-            w6.Expand = false;
-            w6.Fill = false;
+            Gtk.Box.BoxChild w7 = ((Gtk.Box.BoxChild)(this.vbox1[this.statusbar1]));
+            w7.Position = 3;
+            w7.Expand = false;
+            w7.Fill = false;
             this.Add(this.vbox1);
             if ((this.Child != null)) {
                 this.Child.ShowAll();
