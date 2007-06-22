01923def1d47e58eb22d1e8969cb56d85349ca5e
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-22 16:55:33 +0000

    * Test/TestDocumentBufferArchiver.cs, Monodoc.Editor.Gui.mdp: Added   Test for DocumentBufferArchiver, added a test for Serialize method. * Test/Examples/WrapMode.xml: Removed all empty elements that we are   not creating for the time being.

---
+++ hector/Monodoc.Editor/Monodoc.Editor.Gui/Test/TestDocumentBufferArchiver.cs
@@ -0,0 +1,60 @@
+//
+// TestDocumentBufferArchiver.cs: Test unit for DocumentBufferArchiver class.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+
+using Gtk;
+using System;
+using System.IO;
+using NUnit.Framework;
+using Monodoc.Editor.Utils;
+
+namespace Monodoc.Editor.Gui {
+[TestFixture()]
+public class TestDocumentBufferArchiver {
+	
+	private string pathTest;
+	private TextBuffer buffer;
+	
+	[SetUp()]
+	public void Initialize ()
+	{
+		string filePath;
+		
+		filePath = Test.PathOfTestFiles ();
+		pathTest = Path.Combine (filePath, "Examples");
+		Application.Init ();
+		DocumentEditor editor = new DocumentEditor ();
+		buffer = editor.Buffer;
+	}
+	
+	[TearDown()]
+	public void Dispose ()
+	{
+		Application.Quit ();
+	}
+	
+	[Test()]
+	public void Serialize()
+	{
+		string originalText, newText, fileName;
+		
+		fileName = Path.Combine (pathTest, "WrapMode.xml");
+		MonoDocument document = new MonoDocument (fileName);
+		originalText = document.Text;
+		
+		Console.WriteLine (originalText);
+		
+		DocumentBufferArchiver.Deserialize (buffer, originalText);
+		newText = DocumentBufferArchiver.Serialize (buffer);
+		
+		Console.WriteLine (newText);
+		
+		Assert.AreEqual (originalText, newText, "SR01");
+	}
+	
+}
+}
