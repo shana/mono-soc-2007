2032c1b896c68f66f307ee87d07503eee8f495cd
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-22 13:03:20 +0000

    2007-06-22  George Giolfan  <georgegiolfan@yahoo.com>
    	* ColumnDefinitionTest.cs: Added.

---
+++ trunk/george/Source/PresentationFramework/Test/System.Windows.Controls/ColumnDefinitionTest.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+#if Implementation
+using System.Windows;
+using System.Windows.Controls;
+namespace Mono.System.Windows.Controls {
+#else
+namespace System.Windows.Controls {
+#endif
+	[TestFixture]
+	public class ColumnDefinitionTest {
+		[Test]
+		public void StaticProperties() {
+			FrameworkPropertyMetadata width_metadata = (FrameworkPropertyMetadata)ColumnDefinition.WidthProperty.GetMetadata(typeof(ColumnDefinition));
+			Assert.IsNotNull(width_metadata.PropertyChangedCallback, "1");
+			Assert.IsFalse(width_metadata.AffectsArrange, "2");
+			Assert.IsFalse(width_metadata.AffectsMeasure, "3");
+			Assert.IsFalse(width_metadata.AffectsParentArrange, "4");
+			Assert.IsFalse(width_metadata.AffectsParentMeasure, "5");
+			Assert.IsFalse(width_metadata.AffectsRender, "6");
+		}
+	}
+}
