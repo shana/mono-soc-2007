895fb5955c5d4f9df653e52e8fd120f50ee74743
debackerl <debackerl@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-23 21:37:31 +0000

    * Button.cs: SizeRequest, and Allocation implemented, events hooked.

--- laurent/src/Ribbons/Ribbons/Button.cs
+++ laurent/src/Ribbons/Ribbons/Button.cs
@@ -7,11 +7,19 @@ namespace Ribbons
 	public class Button : Bin
 	{
 		private GroupStyle groupStyle;
+		private bool isHot = false;
+		
+		protected double lineWidth = 1.0;
 		
 		public GroupStyle GroupStyle
 		{
 			set { groupStyle = value; }
 			get { return groupStyle; }
+		}
+		
+		public bool IsHot
+		{
+			get { return isHot; }
 		}
 		
 		public string Label
@@ -24,11 +32,16 @@ namespace Ribbons
 			}
 		}
 		
-		public Button()
+		public Button ()
 		{
 			this.SetFlag (WidgetFlags.NoWindow);
 			
 			this.AddEvents ((int)(Gdk.EventMask.ButtonPressMask | Gdk.EventMask.ButtonReleaseMask | Gdk.EventMask.PointerMotionMask));
+		}
+		
+		public Button (string Label) : this()
+		{
+			this.Label = Label;
 		}
 		
 		protected override void OnSizeRequested (ref Requisition requisition)
@@ -39,11 +52,18 @@ namespace Ribbons
 			{
 				Child.GetSizeRequest (out requisition.Width, out requisition.Height);
 			}
+			requisition.Height += (int)(lineWidth * 4);
+			requisition.Width += (int)(lineWidth * 4);
 		}
 		
 		protected override void OnSizeAllocated (Gdk.Rectangle allocation)
 		{
 			base.OnSizeAllocated (allocation);
+			
+			allocation.X += (int)(lineWidth * 2);
+			allocation.Y += (int)(lineWidth * 2);
+			allocation.Height -= (int)(lineWidth * 4);
+			allocation.Width -= (int)(lineWidth * 4);
 			
 			if(Child != null && Child.Visible)
 			{
@@ -64,7 +84,28 @@ namespace Ribbons
 		
 		protected void Draw (Context cr)
 		{
+			
+		}
 		
+		protected override bool OnButtonPressEvent (Gdk.EventButton evnt)
+		{
+			return base.OnButtonPressEvent (evnt);
+		}
+		
+		protected override bool OnEnterNotifyEvent (Gdk.EventCrossing evnt)
+		{
+			bool ret = base.OnEnterNotifyEvent (evnt);
+			isHot = true;
+			this.QueueDraw ();
+			return ret;
+		}
+		
+		protected override bool OnLeaveNotifyEvent (Gdk.EventCrossing evnt)
+		{
+			bool ret = base.OnLeaveNotifyEvent (evnt);
+			isHot = false;
+			this.QueueDraw ();
+			return ret;
 		}
 	}
 }
