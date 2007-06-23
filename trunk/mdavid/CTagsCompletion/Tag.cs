dfb4a5a776952aa4e9db21144c11ccd016a0bf36
MarcosMarin <MarcosMarin@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-23 02:07:50 +0000

    * Tag.cs, CTagsProjectServiceExtension.cs, CTagsProject.cs,   CTagsTextEditorExtension.cs: The project that subclasses   CTagsProject is now responsible for adding the logic about which   tags should be added to the completion list window.

--- trunk/mdavid/CTagsCompletion/Tag.cs
+++ trunk/mdavid/CTagsCompletion/Tag.cs
@@ -39,9 +39,9 @@ namespace CTagsCompletion
 		private string name;
 		private string file;
 		private string pattern;
-		private Dictionary<string, string> tagField;
+		private List<string[]> tagField;
 		
-		public Tag (string name, string file, string pattern, Dictionary<string, string> tagField)
+		public Tag (string name, string file, string pattern, List<string[]> tagField)
 		{
 			this.name = name;
 			this.file = file;
@@ -51,11 +51,11 @@ namespace CTagsCompletion
 		
 		public static Tag CreateTag (string tagEntry)
 		{
-			// At this point it only loads the tag's file and pattern
 			int i1, i2;
 			string file;
 			string pattern;
 			string name;
+			List<string[]> tagField = new List<string[]> ();
 			char delimiter;
 			
 			name = tagEntry.Substring (0, tagEntry.IndexOf ('\t'));
@@ -72,7 +72,31 @@ namespace CTagsCompletion
 			
 			pattern = tagEntry.Substring (i1 + 1, i2 - i1 - 1);
 			
-			return new Tag (name, file, pattern, null);
+			string substrTagField = tagEntry.Substring (tagEntry.IndexOf (";\"\t") + 3);
+			
+			foreach (string field in substrTagField.Split ('\t'))
+				tagField.Add (field.Split (':'));
+			
+			return new Tag (name, file, pattern, tagField);
+		}
+		
+		public override string ToString ()
+		{
+			System.Text.StringBuilder builder = new System.Text.StringBuilder ();
+			
+			builder.Append (string.Format ("{0}\t{1}\t/^{2}$/;\"", name, file, pattern));
+			
+			foreach (string[] keyval in tagField) {
+				builder.Append ("\t");
+				for (int i = 0; i < keyval.Length; i++) {
+					builder.Append (keyval[i]);
+					
+					if (i != keyval.Length - 1)
+						builder.Append (":");
+				}
+			}
+			
+			return builder.ToString ();
 		}
 		
 		public string File {
@@ -83,7 +107,7 @@ namespace CTagsCompletion
 			get { return pattern; }
 		}
 		
-		public virtual Dictionary<string, string> TagField {
+		public virtual List<string[]> TagField {
 			get { return tagField; }
 		}
 
