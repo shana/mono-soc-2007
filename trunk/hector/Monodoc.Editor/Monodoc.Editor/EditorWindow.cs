6f238108d8f3f3b8f7f641048f4060168aaccbcb
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-24 19:01:53 +0000

    * EditorWindow.cs: Changed the handling of the OnSaveAs and OnOpen   events for the EditorWindow. * gtk-gui/Monodoc.Editor.SaveDocDialog.cs, gtk-gui/gui.stetic,   SaveDocDialog.cs: Added basic handlers for events in SaveDocDialog. * OpenDocDialog.cs: Using Respond method to send result of the dialog.

--- trunk/hector/Monodoc.Editor/Monodoc.Editor/EditorWindow.cs
+++ trunk/hector/Monodoc.Editor/Monodoc.Editor/EditorWindow.cs
@@ -7,8 +7,9 @@
 // (C) 2007 Hector E. Gomez M
 //
 
-using System;
 using Gtk;
+using System;
+using System.IO;
 using Monodoc.Editor.Gui;
 using Monodoc.Editor.Utils;
 
@@ -33,27 +34,35 @@ public partial class EditorWindow : Gtk.Window {
 	private void OnOpenActivated(object sender, System.EventArgs e)
 	{
 		OpenDocDialog dialog = new OpenDocDialog ();
-		dialog.Run ();
-		
-		try {
-			MonoDocument doc = new MonoDocument (dialog.Document);
-			DocumentBufferArchiver.Deserialize (docEditView.Buffer, doc.Text);
-		} catch (ArgumentException argexp) {
-			Console.WriteLine (argexp.Message);
+		if (dialog.Run () == (int) ResponseType.Ok) {
+			try {
+				MonoDocument doc = new MonoDocument (dialog.Document);
+				DocumentBufferArchiver.Deserialize (docEditView.Buffer, doc.Text);
+			} catch (ArgumentException argexp) {
+				Console.WriteLine (argexp.Message);
+			}
 		}
+		
+		dialog.Destroy ();
 	}
 
 	private void OnSaveAsActivated (object sender, System.EventArgs e)
 	{
 		SaveDocDialog dialog = new SaveDocDialog ();
-		string xml = DocumentBufferArchiver.Serialize (docEditView.Buffer);
-		Console.WriteLine ("Serialize: \n" + xml);
+		if (dialog.Run () == (int) ResponseType.Ok) {
+			using (FileStream fileStream = new FileStream (dialog.Document, FileMode.CreateNew)) {
+				using (StreamWriter streamWriter = new StreamWriter (fileStream)) {
+					streamWriter.Write (DocumentBufferArchiver.Serialize (docEditView.Buffer));
+				}
+			}
+		}
+		
+		dialog.Destroy ();
 	}
 
 	private void OnSaveActivated (object sender, System.EventArgs e)
 	{
-		string xml = DocumentBufferArchiver.Serialize (docEditView.Buffer);
-		Console.WriteLine ("Serialize: \n" + xml);
+		Console.WriteLine ("Serialize: \n{0}", DocumentBufferArchiver.Serialize (docEditView.Buffer));
 	}
 }
 }
