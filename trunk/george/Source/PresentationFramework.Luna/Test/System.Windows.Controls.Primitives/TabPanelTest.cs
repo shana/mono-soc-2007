5bcf59f89cf763621c4a147f8a9cf47c526d7479
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-25 10:23:27 +0000

    2007-06-25  George Giolfan  <georgegiolfan@yahoo.com>
    	* TabPanelTest.cs: Added.

---
+++ trunk/george/Source/PresentationFramework.Luna/Test/System.Windows.Controls.Primitives/TabPanelTest.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using System.Windows.Media;
+#if Implementation
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+namespace Mono.System.Windows.Controls.Primitives {
+#else
+namespace System.Windows.Controls.Primitives {
+#endif
+	[TestFixture]
+	public class TabPanelTest {
+		#region MeasureAuto
+		[Test]
+		public void MeasureAuto() {
+			new MeasureAutoTabPanel();
+		}
+
+		class MeasureAutoTabPanel : TabPanel {
+			static bool called;
+			static Size measure_constraint;
+			static Size measure_result;
+
+			public MeasureAutoTabPanel() {
+				Children.Add(new TestTabItem());
+				Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+				Assert.IsTrue(called, "1");
+				Assert.IsTrue(double.IsPositiveInfinity(measure_constraint.Width), "2");
+				Assert.IsTrue(double.IsPositiveInfinity(measure_constraint.Height), "3");
+				Assert.AreEqual(measure_result.Width, 27, "4");
+				Assert.AreEqual(measure_result.Height, 5, "5");
+				Assert.AreEqual(DesiredSize.Width, 27, "6");
+				Assert.AreEqual(DesiredSize.Height, 5, "7");
+			}
+
+			class TestTabItem : global::System.Windows.Controls.TabItem {
+				protected override Size MeasureOverride(Size constraint) {
+					called = true;
+					return measure_result = base.MeasureOverride(measure_constraint = constraint);
+				}
+			}
+		}
+		#endregion
+
+		#region Measure
+		[Test]
+		public void Measure() {
+			new MeasureTabPanel();
+		}
+
+		class MeasureTabPanel : TabPanel {
+			static bool called;
+			static Size measure_constraint;
+			static Size measure_result;
+
+			public MeasureTabPanel() {
+				Children.Add(new TestTabItem());
+				Measure(new Size(100, 100));
+				Assert.IsTrue(called, "1");
+				Assert.AreEqual(measure_constraint.Width, 100, "2");
+				Assert.AreEqual(measure_constraint.Height, 100, "3");
+				Assert.AreEqual(measure_result.Width, 27, "4");
+				Assert.AreEqual(measure_result.Height, 5, "5");
+				Assert.AreEqual(DesiredSize.Width, 27, "6");
+				Assert.AreEqual(DesiredSize.Height, 5, "7");
+			}
+
+			class TestTabItem : global::System.Windows.Controls.TabItem {
+				protected override Size MeasureOverride(Size constraint) {
+					called = true;
+					return measure_result = base.MeasureOverride(measure_constraint = constraint);
+				}
+			}
+		}
+		#endregion
+
+		[Test]
+		public void LimitedWidth() {
+			TabPanel t = new TabPanel();
+			for (int i = 0; i < 5; i++) {
+				TabItem tab_item = new TabItem();
+				tab_item.Width = 100;
+				t.Children.Add(tab_item);
+			}
+			t.Measure(new Size(100, double.PositiveInfinity));
+			Assert.AreEqual(t.DesiredSize.Width, 100, "1");
+			Assert.AreEqual(t.DesiredSize.Height, 25, "2");
+		}
+	}
+}
