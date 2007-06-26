a094d1ad75e090fbfbb52f2ad49a0a2d3775847f
MarcosMarin <MarcosMarin@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-26 02:22:59 +0000

    * Navigation/Tag.cs, Navigation/Namespace.cs,   Navigation/ProjectNodeBuilderExtension.cs, CBinding.addin.xml:   Started work on code navigation.

---
+++ mdavid/CBinding/Navigation/ProjectNodeBuilderExtension.cs
@@ -0,0 +1,84 @@
+//
+// ProjectNodeBuilderExtension.cs
+//
+// Authors:
+//   Marcos David Marin Amador <MarcosMarin@gmail.com>
+//
+// Copyright (C) 2007 Marcos David Marin Amador
+//
+//
+// This source code is licenced under The MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+using System.Text;
+
+using Mono.Addins;
+
+using MonoDevelop.Projects;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Ide.Gui.Pads;
+using MonoDevelop.Core;
+using MonoDevelop.Core.Execution;
+
+using CBinding;
+
+namespace CBinding.Navigation
+{
+	public class ProjectNodeBuilderExtension : NodeBuilderExtension
+	{
+		public override bool CanBuildNode (Type dataType)
+		{
+			return typeof(CProject).IsAssignableFrom (dataType);
+		}
+		
+		public override void BuildChildNodes (ITreeBuilder builder, object dataObject)
+		{
+			CProject p = dataObject as CProject;
+			
+			if (p == null) return;
+			
+			CreateTagsDatabase (p);
+		}
+		
+		private void CreateTagsDatabase (CProject project)
+		{
+			string tagsDir = Path.Combine (project.BaseDirectory, ".tags");
+			
+			if (!Directory.Exists (tagsDir))
+				Directory.CreateDirectory (tagsDir);
+			
+			StringBuilder args = new StringBuilder ("--fields=+a --language-force=C++");
+			
+			foreach (ProjectFile file in project.ProjectFiles) 
+				args.AppendFormat (" {0}", file.Name);
+			
+			try {
+				ProcessWrapper p = Runtime.ProcessService.StartProcess ("ctags", args.ToString (), tagsDir, null);
+				p.WaitForExit ();
+			} catch (Exception ex) {
+				IdeApp.Services.MessageService.ShowError (ex, "Could not create tags database (You must have exuberant ctags installed).");
+			}
+		}
+	}
+}
