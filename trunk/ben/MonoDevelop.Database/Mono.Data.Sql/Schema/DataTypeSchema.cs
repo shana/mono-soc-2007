f644ac5502cbb1e08d4ac6474d67f29cfe24fc17
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-27 20:52:50 +0000

    2007-06-26  Ben Motmans  <ben.motmans@gmail.com>
    	* Providers/SqlSchemaProvider.cs: use Convert.IsDbNull instead of try-catch
    	* Providers/SqlSchemaProvider.cs: fixed sql bug in GetTableColumns method
    	* Providers/SqlSchemaProvider.cs: implemented GetDataType method
    	* Schema/DataTypeCategory.cs, Schema/DataTypeSchema.cs: added an enumeration to specify the category
    	of the data type, this can be used in the future for data migration
    
    2007-06-25  Ben Motmans  <ben.motmans@gmail.com>
    
    	* Schema/TableSchema.cs, Schema/ViewSchema.cs, Schema/ProcedureSchema.cs, Schema/ColumnsSchema.cs,
    	Schema/ConstraintSchema.cs: lazy loaded columns, parameters, definition, ... parameters
    	* Providers/SqlSchemaProvider.cs: updated sql statements to allow lazy loading
    	* Providers/SqlSchemaProvider.cs: implemeted GetDatabases method
    	* Providers/SqlSchemaProvider.cs: implemented GetConstraints method
    	* Schema/Range.cs, Schema/DataTypeSchema.cs: length, precision and scale range properties
    
    2007-06-24  Ben Motmans  <ben.motmans@gmail.com>
    
    	* Creation of designer, visualisation and query sub-projects
    	* Providers/ConnectionSettings.cs: class to seperate connection settings from the connection provider
    
    2007-06-23  Ben Motmans  <ben.motmans@gmail.com>
    
    	* Providers/IDbProvider.cs: splitted into Providers/ISchemaProvider.cs and Providers/IConnectionProvider.cs
    	* Providers/ISchemaProvider.cs: Use of generics where possible and logic
    	* Providers/ISchemaProvider.cs: Added GetDatabases method
    
    2007-06-22  Ben Motmans  <ben.motmans@gmail.com>
    
    	* Imported code from MonoQuery

---
+++ trunk/ben/MonoDevelop.Database/Mono.Data.Sql/Schema/DataTypeSchema.cs
@@ -0,0 +1,119 @@
+//
+// Authors:
+//	Christian Hergert  <chris@mosaix.net>
+//	Ben Motmans  <ben.motmans@gmail.com>
+//
+// Copyright (C) 2005 Mosaix Communications, Inc.
+// Copyright (c) 2007 Ben Motmans
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+
+namespace Mono.Data.Sql
+{
+	public class DataTypeSchema : AbstractSchema
+	{
+		protected bool isComplex = false;
+		
+		protected DataTypeCategory category;
+
+		protected Range lengthRange;
+		protected Range precisionRange;
+		protected Range scaleRange;
+		
+		public DataTypeSchema (ISchemaProvider schemaProvider)
+			: base (schemaProvider)
+		{
+		}
+		
+		public DataTypeCategory DataTypeCategory {
+			get { return category; }
+			set {
+				if (category != value) {
+					category = value;
+					OnChanged ();
+				}
+			}
+		}
+
+		public bool HasRange {
+			get { return precisionRange != null; }
+		}
+		
+		public bool HasScale {
+			get { return scaleRange != null; }
+		}
+
+		public Range LengthRange {
+			get { return lengthRange; }
+			set {
+				if (lengthRange != value) {
+					lengthRange = value;
+					OnChanged ();
+				}
+			}
+		}
+		
+		public Range PrecisionRange {
+			get { return precisionRange; }
+			set {
+				if (precisionRange != value) {
+					precisionRange = value;
+					OnChanged ();
+				}
+			}
+		}
+		
+		public Range ScaleRange {
+			get { return scaleRange; }
+			set {
+				if (scaleRange != value) {
+					scaleRange = value;
+					OnChanged ();
+				}
+			}
+		}
+		
+		public bool IsComplex {
+			get {
+				return isComplex;
+			}
+			set {
+				if (isComplex != value) {
+					isComplex = value;
+					OnChanged();
+				}
+			}
+		}
+		
+		public ICollection<ColumnSchema> Columns {
+			get {
+				if (isComplex == false) {
+					return new List<ColumnSchema> ();
+				} else {
+					// TODO: Get complex columns from the provider
+					throw new NotImplementedException();
+				}
+			}
+		}
+	}
+}
---
+++ trunk/ben/MonoDevelop.Database/Mono.Data.Sql/Schema/ProcedureSchema.cs
@@ -0,0 +1,104 @@
+//
+// Authors:
+//	Christian Hergert  <chris@mosaix.net>
+//	Ben Motmans  <ben.motmans@gmail.com>
+//
+// Copyright (C) 2005 Mosaix Communications, Inc.
+// Copyright (c) 2007 Ben Motmans
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Collections.Generic;
+
+namespace Mono.Data.Sql
+{
+	public class ProcedureSchema : AbstractSchema
+	{
+		protected ICollection<ParameterSchema> parameters;
+		protected ICollection<ColumnSchema> columns;
+		protected string language = String.Empty;
+		protected bool isSystemProcedure = false;
+		
+		public ProcedureSchema (ISchemaProvider schemaProvider)
+			: base (schemaProvider)
+		{
+		}
+		
+		public ICollection<ParameterSchema> Parameters {
+			get {
+				if (parameters == null)
+					parameters = provider.GetProcedureParameters (this);
+				return parameters;
+			}
+		}
+		
+		public ICollection<ColumnSchema> Columns {
+			get {
+				if (columns == null)
+					columns = provider.GetProcedureColumns (this);
+				return columns;
+			}
+		}
+		
+		public override string Definition {
+			get {
+				if (definition == null)
+					definition = provider.GetProcedureDefinition (this);
+				return definition;
+			}
+			set { base.Definition = value; }
+		}
+		
+		public LanguageSchema Language {
+			get {
+				throw new NotImplementedException();
+			}
+			set {
+				language = value.FullName;
+			}
+		}
+		
+		public string LanguageName {
+			get {
+				return language;
+			}
+			set {
+				if (language != null) {
+					language = value;
+					OnChanged ();
+				}
+			}
+		}
+		
+		public bool IsSystemProcedure {
+			get {
+				return isSystemProcedure;
+			}
+			set {
+				if (isSystemProcedure != null) {
+					isSystemProcedure = value;
+					OnChanged ();
+				}
+			}
+		}
+	}
+}
