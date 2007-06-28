93a83c75315ec511d4da52f48e0694e6348fdcc2
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-06-28 06:01:27 +0000

    * Monodoc.Editor.Gui.mdp, DocumentTab.cs: Added DocumentTab that   represents a "Tab" ie a ScrolledWindow with an associated   DocumentEditor. * DocumentEditor.cs: Added a helper method to initialize several   properties for the DocumentEditor.

---
+++ hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentTab.cs
@@ -0,0 +1,60 @@
+//
+// DocumentTab.cs: ScrolledWindow based class that represents a tab in notebook that contains a document.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+//
+
+using System;
+using Gtk;
+
+namespace Monodoc.Editor.Gui {
+public class DocumentTab : Gtk.ScrolledWindow {
+	private DocumentEditor editor;
+	private Label title_label;
+	string title;
+	
+	public DocumentTab () : base ()
+	{
+		InitializeProperties ();
+		
+		editor = new DocumentEditor ();
+		this.Add (editor);
+		
+		title = "Untitled";
+		title_label = new Label (title);
+	}
+	
+	private void InitializeProperties ()
+	{
+		this.CanFocus = true;
+		this.VscrollbarPolicy = PolicyType.Automatic;
+		this.HscrollbarPolicy = PolicyType.Automatic;
+		this.ShadowType = ((ShadowType) 1);
+	}
+	
+	public Label TitleLabel {
+		get {
+			return title_label;
+		}
+	}
+	
+	public string Title {
+		get {
+			return title;
+		}
+		
+		set {
+			title = title_label.Text = value;
+		}
+	}
+	
+	public TextBuffer Buffer {
+		get {
+			return editor.Buffer;
+		}
+	}
+}
+}
