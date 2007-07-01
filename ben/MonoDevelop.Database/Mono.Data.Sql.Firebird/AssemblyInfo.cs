c1ffe60a3fb3319ed26de3c4c719cf26457a9f47
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-01 22:04:46 +0000

    2007-07-01  Ben Motmans <ben.motmans@gmail.com>
    	* FirebirdSchemaProvider.cs: 
    	* FirebirdDbFactory.cs: 
    	* FirebirdConnectionProvider.cs: 
    	Imported original code and splitted in 3 classes
    	Use of custom sql queries instead of GetSchema method to get more information
    	Implementation of GetViewColumns, GetProcedures, GetProcedureColumns, GetProcedureParameters,
    	GetUsers and GetTriggers methods

---
+++ ben/MonoDevelop.Database/Mono.Data.Sql.Firebird/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Mono.Data.Sql.Firebird")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("http://www.monodevelop.com")]
+[assembly: AssemblyCopyright("MIT X11")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("@ASSEMBLY_VERSION@")]
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+
+[assembly: Mono.About ("http://www.monodevelop.com")]
+[assembly: Mono.UsageComplement ("")]
+[assembly: Mono.Author ("Christian Hergert, Daniel Morgan, Ben Motmans")]
