0a8aca9dc4e8b909dfef3937fe2ecc5efacd2008
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-01 22:08:47 +0000

    2007-07-01  Ben Motmans <ben.motmans@gmail.com>
    	* MySqlSchemaProvider.cs: 
    	* MySqlDbFactory.cs: 
    	* MySqlConnectionProvider.cs: 
    	Imported original code and splitted in 3 classes
    	GetDatabases, GetTableConstraints, GetViews, GetProcedures, GetViewColumns, GetProcedureColumns implementation
    	improved support for MySql 5.x for the GetTables method
    	updated sql statements to allow lazy loading

---
+++ ben/MonoDevelop.Database/Mono.Data.Sql.MySql/MySqlConnectionProvider.cs
@@ -0,0 +1,72 @@
+﻿//
+// Authors:
+//	Christian Hergert  <chris@mosaix.net>
+//	Daniel Morgan <danielmorgan@verizon.net>
+//	Sureshkumar T <tsureshkumar@novell.com>
+//	Ben Motmans  <ben.motmans@gmail.com>
+//
+// Copyright (C) 2005 Mosaix Communications, Inc.
+// Copyright (c) 2007 Ben Motmans
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+using System.Data;
+using ByteFX.Data.MySqlClient;
+using System.Collections.Generic;
+
+namespace Mono.Data.Sql
+{
+	public class MySqlConnectionProvider : AbstractConnectionProvider
+	{
+		public MySqlConnectionProvider (ConnectionSettings settings)
+			: base (settings)
+		{
+		}
+
+		public override bool Open (out string errorMessage)
+		{
+			string connStr = null;
+			try {	
+				if (settings.UseConnectionString) {
+					connStr = settings.ConnectionString;
+				} else {
+					//"Server=Server;Port=1234;Database=Test;Uid=UserName;Pwd=asdasd;Pooling=true;"
+					//Default port is 3306. Enter value -1 to use a named pipe connection. 
+					connStr = String.Format ("Server={0};Port={1};Database={2};Uid={3};Pwd={4};Pooling=true;",
+						settings.Server, settings.Port, settings.Database, settings.Username, settings.Password);
+				}
+				SetConnectionStringParameter (connStr, String.Empty, "pooling", settings.EnablePooling.ToString ());
+				if (settings.EnablePooling) {
+					SetConnectionStringParameter (connStr, String.Empty, "MinPoolSize", settings.MinPoolSize.ToString ());
+					SetConnectionStringParameter (connStr, String.Empty, "MaxPoolSize", settings.MaxPoolSize.ToString ());
+				}
+				connection = new MySqlConnection (connStr);
+				connection.Open ();
+				
+				errorMessage = String.Empty;
+				return true;
+			} catch {
+				errorMessage = String.Format ("Unable to connect. (CS={0})", connStr == null ? "NULL" : connStr);
+				return false;
+			}
+		}
+	}
+}
