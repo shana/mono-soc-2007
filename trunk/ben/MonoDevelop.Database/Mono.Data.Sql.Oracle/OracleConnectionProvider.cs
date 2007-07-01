ab95fb01f80b386b703577f8693eb3e2fe89ab41
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-01 22:11:14 +0000

    2007-07-01  Ben Motmans <ben.motmans@gmail.com>
    	* OracleSchemaProvider.cs: 
    	* OracleDbFactory.cs: 
    	* OracleConnectionProvider.cs: 
    	Imported original code and splitted in 3 classes

---
+++ trunk/ben/MonoDevelop.Database/Mono.Data.Sql.Oracle/OracleConnectionProvider.cs
@@ -0,0 +1,70 @@
+﻿//
+// Authors:
+//   Christian Hergert	<chris@mosaix.net>
+//   Daniel Morgan <danielmorgan@verizon.net>
+//   Ben Motmans  <ben.motmans@gmail.com>
+//
+// Copyright (C) 2005 Mosaix Communications, Inc.
+// Copyright (C) 2005 Daniel Morgan
+// Copyright (c) 2007 Ben Motmans
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+using System;
+using System.Data;
+using System.Data.OracleClient;
+using System.Collections.Generic;
+
+namespace Mono.Data.Sql
+{
+	public class OracleConnectionProvider : AbstractConnectionProvider
+	{
+		public OracleConnectionProvider (ConnectionSettings settings)
+			: base (settings)
+		{
+		}
+
+		public override bool SupportsPooling {
+			get { return false; }
+		}
+
+		public override bool Open (out string errorMessage)
+		{
+			string connStr = null;
+			try {	
+				if (settings.UseConnectionString) {
+					connStr = settings.ConnectionString;
+				} else {
+					//Data Source=MyOracleDB;User Id=myUsername;Password=myPassword;Integrated Security=no; 
+					connStr = String.Format ("Data Source={0};User Id={1};Password={2};Integrated Security=no;",
+						settings.Database, settings.Username, settings.Password);
+				}
+				connection = new OracleConnection (connStr);
+				connection.Open ();
+				
+				errorMessage = String.Empty;
+				return true;
+			} catch {
+				errorMessage = String.Format ("Unable to connect. (CS={0})", connStr == null ? "NULL" : connStr);
+				return false;
+			}
+		}
+	}
+}
