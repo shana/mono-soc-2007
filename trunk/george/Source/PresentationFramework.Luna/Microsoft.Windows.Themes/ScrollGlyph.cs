5171f7df40dc4e2a115ff403e72620cc0b02bf89
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-02 13:23:54 +0000

    2007-07-02  George Giolfan  <georgegiolfan@yahoo.com>
    	* ScrolGllyph.cs, ThemeColor.cs: Added.

---
+++ trunk/george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/ScrollGlyph.cs
@@ -0,0 +1,15 @@
+#if Implementation
+namespace Mono.Microsoft.Windows.Themes {
+#else
+namespace Microsoft.Windows.Themes {
+#endif
+	public enum ScrollGlyph {
+		None,
+		LeftArrow,
+		RightArrow,
+		UpArrow,
+		DownArrow,
+		VerticalGripper,
+		HorizontalGripper
+	}
+}
