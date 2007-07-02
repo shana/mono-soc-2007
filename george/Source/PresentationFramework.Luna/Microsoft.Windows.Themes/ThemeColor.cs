aa3ff17de8cf7443350332874a91c0600b42a82c
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-02 13:23:54 +0000

    2007-07-02  George Giolfan  <georgegiolfan@yahoo.com>
    	* ScrolGllyph.cs, ThemeColor.cs: Added.

---
+++ george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/ThemeColor.cs
@@ -0,0 +1,11 @@
+#if Implementation
+namespace Mono.Microsoft.Windows.Themes {
+#else
+namespace Microsoft.Windows.Themes {
+#endif
+	public enum ThemeColor {
+		NormalColor,
+		Homestead,
+		Metallic
+	}
+}
