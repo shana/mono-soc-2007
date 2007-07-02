90906a724ffa8b11256c525974d8268b04b47cfc
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-02 11:38:39 +0000

    2007-07-02  Ben Motmans <ben.motmans@gmail.com>
    	* OdbcSchemaProvider.cs: 
    	Imported the old DbProvider and updated all to code to use the new API

--- ben/MonoDevelop.Database/Mono.Data.Sql.Odbc/OdbcSchemaProvider.cs
+++ ben/MonoDevelop.Database/Mono.Data.Sql.Odbc/OdbcSchemaProvider.cs
@@ -36,9 +36,131 @@ namespace Mono.Data.Sql
 {
 	public class OdbcSchemaProvider : AbstractSchemaProvider
 	{
-		public OdbcSchemaProvider (OdbcConnectionProvider connectionProvider)
+		public OdbcSchemaProvider (IConnectionProvider connectionProvider)
 			: base (connectionProvider)
 		{
+		}
+		
+		public override bool SupportsSchemaType (Type type)
+		{
+			if (type == typeof(TableSchema))
+				return true;
+			else if (type == typeof(ViewSchema))
+				return true;
+			else if (type == typeof(ProcedureSchema))
+				return true;
+			else if (type == typeof(AggregateSchema))
+				return true;
+			else if (type == typeof(GroupSchema))
+				return true;
+			else if (type == typeof(UserSchema))
+				return true;
+			else if (type == typeof(LanguageSchema))
+				return true;
+			else if (type == typeof(OperatorSchema))
+				return true;
+			else if (type == typeof(RoleSchema))
+				return true;
+			else if (type == typeof(SequenceSchema))
+				return true;
+			else if (type == typeof(DataTypeSchema))
+				return true;
+			else if (type == typeof(TriggerSchema))
+				return true;
+			else if (type == typeof(ColumnSchema))
+				return true;
+			else if (type == typeof(ConstraintSchema))
+				return true;
+			else if (type == typeof(RuleSchema))
+				return true;
+			else
+				return false;
+		}
+		
+		public override ICollection<DatabaseSchema> GetDatabases ()
+		{
+			CheckConnectionState ();
+			List<DatabaseSchema> databases = new List<DatabaseSchema> ();
+			
+			//TODO:
+
+			return databases;
+		}
+
+		public override ICollection<TableSchema> GetTables ()
+		{
+			CheckConnectionState ();
+			List<TableSchema> tables = new List<TableSchema> ();
+			
+			//TODO:
+			
+			return tables;
+		}
+		
+		public override ICollection<ColumnSchema> GetTableColumns (TableSchema table)
+		{
+			CheckConnectionState ();
+			List<ColumnSchema> columns = new List<ColumnSchema> ();
+			
+			//TODO:
+			
+			return columns;
+		}
+
+		public override ICollection<ViewSchema> GetViews ()
+		{
+			CheckConnectionState ();
+			List<ViewSchema> views = new List<ViewSchema> ();
+
+			//TODO:
+			
+			return views;
+		}
+
+		public override ICollection<ColumnSchema> GetViewColumns (ViewSchema view)
+		{
+			CheckConnectionState ();
+			List<ColumnSchema> columns = new List<ColumnSchema> ();
+			
+			//TODO:
+			return columns;
+		}
+
+		public override ICollection<ProcedureSchema> GetProcedures ()
+		{
+			CheckConnectionState ();
+			List<ProcedureSchema> procedures = new List<ProcedureSchema> ();
+			
+			//TODO:
+			
+			return procedures; 
+		}
+
+		public override ICollection<ColumnSchema> GetProcedureColumns (ProcedureSchema procedure)
+		{
+			CheckConnectionState ();
+			List<ColumnSchema> columns = new List<ColumnSchema> ();
+			
+			//TODO:
+		      
+			return columns;
+		}
+		
+		public override ICollection<ParameterSchema> GetProcedureParameters (ProcedureSchema procedure)
+		{
+			CheckConnectionState ();
+			//TODO:
+			throw new NotImplementedException ();
+		}
+
+		public override ICollection<ConstraintSchema> GetTableConstraints (TableSchema table)
+		{
+			CheckConnectionState ();
+			List<ConstraintSchema> constraints = new List<ConstraintSchema> ();
+			
+			//TODO:
+
+			return constraints;
 		}
 	}
 }
