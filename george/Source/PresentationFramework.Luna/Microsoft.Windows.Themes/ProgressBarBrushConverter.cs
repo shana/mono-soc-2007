b8b969f736e9a49f32d99f8b573845f0285200c9
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-02 11:42:39 +0000

    2007-07-02  George Giolfan  <georgegiolfan@yahoo.com>
    	* ProgressBarBrushConverter.cs: Visual appearance is very close now, however the performance is not.

--- george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/ProgressBarBrushConverter.cs
+++ george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/ProgressBarBrushConverter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 #if Implementation
@@ -10,13 +11,26 @@ namespace Microsoft.Windows.Themes {
 	public class ProgressBarBrushConverter : IMultiValueConverter {
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
 			Brush foreground = (Brush)values[0];
+			//FIXME: How is it used?
 			bool is_indeterminate = (bool)values[1];
 			double indicator_lenght_in_orientation_direction = (double)values[2];
 			double indicator_lenght_in_other_direction = (double)values[3];
 			double track_lenght_in_orientation_direction = (double)values[4];
 			const double LineWidth = 6;
-			//FIXME
-			return foreground;
+			const double LineSpacing = 2;
+			DrawingGroup drawing = new DrawingGroup();
+			DrawingContext drawing_context = drawing.Open();
+			int lines = (int)Math.Ceiling(indicator_lenght_in_orientation_direction / (LineWidth + LineSpacing));
+			int line_index;
+			for (line_index = 0; line_index < lines - 1; line_index++)
+				drawing_context.DrawRectangle(foreground, null, new Rect(line_index * (LineWidth + LineSpacing), 0, LineWidth, indicator_lenght_in_other_direction));
+			drawing_context.DrawRectangle(foreground, null, new Rect(line_index * (LineWidth + LineSpacing), 0, indicator_lenght_in_orientation_direction - (lines - 1) * (LineWidth + LineSpacing), indicator_lenght_in_other_direction));
+			drawing_context.Close();
+			DrawingBrush result = new DrawingBrush(drawing);
+			result.Stretch = Stretch.None;
+			result.AlignmentX = AlignmentX.Left;
+			result.AlignmentY = AlignmentY.Top;
+			return result;
 		}
 
 		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture) {
