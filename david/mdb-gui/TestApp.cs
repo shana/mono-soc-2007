b9a7903e8ffa28e27bcf9611dd2f567e0be14e3b
dsrbecky <dsrbecky@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-04 16:19:37 +0000

    Update TestApp to test Callstack. [Ok] Callstack works. Selection of of callstack item is persisted over stepping.

--- david/mdb-gui/TestApp.cs
+++ david/mdb-gui/TestApp.cs
@@ -1,9 +1,21 @@
 using System;
 class TestApp
 {
-  public static void Main(string[] args)
-  {
-  	string msg = "Hello world";
-    Console.WriteLine(msg);
-  }
+	public static void Main(string[] args)
+	{
+		int number = 1234;
+		Fun1();
+	}
+	
+	public static void Fun1()
+	{
+		object obj = new object();
+		Fun2();
+	}
+	
+	public static void Fun2()
+	{
+		string msg = "Hello world";
+		Console.WriteLine(msg);
+	}
 }
