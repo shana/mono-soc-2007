0235e2a18ed678fd7b6f1a5131f0f5ce2895c549
debackerl <debackerl@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-05 15:56:48 +0000

    * Ribbon.cs: Removal of obselete commented code * RibbonGroup.cs: Updated layout computations to include an expand button

--- laurent/src/Ribbons/Ribbons/RibbonGroup.cs
+++ laurent/src/Ribbons/Ribbons/RibbonGroup.cs
@@ -9,7 +9,11 @@ namespace Ribbons
 		protected Theme theme = new Theme ();
 		protected string lbl;
 		protected Pango.Layout lbl_layout;
+		protected Gtk.Button expand;
 		
+		private double barHeight;
+		
+		protected double childPadding = 1.0;
 		protected double lineWidth = 1.0;
 		protected double space = 2.0;
 		
@@ -38,25 +42,59 @@ namespace Ribbons
 			
 			Label = null;
 			HeightRequest = 90;
-			BorderWidth = 1;
+			
+			expand = new Gtk.Button ("+");
+			expand.Parent = this;
+		}
+		
+		protected override void ForAll (bool include_internals, Callback callback)
+		{
+			base.ForAll (include_internals, callback);
+			callback (expand);
 		}
 		
 		protected override void OnSizeRequested (ref Requisition requisition)
 		{
 			base.OnSizeRequested (ref requisition);
 			
+			int lw, lh;
+			lbl_layout.GetPixelSize (out lw, out lh);
+			
+			Requisition childRequisition = new Requisition ();
+			if(Child != null && Child.Visible)
+			{
+				childRequisition = Child.SizeRequest ();
+			}
+			
+			barHeight = lh + 2 * space;
+			if(expand != null && expand.Visible)
+			{
+				expand.SetSizeRequest (lh, lh);
+				expand.SizeRequest ();
+			}
+			
 			if(WidthRequest == -1)
 			{
 				if(Child != null && Child.Visible)
 				{
-					Requisition childRequisition = Child.SizeRequest ();
-					requisition.Width = childRequisition.Width + (int)(2 * (2*lineWidth + BorderWidth));
+					requisition.Width = childRequisition.Width + (int)(2 * (2*lineWidth + childPadding));
 				}
 				else
 				{
-					int lw, lh;
-					lbl_layout.GetPixelSize (out lw, out lh);
-					requisition.Width = lw + (int)(2 * (2*(lineWidth+space)));
+					requisition.Width = lw + (int)(2 * (lineWidth+space));
+					if(expand != null && expand.Visible)
+					{
+						requisition.Width += expand.WidthRequest + (int)space;
+					}
+				}
+			}
+			
+			if(HeightRequest == -1)
+			{
+				requisition.Height = (int)(4 * lineWidth + barHeight);
+				if(Child != null && Child.Visible)
+				{
+					requisition.Height += childRequisition.Height + (int)(2 * childPadding);
 				}
 			}
 		}
@@ -65,14 +103,23 @@ namespace Ribbons
 		{
 			base.OnSizeAllocated (allocation);
 			
+			if(expand != null && expand.Visible)
+			{
+				double frameSize = 2*lineWidth + space;
+				Gdk.Rectangle r;
+				r.Height = expand.HeightRequest;
+				r.Width = expand.WidthRequest;
+				r.X = allocation.X + allocation.Width - r.Width - (int)frameSize;
+				r.Y = allocation.Y + allocation.Height - r.Height - (int)frameSize;
+				expand.SizeAllocate (r);
+			}
+			
 			if(Child != null && Child.Visible)
 			{
-				int lw, lh;
-				lbl_layout.GetPixelSize (out lw, out lh);
-				int frame_size = (int)(2*lineWidth + BorderWidth);
-				int wi = allocation.Width - 2 * frame_size; 
-				int he = allocation.Height - 2 * frame_size - (lh + (int)(2*space)); 
-				Gdk.Rectangle r = new Gdk.Rectangle (allocation.X + frame_size, allocation.Y + frame_size, wi, he);
+				double frameSize = 2*lineWidth + childPadding;
+				int wi = allocation.Width - (int)(2 * frameSize);
+				int he = allocation.Height - (int)(2 * frameSize + barHeight); 
+				Gdk.Rectangle r = new Gdk.Rectangle (allocation.X + (int)frameSize, allocation.Y + (int)frameSize, wi, he);
 				Child.SizeAllocate (r);
 			}
 		}
