8a8ce6fbbcae0e08e1c4419bd1e0bb6f9dbc8fb5
dsrbecky <dsrbecky@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-08 19:26:31 +0000

    Implementation of ArrayNode.  Arrays with non-standard bounds and multidimensional arrays are supported.  Huge arrays are recursively subdivided into smaller sets.

---
+++ trunk/david/mdb-gui/TreeModel/ArrayNode.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Text;
+
+using Mono.Debugger;
+using Mono.Debugger.Languages;
+
+namespace Mono.Debugger.Frontend.TreeModel
+{
+	public class ArrayNode: AbstractNode
+	{
+		string name;
+		StackFrame stackFrame;
+		TargetArrayObject obj;
+		
+		ArraySubsetNode universalSubset;
+		
+		public override string Name {
+			get { return name; }
+		}
+		
+		public override string Value {
+			get { return this.Type; }
+		}
+		
+		public override string Type {
+			get { return obj.TypeName; }
+		}
+		
+		public override bool HasChildNodes {
+			get { return universalSubset.HasChildNodes; }
+		}
+		
+		public override AbstractNode[] ChildNodes {
+			get { return universalSubset.ChildNodes; }
+		}
+		
+		public ArrayNode(string name, StackFrame stackFrame, TargetArrayObject obj)
+		{
+			this.name = name;
+			this.stackFrame = stackFrame;
+			this.obj = obj;
+			
+			universalSubset = new ArraySubsetNode(stackFrame, obj, new int[0]);
+		}
+	}
+}
---
+++ trunk/david/mdb-gui/TreeModel/ArraySubsetNode.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Text;
+
+using Mono.Debugger;
+using Mono.Debugger.Languages;
+
+namespace Mono.Debugger.Frontend.TreeModel
+{
+	/// <summary>
+	/// Represent a subset of array elements
+	/// </summary>
+	public class ArraySubsetNode: AbstractNode
+	{
+		const int SubsetOptimalSize = 5;
+		const int SubsetMaximalSize = 7;
+		
+		StackFrame stackFrame;
+		TargetArrayObject obj;
+		
+		int[] indicesPefix;
+		// The current dimension
+		int dimension;
+		// The bounds of current dimension
+		int lowerBound;
+		int upperBound;
+		// The start and end of the subset
+		int startIndex;
+		int endIndex;
+		
+		public override string Name {
+			get {
+				StringBuilder name = new StringBuilder();
+				name.Append("[");
+				// The indice prefix - eg 1,2,3,
+				foreach(int i in indicesPefix) {
+					name.Append(i);
+					name.Append(",");
+				}
+				// The group range - eg 100..199
+				if (startIndex != lowerBound || endIndex != upperBound) {
+					name.Append(startIndex);
+					name.Append("..");
+					name.Append(endIndex);
+				}
+				// Comas for remaining dimensions
+				name.Append(',', obj.Type.Rank - dimension - 1);
+				name.Append("]");
+				return name.ToString();
+			}
+		}
+		
+		public override bool HasChildNodes {
+			get {
+				return startIndex <= endIndex;
+			}
+		}
+		
+		public override AbstractNode[] ChildNodes {
+			get {
+				return GetChildNodes();
+			}
+		}
+		
+		public ArraySubsetNode(StackFrame stackFrame, TargetArrayObject obj, int[] indicesPefix, int startIndex, int endIndex)
+		{
+			this.stackFrame = stackFrame;
+			this.obj = obj;
+			this.indicesPefix = indicesPefix;
+			this.startIndex = startIndex;
+			this.endIndex = endIndex;
+			
+			dimension = indicesPefix.Length;
+			lowerBound = obj.GetLowerBound(stackFrame.Thread, dimension);
+			upperBound = obj.GetUpperBound(stackFrame.Thread, dimension);
+		}
+		
+		public ArraySubsetNode(StackFrame stackFrame, TargetArrayObject obj, int[] indicesPefix)
+			: this(stackFrame, obj, indicesPefix, 0, 0)
+		{
+			this.startIndex = lowerBound;
+			this.endIndex = upperBound;
+		}
+		
+		AbstractNode GetChildNode(int index)
+		{
+			int[] newIndices = new int[dimension + 1];
+			indicesPefix.CopyTo(newIndices, 0);
+			newIndices[dimension] = index;
+			
+			if (newIndices.Length == obj.Type.Rank) {
+				// We have reached the last dimension - create the element
+				TargetObject element = obj.GetElement(stackFrame.Thread, newIndices);
+				return NodeFactory.Create(IndicesToString(newIndices), element, stackFrame);
+			} else {
+				// Create a subset for the next dimension
+				return new ArraySubsetNode(stackFrame, obj, newIndices);
+			}
+		}
+		
+		public static string IndicesToString(int[] indices)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("[");
+			for(int i = 0; i < indices.Length; i++) {
+				sb.Append(indices[i]);
+				if (i < indices.Length - 1) {
+					sb.Append(",");
+				}
+			}
+			sb.Append("]");
+			return sb.ToString();
+		}
+		
+		AbstractNode[] GetChildNodes()
+		{
+			int childCount = endIndex - startIndex + 1;
+			if (childCount <= SubsetMaximalSize) {
+				// Return array of all childs
+				AbstractNode[] childs = new AbstractNode[childCount];
+				for(int i = 0; i < childCount; i++) {
+					childs[i] = GetChildNode(startIndex + i);
+				}
+				return childs;
+			} else {
+				// Subdivide to smaller groups
+				int groupSize = SubsetOptimalSize;
+				
+				// The number of groups must not be too big either
+				// Increase groupSize if necessary
+				int groupCount;
+				while(true) {
+					groupCount = ((childCount - 1) / groupSize) + 1; // Round up
+					if (groupCount <= SubsetMaximalSize) {
+						break;
+					} else {
+						groupSize = groupSize * groupSize;
+					}
+				}
+				
+				// Return the smaller groups
+				AbstractNode[] childs = new AbstractNode[groupCount];
+				for(int i = 0; i < groupCount; i++) {
+					int start = startIndex + i * groupSize;
+					int end = Math.Min(upperBound, start + groupSize - 1);
+					childs[i] = new ArraySubsetNode(stackFrame, obj, indicesPefix, start, end);
+				}
+				return childs;
+			}
+		}
+	}
+}
