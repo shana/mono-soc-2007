83093a446eea7b7708b8f43994d02ba1dca2b9f0
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-11 11:05:17 +0000

    2007-07-11  George Giolfan  <georgegiolfan@yahoo.com>
    	* CollectionView.cs: More work.

--- trunk/george/Source/PresentationFramework/System.Windows.Data/CollectionView.cs
+++ trunk/george/Source/PresentationFramework/System.Windows.Data/CollectionView.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Globalization;
+using System.Threading;
 using System.Windows.Threading;
 #if Implementation
 using System;
@@ -20,6 +21,8 @@ namespace System.Windows.Data {
 		int current_position = -1;
 		Predicate<Object> filter;
 		bool? cached_is_empty;
+		bool is_current_after_last;
+		bool is_current_before_first;
 		IEnumerable source_collection;
 		#endregion
 
@@ -33,6 +36,11 @@ namespace System.Windows.Data {
 			if (enumerator.MoveNext()) {
 				current_item = enumerator.Current;
 				current_position = 0;
+				is_current_after_last = false;
+				is_current_before_first = false;
+			} else {
+				is_current_after_last = true;
+				is_current_before_first = true;
 			}
 		}
 		#endregion
@@ -100,17 +108,11 @@ namespace System.Windows.Data {
 		}
 
 		public virtual bool IsCurrentAfterLast {
-			get { 
-				//WDTDH
-				return true; 
-			}
+			get { return is_current_after_last; }
 		}
 
 		public virtual bool IsCurrentBeforeFirst {
-			get {
-				//WDTDH
-				return true; 
-			}
+			get { return is_current_before_first; }
 		}
 
 		public virtual bool IsEmpty {
@@ -211,85 +213,105 @@ namespace System.Windows.Data {
 		}
 
 		public virtual bool MoveCurrentTo(object item) {
+			if (object.Equals(current_item, item))
+				return true;
 			int current_collection_item_index = 0;
 			foreach (object collection_item in source_collection) {
-				if (object.Equals(item, collection_item)) {
-					current_item = item;
-					current_position = current_collection_item_index;
-					return true;
-				}
+				if (object.Equals(item, collection_item))
+					return MoveCurrentToPosition(current_collection_item_index);
 				current_collection_item_index++;
 			}
-			current_item = null;
-			current_position = -1;
-			return false;
+			return MoveCurrentToPosition(-1);
+			//int current_collection_item_index = 0;
+			//foreach (object collection_item in source_collection) {
+			//    if (object.Equals(item, collection_item)) {
+			//        current_item = item;
+			//        current_position = current_collection_item_index;
+			//        is_current_after_last = false;
+			//        is_current_before_first = false;
+			//        OnCurrentChanged();
+			//        return true;
+			//    }
+			//    current_collection_item_index++;
+			//}
+			//current_item = null;
+			//current_position = -1;
+			//is_current_before_first = true;
+			//OnCurrentChanged();
+			//return false;
 		}
 
 		public virtual bool MoveCurrentToFirst() {
-			current_position = 0;
-			IEnumerator enumerator = source_collection.GetEnumerator();
-			if (enumerator.MoveNext()) {
-				current_item = enumerator.Current;
-				return true;
-			} else {
-				current_item = null;
-				return false;
-			}
+			return MoveCurrentToFirstInternal();
 		}
 
 		public virtual bool MoveCurrentToLast() {
-			current_item = null;
-			current_position = -1;
-			foreach(object item in source_collection) {
-				current_item = item;
-				current_position++;
-			}
-			return current_position != -1;
+			return MoveCurrentToLastInternal();
 		}
 
 		public virtual bool MoveCurrentToNext() {
-			bool passed_current_item = false;
-			current_position = 0;
-			foreach (object item in source_collection) {
-				if (passed_current_item) {
-					current_item = item;
-					return true;
-				}
-				passed_current_item = object.Equals(current_item, item);
-				current_position++;
-			}
-			current_item = null;
-			return false;
+			return is_current_after_last ? false : MoveCurrentToPosition(current_position + 1);
+			//if (is_current_before_first)
+			//    return MoveCurrentToFirstInternal();
+			//bool passed_current_item = false;
+			//current_position = 0;
+			//foreach (object item in source_collection) {
+			//    if (passed_current_item) {
+			//        current_item = item;
+			//        return true;
+			//    }
+			//    passed_current_item = object.Equals(current_item, item);
+			//    current_position++;
+			//}
+			//current_item = null;
+			//is_current_after_last = true;
+			//return false;
 		}
 
 		public virtual bool MoveCurrentToPosition(int position) {
+			is_current_after_last = false;
+			is_current_before_first = false;
+			if (position < 0) {
+				current_position = -1;
+				current_item = null;
+				is_current_before_first = true;
+				OnCurrentChanged();
+				return false;
+			}
 			current_position = 0;
 			foreach (object item in source_collection) {
 				if (current_position == position) {
 					current_item = item;
+					OnCurrentChanged();
 					return true;
 				}
 				current_position++;
 			}
 			current_item = null;
+			is_current_after_last = true;
 			if (position > current_position)
 				throw new ArgumentOutOfRangeException("position");
+			OnCurrentChanged();
 			return false;
 		}
 
 		public virtual bool MoveCurrentToPrevious() {
-			object previous_item = null;
-			int previous_position = -1;
-			foreach (object item in source_collection) {
-				if (object.Equals(item, current_item)) {
-					current_item = previous_item;
-					current_position = previous_position;
-					return current_position != -1;
-				}
-				previous_item = item;
-				previous_position++;
-			}
-			return false;
+			return is_current_before_first ? false : MoveCurrentToPosition(current_position - 1);
+			//if (is_current_after_last)
+			//    return MoveCurrentToLastInternal();
+			//object previous_item = null;
+			//int previous_position = -1;
+			//foreach (object item in source_collection) {
+			//    if (object.Equals(item, current_item)) {
+			//        current_item = previous_item;
+			//        current_position = previous_position;
+			//        is_current_before_first = current_position == -1;
+			//        return !is_current_before_first;
+			//    }
+			//    previous_item = item;
+			//    previous_position++;
+			//}
+			//return false;
 		}
 
 		public virtual bool PassesFilter(object item) {
@@ -316,7 +338,7 @@ namespace System.Windows.Data {
 		}
 
 		protected virtual void OnBeginChangeLogging(NotifyCollectionChangedEventArgs args) {
-			//WDTDH
+			//LAMESPEC?: Documentation says it should throw ArgumentNullException if args is null. It seems it does not.
 		}
 
 		protected virtual void OnCollectionChanged(NotifyCollectionChangedEventArgs args) {
@@ -325,7 +347,11 @@ namespace System.Windows.Data {
 		}
 
 		protected void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args) {
-			//WDTDH
+			//FIXME
+			//LAMESPEC?
+			if (Dispatcher.Thread != Thread.CurrentThread)
+				throw new NotSupportedException("This type of CollectionView does not support changes to its SourceCollection from a thread different from the Dispatcher thread.");
+			ProcessCollectionChanged(args);
 		}
 
 		protected virtual void OnCurrentChanged() {
@@ -349,7 +375,6 @@ namespace System.Windows.Data {
 		}
 
 		protected virtual void ProcessCollectionChanged(NotifyCollectionChangedEventArgs args) {
-			//WDTDH
 		}
 
 		protected void RefreshOrDefer() {
@@ -363,6 +388,8 @@ namespace System.Windows.Data {
 		protected void SetCurrent (object newItem, int newPosition) {
 			current_item = newItem;
 			current_position = newPosition;
+			is_current_before_first = current_position < 0;
+			is_current_after_last = current_position >= Count;
 		}
 		#endregion
 
@@ -379,32 +406,54 @@ namespace System.Windows.Data {
 		#region Explicit Interface Implementations
 		#region IEnumerable
 		IEnumerator IEnumerable.GetEnumerator() {
-			//WDTDH
-			return null;
+			return GetEnumerator();
 		}
 		#endregion
 
 		#region INotifyCollectionChanged
 		event NotifyCollectionChangedEventHandler INotifyCollectionChanged.CollectionChanged {
-			add {
-				//WDTDH
-			}
-			remove {
-				//WDTDH
-			}
+			add { CollectionChanged += value; }
+			remove { CollectionChanged -= value; }
 		}
 		#endregion
 
 		#region INotifyPropertyChanged
 		event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged {
-			add {
-				//WDTDH
-			}
-			remove {
-				//WDTDH
-			}
+			add { PropertyChanged += value; }
+			remove { PropertyChanged -= value; }
 		}
 		#endregion
+		#endregion
+
+		#region Private Methods
+		//TODO: Clean these in the end.
+		bool MoveCurrentToFirstInternal() {
+			current_position = 0;
+			IEnumerator enumerator = source_collection.GetEnumerator();
+			if (enumerator.MoveNext()) {
+				current_item = enumerator.Current;
+				is_current_after_last = false;
+				is_current_before_first = false;
+				return true;
+			} else {
+				current_item = null;
+				is_current_after_last = true;
+				is_current_before_first = true;
+				return false;
+			}
+		}
+
+		bool MoveCurrentToLastInternal() {
+			current_item = null;
+			current_position = -1;
+			foreach (object item in source_collection) {
+				current_item = item;
+				current_position++;
+			}
+			is_current_after_last = current_position == -1;
+			is_current_before_first = is_current_after_last;
+			return !is_current_after_last;
+		}
 		#endregion
 	}
 }
