2addab8efda93d610a2bdc9423b8d5c2d2393040
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-13 13:25:30 +0000

    2007-07-13  George Giolfan  <georgegiolfan@yahoo.com>
    	* Mono.WindowsPresentationFoundation: Started implementing a visual structure viewer.

--- george/Source/Mono.WindowsPresentationFoundation/Mono.WindowsPresentationFoundation.ColorFinder/ColorFinderWindow.cs
+++ george/Source/Mono.WindowsPresentationFoundation/Mono.WindowsPresentationFoundation.ColorFinder/ColorFinderWindow.cs
@@ -122,13 +122,5 @@ namespace Mono.WindowsPresentationFoundation.ColorFinder {
 				}
 			}
 		}
-
-		class Label : System.Windows.Controls.Label {
-			public Label(string labelText, UIElement target) {
-				Content = labelText;
-				Target = target;
-				Padding = new Thickness(1);
-			}
-		}
 	}
 }
---
+++ george/Source/Mono.WindowsPresentationFoundation/Mono.WindowsPresentationFoundation/Label.cs
@@ -0,0 +1,10 @@
+using System.Windows;
+namespace Mono.WindowsPresentationFoundation {
+	class Label : System.Windows.Controls.Label {
+		public Label(string labelText, UIElement target) {
+			Content = labelText;
+			Target = target;
+			Padding = new Thickness(1);
+		}
+	}
+}
