68d864e3c222e5114763cfa61c9f3747eb9163ad
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-15 16:06:45 +0000

    2007-07-15  Ben Motmans <ben.motmans@gmail.com>
    	* Modified install script to ignore unfinished components

--- trunk/ben/install.sh
+++ trunk/ben/install.sh
@@ -11,3 +11,11 @@ find ./MonoDevelop.Database -type f -iname "*.dll" -or -iname "*.addin.xml" | wh
 	cp -f "$a" $DIR
 #	echo "$a"
 done
+
+
+# temp remove some addins
+rm $DIR/Mono.Data.Sql.Firebird.addin.xml
+rm $DIR/Mono.Data.Sql.Odbc.addin.xml
+rm $DIR/Mono.Data.Sql.Oracle.addin.xml
+rm $DIR/Mono.Data.Sql.Sybase.addin.xml
+rm $DIR/MonoDevelop.Database.Visualization.addin.xml
