f500d5963b83f9ca55aac7f882feaf2478621a99
matej.spiller <matej.spiller@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-15 19:00:07 +0000

    - receive test

--- trunk/matej/Test/Messaging.Test/Main.cs
+++ trunk/matej/Test/Messaging.Test/Main.cs
@@ -14,13 +14,26 @@ namespace Messaging.Test
 {
 	class MainClass
 	{
-		public static void Main(string[] args)
-		{
+		
+		public static void SendMessage() {
 			MessageQueue myQueue = new MessageQueue("tcp://localhost:61616:testqueue");
    			Message msg = new Message("Hello this is test message" /*, new XmlMessageFormatter() */ );
    			// Send the image to the queue.
    			myQueue.Send(msg);
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello World!");			
+		}
+		
+		public static void ReceiveMessage() {
+			MessageQueue myQueue = new MessageQueue("tcp://localhost:61616:testqueue");
+			Message myMessage =    myQueue.Receive();
+   			String str = (String)myMessage.Body;  // we can see in the str of the message
+			Console.WriteLine(str);			
+		}
+		
+		public static void Main(string[] args)
+		{
+			SendMessage();
+			ReceiveMessage();
 		}
 	}
 }
