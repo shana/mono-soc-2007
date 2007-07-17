230c9e20f5e3d4db06b5008848d4965a3e837edb
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-17 23:31:55 +0000

    2007-07-18  Ben Motmans <ben.motmans@gmail.com>
    	* Dialogs/TableEditorDialog.cs, Widgets/TriggersEditorWidget.cs,
    	Widgets/IndicesEditorWidget.cs, Widgets/ConstraintsEditorWidget.cs,
    	Widgets/ColumnsEditorWidget.cs: Widgets and dialogs used by the Table Editor (unfinished)

---
+++ trunk/ben/MonoDevelop.Database/MonoDevelop.Database.Designer/Widgets/TriggersEditorWidget.cs
@@ -0,0 +1,17 @@
+
+
+using System;
+
+namespace MonoDevelop.Database.Designer
+{
+	
+	
+	public partial class TriggersEditorWidget : Gtk.Bin
+	{
+		
+		public TriggersEditorWidget()
+		{
+			this.Build();
+		}
+	}
+}
