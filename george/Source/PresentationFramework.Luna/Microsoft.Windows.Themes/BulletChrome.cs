7c8eba2a6977fbe42c306d98e8ccc4b2441dee23
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-17 19:07:58 +0000

    2007-07-16  George Giolfan  <georgegiolfan@yahoo.com>
    	* It compiles now.

--- george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/BulletChrome.cs
+++ george/Source/PresentationFramework.Luna/Microsoft.Windows.Themes/BulletChrome.cs
@@ -90,15 +90,15 @@ namespace Microsoft.Windows.Themes {
 				double radius_y = ActualHeight / 2 - .5;
 				Point center = new Point(radius_x + .5, radius_y + .5);
 				if (Background != null)
-				    drawingContext.DrawEllipse(Background, null, center, radius_x - 1, radius_y - 1);
+					drawingContext.DrawEllipse(Background, null, center, radius_x - 1, radius_y - 1);
 				if (RenderPressed)
-				    drawingContext.DrawEllipse(pressed_brush, null, center, radius_x - 1, radius_y - 1);
+					drawingContext.DrawEllipse(pressed_brush, null, center, radius_x - 1, radius_y - 1);
 				if (BorderBrush != null)
-				    drawingContext.DrawEllipse(null, new Pen(BorderBrush, 1), center, radius_x, radius_y);
+					drawingContext.DrawEllipse(null, new Pen(BorderBrush, 1), center, radius_x, radius_y);
 				if (IsChecked.Value)
 					drawingContext.DrawEllipse(radio_button_check_brush, null, center, radius_x - 3.5, radius_y - 3.5);
 				if (RenderMouseOver && !RenderPressed)
-				    drawingContext.DrawEllipse(null, new Pen(mouse_over_brush, 2), center, radius_x - 2, radius_y - 2);
+					drawingContext.DrawEllipse(null, new Pen(mouse_over_brush, 2), center, radius_x - 2, radius_y - 2);
 				#endregion
 			} else {
 				#region Render check box bullet
@@ -109,7 +109,9 @@ namespace Microsoft.Windows.Themes {
 				double height;
 				bool fill_displayed;
 				if (render_pressed || background != null) {
-					if (fill_displayed = ((width = ActualWidth - border_thickness.Left - border_thickness.Right) >= 0 && (height = ActualHeight - border_thickness.Top - border_thickness.Bottom) >= 0)) {
+					width = ActualWidth - border_thickness.Left - border_thickness.Right;
+					height = ActualHeight - border_thickness.Top - border_thickness.Bottom;
+					if (fill_displayed = (width >= 0 && height >= 0)) {
 						Brush fill_brush;
 						if (render_pressed)
 							fill_brush = new LinearGradientBrush(new GradientStopCollection(new GradientStop[] {
@@ -130,43 +132,43 @@ namespace Microsoft.Windows.Themes {
 								new GradientStop(Color.FromArgb(0xFF, 0xF8, 0xB3, 0x30), 1)
 							}), new Point(0, 0), new Point(1, 1)), 2), new Rect(border_thickness.Left + 1, border_thickness.Top + 1, width, height));
 				bool has_uniform_border_thickness = border_thickness.Bottom == border_thickness.Left && border_thickness.Bottom == BorderThickness.Right && border_thickness.Bottom == BorderThickness.Top;
-				if (IsChecked.Value) {
-					if (!has_uniform_border_thickness)
-						drawingContext.PushTransform(new TranslateTransform(border_thickness.Left - 1, border_thickness.Top - 1));
-					StreamGeometry stream_geometry = new StreamGeometry();
-					StreamGeometryContext context = stream_geometry.Open();
-					context.BeginFigure(new Point(3, 5), true, true);
-					context.LineTo(new Point(3, 7.8), false, false);
-					context.LineTo(new Point(5.5, 10.4), false, false);
-					context.LineTo(new Point(10.1, 5.8), false, false);
-					context.LineTo(new Point(10.1, 3), false, false);
-					context.LineTo(new Point(5.5, 7.6), false, false);
-					context.Close();
-					drawingContext.DrawGeometry(new SolidColorBrush(render_pressed ? Color.FromArgb(0xFF, 0x1A, 0x7E, 0x18) : Color.FromArgb(0xFF, 0x21, 0xA1, 0x21)), null, stream_geometry);
-					if (!has_uniform_border_thickness)
-						drawingContext.Pop();
-				}
 				if (fill_displayed)
-					if (BorderBrush != null) {
-						if (has_uniform_border_thickness) {
-							double border_thickness_size = border_thickness.Bottom;
-							if ((width = ActualWidth - 2 * border_thickness_size + 1) >= 0 && (height = ActualHeight - 2 * border_thickness_size + 1) >= 0)
-								drawingContext.DrawRectangle(null, new Pen(BorderBrush, border_thickness.Bottom), new Rect(border_thickness_size - 0.5, border_thickness_size - 0.5, width, height));
-						} else {
-							drawingContext.DrawGeometry(BorderBrush, null, new PathGeometry(new PathFigure[] {
-								new PathFigure(new Point(0, 0), new PathSegment[] {
-									new LineSegment(new Point(ActualWidth, 0), false),
-									new LineSegment(new Point(ActualWidth, ActualHeight), false),
-									new LineSegment(new Point(0, ActualHeight), false)
-								}, true),
-								new PathFigure(new Point(border_thickness.Left, border_thickness.Top), new PathSegment[] {
-									new LineSegment(new Point(ActualWidth - border_thickness.Right, border_thickness.Top), false),
-									new LineSegment(new Point(ActualWidth - border_thickness.Right, ActualHeight - border_thickness.Bottom), false),
-									new LineSegment(new Point(border_thickness.Left, ActualHeight-border_thickness.Bottom), false)
-								}, true)
-							}));
-						}
+					if (IsChecked.Value) {
+						if (!has_uniform_border_thickness)
+							drawingContext.PushTransform(new TranslateTransform(border_thickness.Left - 1, border_thickness.Top - 1));
+						StreamGeometry stream_geometry = new StreamGeometry();
+						StreamGeometryContext context = stream_geometry.Open();
+						context.BeginFigure(new Point(3, 5), true, true);
+						context.LineTo(new Point(3, 7.8), false, false);
+						context.LineTo(new Point(5.5, 10.4), false, false);
+						context.LineTo(new Point(10.1, 5.8), false, false);
+						context.LineTo(new Point(10.1, 3), false, false);
+						context.LineTo(new Point(5.5, 7.6), false, false);
+						context.Close();
+						drawingContext.DrawGeometry(new SolidColorBrush(render_pressed ? Color.FromArgb(0xFF, 0x1A, 0x7E, 0x18) : Color.FromArgb(0xFF, 0x21, 0xA1, 0x21)), null, stream_geometry);
+						if (!has_uniform_border_thickness)
+							drawingContext.Pop();
 					}
+				if (BorderBrush != null) {
+					if (has_uniform_border_thickness) {
+						double border_thickness_size = border_thickness.Bottom;
+						if ((width = ActualWidth - 2 * border_thickness_size + 1) >= 0 && (height = ActualHeight - 2 * border_thickness_size + 1) >= 0)
+							drawingContext.DrawRectangle(null, new Pen(BorderBrush, border_thickness.Bottom), new Rect(border_thickness_size - 0.5, border_thickness_size - 0.5, width, height));
+					} else {
+						drawingContext.DrawGeometry(BorderBrush, null, new PathGeometry(new PathFigure[] {
+							new PathFigure(new Point(0, 0), new PathSegment[] {
+								new LineSegment(new Point(ActualWidth, 0), false),
+								new LineSegment(new Point(ActualWidth, ActualHeight), false),
+								new LineSegment(new Point(0, ActualHeight), false)
+							}, true),
+							new PathFigure(new Point(border_thickness.Left, border_thickness.Top), new PathSegment[] {
+								new LineSegment(new Point((width = ActualWidth - border_thickness.Right) >= 0 ? width : border_thickness.Left, border_thickness.Top), false),
+								new LineSegment(new Point((width = ActualWidth - border_thickness.Right) >= 0 ? width : border_thickness.Left, (height = ActualHeight - border_thickness.Bottom) >= 0 ? height : border_thickness.Top), false),
+								new LineSegment(new Point(border_thickness.Left, (height = ActualHeight - border_thickness.Bottom) >= 0 ? height : border_thickness.Top), false)
+							}, true)
+						}));
+					}
+				}
 				#endregion
 			}
 		}
