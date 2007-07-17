6d033aafe2d24f3d9dfc5aa9801732fdd5eed39a
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-17 00:08:57 +0000

    2007-07-17  Ben Motmans <ben.motmans@gmail.com>
    	* Makefile.am: initial makefile
    	* NpgsqlDbFactory.cs: implemented methods required for the connection settings dialog

--- trunk/ben/MonoDevelop.Database/MonoDevelop.Database.Sql.Npgsql/NpgsqlDbFactory.cs
+++ trunk/ben/MonoDevelop.Database/MonoDevelop.Database.Sql.Npgsql/NpgsqlDbFactory.cs
@@ -23,11 +23,14 @@
 // THE SOFTWARE.
 //
 
+using Gtk;
 using System;
 using System.Data;
 using System.Collections.Generic;
-
-namespaceMonoDevelop.Database.Sql
+using MonoDevelop.Core;
+using MonoDevelop.Core.Gui;
+using MonoDevelop.Database.Components;
+namespace MonoDevelop.Database.Sql
 {
 	public class NpgsqlDbFactory : IDbFactory
 	{
@@ -58,9 +61,9 @@ namespaceMonoDevelop.Database.Sql
 			}
 		}
 		
-		public IConnectionPool CreateConnectionPool (ConnectionSettings settings)
+		public IConnectionPool CreateConnectionPool (DatabaseConnectionContext context)
 		{
-			return new DefaultConnectionPool (this, ConnectionProvider, settings);
+			return new DefaultConnectionPool (this, ConnectionProvider, context);
 		}
 		
 		public ISchemaProvider CreateSchemaProvider (IConnectionPool connectionPool)
@@ -68,16 +71,50 @@ namespaceMonoDevelop.Database.Sql
 			return new NpgsqlSchemaProvider (connectionPool);
 		}
 		
-		public ConnectionSettings GetDefaultConnectionSettings ()
+		public object GetOption (string name)
 		{
-			ConnectionSettings settings = new ConnectionSettings ();
+			switch (name) {
+			case "settings.requires.server":
+			case "settings.requires.port":
+			case "settings.requires.username":
+			case "settings.requires.password":
+				return true;
+			default:
+				return null;
+			}
+		}
+		
+		public DatabaseConnectionSettings GetDefaultConnectionSettings ()
+		{
+			DatabaseConnectionSettings settings = new DatabaseConnectionSettings ();
 			settings.ProviderIdentifier = Identifier;
 			settings.Server = "localhost";
 			settings.Port = 5432;
 			settings.Username = "SYSDBA";
-			settings.Password = String.Empty;
-			settings.Database = String.Empty;
+			settings.MaxPoolSize = 5;
+			
 			return settings;
+		}
+		
+		public bool ShowOpenDatabaseDialog (out string database)
+		{
+			database = null;
+			return false;
+		}
+		
+		public bool ShowEditDatabaseConnectionDialog (DatabaseConnectionSettings connectionSettings)
+		{
+			DatabaseConnectionSettingsDialog dlg = new DatabaseConnectionSettingsDialog (connectionSettings);
+			int result = dlg.Run ();
+			dlg.Destroy ();
+			return result == (int)ResponseType.Ok;
+		}
+		
+		public bool ShowRemoveDatabaseConnectionDialog (DatabaseConnectionSettings connectionSettings)
+		{
+			return Services.MessageService.AskQuestionFormatted (
+				GettextCatalog.GetString ("Are you sure you want to remove connection '{0}'?"),
+				connectionSettings.Name); 
 		}
 	}
 }
