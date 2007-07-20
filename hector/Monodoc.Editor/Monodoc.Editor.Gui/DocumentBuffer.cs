58a33da665f6163f686aa6a410cae76f97491521
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-20 02:14:22 +0000

    * DocumentBuffer.cs: Added more implementation for easy editing of   summary text.

--- hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentBuffer.cs
+++ hector/Monodoc.Editor/Monodoc.Editor.Gui/DocumentBuffer.cs
@@ -21,7 +21,9 @@ public class DocumentBuffer : TextBuffer {
 	
 	public DocumentBuffer () : base (new DocumentTagTable ())
 	{
-		this.InsertText += OnAddedText;
+		InsertText += OnAddedText;
+		TagRemoved += OnRemoved;
+		DeleteRange += OnDelete;
 		document_loaded = false;
 	}
 
@@ -36,7 +38,47 @@ public class DocumentBuffer : TextBuffer {
 		if (document_loaded) {
 			Console.WriteLine ("DEBUG: Inserting: {0}, at Offset {1}", args.Text, args.Pos.Offset);
 			Console.WriteLine ("DEBUG: Offset: {0} Char {1}", args.Pos.Offset, args.Pos.Char);
+
+			TextIter insertIter = args.Pos;
+			int last_index = insertIter.Tags.Length - 1;
+			TextTag last = insertIter.Tags [last_index];
+
+			TextIter fooIter = GetIterAtOffset (insertIter.Offset - args.Text.Length - 1);
+			TextTag last_last = fooIter.Tags [fooIter.Tags.Length - 1];
+
+			TextIter nextIter = fooIter;
+			nextIter.ForwardChar ();
+
+			if (insertIter.BeginsTag (last) && last.Editable) {
+				Console.WriteLine ("DEBUG: Inserting text from start of editable region");
+				Console.WriteLine ("DEBUG: Tag Name: {0} HasTag: {1} Char: {2}" , last_last.Name, fooIter.HasTag (last_last), fooIter.Char);  
+
+				int offset = insertIter.Offset - args.Text.Length;
+				TextIter newIter = GetIterAtOffset (offset);
+				ApplyTag (last, newIter, insertIter);
+			} else if (fooIter.HasTag (last_last) && !nextIter.HasTag (last_last) && last_last.Editable) {
+				Console.WriteLine ("DEBUG: Inserting text from end of editable region");
+				Console.WriteLine ("DEBUG: Tag Name: {0} HasTag: {1} Char: {2}" , last_last.Name, fooIter.HasTag (last_last), fooIter.Char);  
+
+				int offset = fooIter.Offset + args.Text.Length + 1;
+				TextIter newIter = GetIterAtOffset (offset);
+				ApplyTag (last_last, fooIter, newIter);
+			}
 		}
+	}
+
+	private void OnRemoved (object o, TagRemovedArgs args)
+	{
+		Console.WriteLine ("DEBUG: Deleting Tag {0}", args.Tag.Name);
+	}
+
+	private void OnDelete (object o, DeleteRangeArgs args)
+	{
+		Console.WriteLine ("DEBUG: Deleting range");
+		if (args.Start.Offset == args.End.Offset)
+			Console.WriteLine ("DEBUG: Zero length range.");
+		Console.WriteLine ("DEBUG: Start Offset: {0} Char: {1}", args.Start.Offset, args.Start.Char);
+		Console.WriteLine ("DEBUG: End Offset: {0} Char: {1}", args.End.Offset, args.End.Char);
 	}
 }
 }
