852d925885650b52714971c25bf80505fadcce90
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-22 19:42:43 +0000

    2007-07-22  George Giolfan  <georgegiolfan@yahoo.com>
    	* TickPlacement.cs: Added.
    	* Slider.cs, SliderTest.cs, Slider sample: Switched to implemented TickPlacement.

---
+++ george/Source/PresentationFramework/System.Windows.Controls.Primitives/TickPlacement.cs
@@ -0,0 +1,12 @@
+#if Implementation
+namespace Mono.System.Windows.Controls.Primitives {
+#else
+namespace System.Windows.Controls.Primitives {
+#endif
+	public enum TickPlacement {
+		None,
+		TopLeft,
+		BottomRight,
+		Both
+	}
+}
