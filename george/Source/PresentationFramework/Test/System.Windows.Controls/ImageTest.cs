62e8f21f21dfeb2557385e8c3fcdbd56b37da63b
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-25 19:14:55 +0000

    2007-07-25  George Giolfan  <georgegiolfan@yahoo.com>
    	* ImageTest.cs: Added layout tests.

--- george/Source/PresentationFramework/Test/System.Windows.Controls/ImageTest.cs
+++ george/Source/PresentationFramework/Test/System.Windows.Controls/ImageTest.cs
@@ -1,7 +1,9 @@
 using NUnit.Framework;
 using System.Windows.Markup;
+using System.Windows.Media;
 #if Implementation
 using System;
+using System.Windows;
 namespace Mono.System.Windows.Controls {
 #else
 namespace System.Windows.Controls {
@@ -49,6 +51,224 @@ namespace System.Windows.Controls {
 		class BaseUriDefaultValueImage : Image {
 			public BaseUriDefaultValueImage() {
 				Assert.IsNull(BaseUri);
+			}
+		}
+		#endregion
+
+		#region ArrangeOverride
+		[Test]
+		public void ArrangeOverride() {
+			new ArrangeOverrideImage();
+		}
+
+		class ArrangeOverrideImage : Image {
+			Size arrange_size;
+			Size arrange_result;
+
+			public ArrangeOverrideImage() {
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(arrange_size, new Size(100, 100), "1");
+				Assert.AreEqual(arrange_result, new Size(0, 0), "2");
+			}
+
+			protected override Size ArrangeOverride(Size arrangeSize) {
+				return arrange_result = base.ArrangeOverride(arrange_size = arrangeSize);
+			}
+		}
+		#endregion
+
+		#region ArrangeOverride2
+		[Test]
+		public void ArrangeOverride2() {
+			new ArrangeOverride2Image();
+		}
+
+		class ArrangeOverride2Image : Image {
+			Size arrange_size;
+			Size arrange_result;
+
+			public ArrangeOverride2Image() {
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 10))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(arrange_size, new Size(100, 100), "1");
+				Assert.AreEqual(arrange_result, new Size(100, 100), "2");
+			}
+
+			protected override Size ArrangeOverride(Size arrangeSize) {
+				return arrange_result = base.ArrangeOverride(arrange_size = arrangeSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverride
+		[Test]
+		public void MeasureOverride() {
+			new MeasureOverrideImage();
+		}
+
+		class MeasureOverrideImage : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverrideImage() {
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(0, 0), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverride2
+		[Test]
+		public void MeasureOverride2() {
+			new MeasureOverride2Image();
+		}
+
+		class MeasureOverride2Image : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverride2Image() {
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 10))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(100, 100), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverrideStretchNone
+		[Test]
+		public void MeasureOverrideStretchNone() {
+			new MeasureOverrideStretchNoneImage();
+		}
+
+		class MeasureOverrideStretchNoneImage : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverrideStretchNoneImage() {
+				Stretch = Stretch.None;
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 10))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(10, 10), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverrideStretchNoneDifferentWidthHeight
+		[Test]
+		public void MeasureOverrideStretchNoneDifferentWidthHeight() {
+			new MeasureOverrideStretchNoneDifferentWidthHeightImage();
+		}
+
+		class MeasureOverrideStretchNoneDifferentWidthHeightImage : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverrideStretchNoneDifferentWidthHeightImage() {
+				Stretch = Stretch.None;
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 20))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(10, 20), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverrideStretchFillDifferentWidthHeight
+		[Test]
+		public void MeasureOverrideStretchFillDifferentWidthHeight() {
+			new MeasureOverrideStretchFillDifferentWidthHeightImage();
+		}
+
+		class MeasureOverrideStretchFillDifferentWidthHeightImage : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverrideStretchFillDifferentWidthHeightImage() {
+				Stretch = Stretch.Fill;
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 20))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(100, 100), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
+			}
+		}
+		#endregion
+
+		#region MeasureOverrideStretchUniformDifferentWidthHeight
+		[Test]
+		public void MeasureOverrideStretchUniformDifferentWidthHeight() {
+			new MeasureOverrideStretchUniformDifferentWidthHeightImage();
+		}
+
+		class MeasureOverrideStretchUniformDifferentWidthHeightImage : Image {
+			Size measure_size;
+			Size measure_result;
+
+			public MeasureOverrideStretchUniformDifferentWidthHeightImage() {
+				Stretch = Stretch.Uniform;
+				Source = new DrawingImage(new GeometryDrawing(Brushes.Red, null, new RectangleGeometry(new Rect(0, 0, 10, 20))));
+				Width = 100;
+				Height = 100;
+				Window w = new Window();
+				w.Content = this;
+				w.Show();
+				Assert.AreEqual(measure_size, new Size(100, 100), "1");
+				Assert.AreEqual(measure_result, new Size(50, 100), "2");
+			}
+
+			protected override Size MeasureOverride(Size availableSize) {
+				return measure_result = base.MeasureOverride(measure_size = availableSize);
 			}
 		}
 		#endregion
