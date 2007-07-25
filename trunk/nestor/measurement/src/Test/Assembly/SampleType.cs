19b651bf942f46ca0b87382a5d7f40c9d0acdb4b
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-25 10:22:08 +0000

    Added more tests.
    I think that I have the error.

--- trunk/nestor/measurement/src/Test/Assembly/SampleType.cs
+++ trunk/nestor/measurement/src/Test/Assembly/SampleType.cs
@@ -2,5 +2,20 @@ using System;
 
 namespace Test.Assembly {
 	public class SampleType {
+		public void MethodWithoutParameters () 
+		{
+		}
+
+		public void MethodWithOneParameter (int x) 
+		{
+		}
+
+		public void MethodWithTwoParameters (int x, char j) 
+		{
+		}
+
+		public void MethodWithThreeParameters (int x, char j, string z) 
+		{
+		}
 	}
 }
