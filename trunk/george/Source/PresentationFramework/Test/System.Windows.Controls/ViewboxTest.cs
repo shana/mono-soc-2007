1a4703f870e5807f394e25618b3050a87bcb84a2
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-28 20:33:56 +0000

    2007-07-28  George Giolfan  <georgegiolfan@yahoo.com>
    	* ViewboxTest.cs: Added.

---
+++ trunk/george/Source/PresentationFramework/Test/System.Windows.Controls/ViewboxTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+#if Implementation
+using System.Windows;
+using System.Windows.Controls;
+namespace Mono.System.Windows.Controls {
+#else
+namespace System.Windows.Controls {
+#endif
+	[TestFixture]
+	public class ViewboxTest {
+	}
+}
