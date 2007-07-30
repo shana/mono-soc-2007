cb69b63bd5b1166d662aa3dfc0e1c39d37a22e70
dsrbecky <dsrbecky@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-30 15:29:28 +0000

    Created a specialized RemoteTreeStore for every pad.  These stores now contain the logic to fill the store.

---
+++ david/mdb-gui/Pads/DebuggerPad.cs
@@ -0,0 +1,86 @@
+using System;
+
+using Gtk;
+
+namespace Mono.Debugger.Frontend
+{
+	public abstract class DebuggerPad: Gtk.ScrolledWindow
+	{
+		MdbGui mdbGui;
+		Gtk.TreeStore gtkStore;
+		Gtk.TreeView gtkTree;
+		
+		protected MdbGui MdbGui {
+			get { return mdbGui; }
+		}
+		
+		protected TreeStore GtkStore {
+			get { return gtkStore; }
+		}
+		
+		protected TreeView GtkTree {
+			get { return gtkTree; }
+		}
+		
+		public DebuggerPad(MdbGui mdbGui, Type[] columnTypes)
+		{
+			this.mdbGui = mdbGui;
+			this.gtkStore = new Gtk.TreeStore(columnTypes);
+			this.gtkTree = new Gtk.TreeView(gtkStore);
+			
+			this.ShadowType = ShadowType.In;
+			gtkTree.RulesHint = true;
+			gtkTree.HeadersVisible = true;
+			
+			Add(gtkTree);
+			ShowAll();
+		}
+		
+		protected TreeViewColumn AddImageColumn(string columnHeader, int columnIndex)
+		{
+			TreeViewColumn column = new TreeViewColumn();
+			CellRenderer iconRenderer = new CellRendererPixbuf();
+			column.Title = columnHeader;
+			column.PackStart(iconRenderer, false);
+			column.AddAttribute(iconRenderer, "pixbuf", columnIndex);
+			column.Resizable = true;
+			column.Alignment = 0.0f;
+			gtkTree.AppendColumn(column);
+			
+			return column;
+		}
+		
+		protected TreeViewColumn AddImageTextColumn(string columnHeader, int imageColumnIndex, int textColumnIndex)
+		{
+			TreeViewColumn column = new TreeViewColumn();
+			CellRenderer textRenderer = new CellRendererText();
+			CellRenderer iconRenderer = new CellRendererPixbuf();
+			column.Title = columnHeader;
+			column.PackStart(iconRenderer, false);
+			column.PackStart(textRenderer, true);
+			column.AddAttribute(iconRenderer, "pixbuf", imageColumnIndex);
+			column.AddAttribute(textRenderer, "text", textColumnIndex);
+			column.Resizable = true;
+			column.Alignment = 0.0f;
+			gtkTree.AppendColumn(column);
+			
+			return column;
+		}
+		
+		protected TreeViewColumn AddTextColumn(string columnHeader, int columnIndex)
+		{
+			TreeViewColumn column = new TreeViewColumn();
+			CellRenderer textRenderer = new CellRendererText();
+			column.Title = columnHeader;
+			column.PackStart(textRenderer, true);
+			column.AddAttribute(textRenderer, "text", columnIndex);
+			column.Resizable = true;
+			column.Alignment = 0.0f;
+			gtkTree.AppendColumn(column);
+			
+			return column;
+		}
+		
+		public abstract void UpdateDisplay();
+	}
+}
---
+++ david/mdb-gui/Pads/Stores/LocalsStore.cs
@@ -0,0 +1,111 @@
+using System;
+
+using Mono.Debugger;
+using Mono.Debugger.Languages;
+using Mono.Debugger.Frontend.TreeModel;
+
+namespace Mono.Debugger.Frontend
+{
+	public class LocalsStore: RemoteTreeStore
+	{
+		Interpreter interpreter;
+		
+		public const int ColumnNode     = 0;
+		public const int ColumnID       = 1;
+		public const int ColumnImage    = 2;
+		public const int ColumnName     = 3;
+		public const int ColumnValue    = 4;
+		public const int ColumnType     = 5;
+		
+		public new static Type[] ColumnTypes = new Type[] {
+			typeof(AbstractNode),
+			typeof(string),
+			typeof(Gdk.Pixbuf),
+			typeof(string),
+			typeof(string),
+			typeof(string)
+		};
+		
+		public LocalsStore(Interpreter interpreter): base(ColumnTypes)
+		{
+			this.interpreter = interpreter;
+		}
+		
+		public void UpdateTree()
+		{
+			StackFrame currentFrame;
+			
+			try {
+				currentFrame = interpreter.CurrentThread.GetBacktrace().CurrentFrame;
+			} catch {
+				RootNode.Clear();
+				return;
+			}
+			
+			RootNode.Clear();
+			
+			foreach (TargetVariable variable in currentFrame.Method.Parameters) {
+				AbstractNode node = NodeFactory.Create(variable, currentFrame);
+				AppendNode(RootNode, node);
+			}
+				
+			foreach (TargetVariable variable in currentFrame.Locals) {
+				AbstractNode node = NodeFactory.Create(variable, currentFrame);
+				AppendNode(RootNode, node);
+			}
+			
+			if (currentFrame.Method.HasThis) {
+				AbstractNode node = NodeFactory.Create(currentFrame.Method.This, currentFrame);
+				AppendNode(RootNode, node);
+			}
+		}
+		
+		void AppendNode(RemoteTreeNode treeNode, AbstractNode node)
+		{
+			string idPrefix;
+			if (treeNode == RootNode) {
+				idPrefix = String.Empty;
+			} else {
+				idPrefix = (string)treeNode.GetValue(ColumnID) + ".";
+			}
+			string id = idPrefix + node.Name;
+			
+			RemoteTreeNode newNode = treeNode.AppendNode();
+			newNode.SetValue(ColumnNode, node);
+			newNode.SetValue(ColumnID, id);
+			newNode.SetValue(ColumnImage, node.Image);
+			newNode.SetValue(ColumnName, node.Name);
+			newNode.SetValue(ColumnValue, node.Value);
+			newNode.SetValue(ColumnType, node.Type);
+			
+			// Placeholder so that the item is expandable
+			if (node.HasChildNodes) {
+				newNode.AppendNode();
+			}
+		}
+		
+		/// <summary> Expands the given node </summary>
+		/// <returns> Number of child nodes </returns>
+		public int ExpandNode(RemoteTreePath path)
+		{
+			RemoteTreeNode treeNode = GetNode(path);
+			
+			// Remove all current children
+			treeNode.Clear();
+			
+			// Add childs
+			AbstractNode node = (AbstractNode)treeNode.GetValue(ColumnNode);
+			AbstractNode[] childs;
+			try {
+				childs = node.ChildNodes;
+			} catch {
+				AppendNode(treeNode, new ErrorNode(String.Empty, "Can not get child nodes"));
+				return 1;
+			}
+			foreach(AbstractNode child in childs) {
+				AppendNode(treeNode, child);
+			}
+			return childs.Length;
+		}
+	}
+}
