5b833ba886adbc074f1a4a1c3eab9a4fdd621b81
dsrbecky <dsrbecky@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-30 15:38:55 +0000

    Algorithm for smart update of the local variables pad.

--- david/mdb-gui/Pads/Stores/LocalsStore.cs
+++ david/mdb-gui/Pads/Stores/LocalsStore.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 
 using Mono.Debugger;
 using Mono.Debugger.Languages;
@@ -31,78 +32,113 @@ namespace Mono.Debugger.Frontend
 		
 		public void UpdateTree()
 		{
+		}
+		
+		AbstractNode[] GetRootVariables()
+		{
+			ArrayList rootVariables = new ArrayList();
+			
 			StackFrame currentFrame;
 			
 			try {
 				currentFrame = interpreter.CurrentThread.GetBacktrace().CurrentFrame;
 			} catch {
-				RootNode.Clear();
-				return;
+				return new AbstractNode[0];
 			}
 			
-			RootNode.Clear();
-			
 			foreach (TargetVariable variable in currentFrame.Method.Parameters) {
-				AbstractNode node = NodeFactory.Create(variable, currentFrame);
-				AppendNode(RootNode, node);
+				rootVariables.Add(NodeFactory.Create(variable, currentFrame));
 			}
 				
 			foreach (TargetVariable variable in currentFrame.Locals) {
-				AbstractNode node = NodeFactory.Create(variable, currentFrame);
-				AppendNode(RootNode, node);
+				rootVariables.Add(NodeFactory.Create(variable, currentFrame));
 			}
 			
 			if (currentFrame.Method.HasThis) {
-				AbstractNode node = NodeFactory.Create(currentFrame.Method.This, currentFrame);
-				AppendNode(RootNode, node);
+				rootVariables.Add(NodeFactory.Create(currentFrame.Method.This, currentFrame));
 			}
+			
+			return (AbstractNode[])rootVariables.ToArray(typeof(AbstractNode));
 		}
 		
-		void AppendNode(RemoteTreeNode treeNode, AbstractNode node)
+		void UpdateNodeRecursive(RemoteTreeNode node, AbstractNode variable)
 		{
-			string idPrefix;
-			if (treeNode == RootNode) {
-				idPrefix = String.Empty;
+			// Get full name of the node
+			string fullNamePrefix;
+			if (node != RootNode && node.Parent != RootNode) {
+				fullNamePrefix = (string)node.Parent.GetValue(ColumnFullName) + ".";
 			} else {
-				idPrefix = (string)treeNode.GetValue(ColumnFullName) + ".";
+				fullNamePrefix = String.Empty;
 			}
-			string id = idPrefix + node.Name;
+			string fullName = fullNamePrefix + variable.Name;
 			
-			RemoteTreeNode newNode = treeNode.AppendNode();
-			newNode.Tag = node;
-			newNode.SetValue(ColumnFullName, id);
-			newNode.SetValue(ColumnImage, node.Image);
-			newNode.SetValue(ColumnName, node.Name);
-			newNode.SetValue(ColumnValue, node.Value);
-			newNode.SetValue(ColumnType, node.Type);
+			// Update the node itself
+			node.Tag = variable;
+			node.SetValue(ColumnFullName, fullName);
+			node.SetValue(ColumnImage, variable.Image);
+			node.SetValue(ColumnName, variable.Name);
+			node.SetValue(ColumnValue, variable.Value);
+			node.SetValue(ColumnType, variable.Type);
 			
-			// Placeholder so that the item is expandable
-			if (node.HasChildNodes) {
-				newNode.AppendNode();
+			// Recursively update the childs of this node
+			if (!variable.HasChildNodes) {
+				// Does not have childs
+				node.Clear();
+			} if (!updateChilds) {
+				// Has childs but do not update them
+				if (node.ChildCount == 0) {
+					// Placeholder so that the item is expandable
+					node.AppendNode();
+				}
+			} else {
+				// Update childs
+				AbstractNode[] variables;
+				try {
+					variables = ((AbstractNode)node.Tag).ChildNodes;
+				} catch {
+					variables = new AbstractNode[] { new ErrorNode(String.Empty, "Can not get child nodes") };
+				}
+				
+				// Iterate over the current tree nodes and update them
+				// Try to do it with minimal number of changes to the tree
+				for(int i = 0; i < node.ChildCount; i++) {
+					RemoteTreeNode childNode = node.GetChild(i);
+					string childNodeName = (string)childNode.GetValue(ColumnName);
+					
+					// Update 'i'th node to 'i'th variable
+					
+					// Find a variable with the same name as this node
+					// (includes the case where there are no variables left)
+					int varIndex = -1;
+					for(int j = i; j < variables.Length; j++) {
+						if (variables[j].Name == childNodeName) {
+							varIndex = j;
+							break;
+						}
+					}
+					
+					// Not found - remove this node
+					if (varIndex == -1) {
+						childNode.Remove();
+						continue;
+					}
+					
+					// Insert the variables before the match
+					for(int j = i; j < varIndex; j++) {
+						RemoteTreeNode newNode = childNode.InsertNode(j);
+						UpdateNodeRecursive(newNode, variables[j]);
+					}
+					
+					// Update the match
+					UpdateNodeRecursive(childNode, variables[varIndex]);
+				}
+				
+				// Add any variables left over
+				for(int i = node.ChildCount; i < variables.Length; i++) {
+					RemoteTreeNode newNode = node.AppendNode();
+					UpdateNodeRecursive(newNode, variables[i]);
+				}
 			}
-		}
-		
-		/// <summary> Expands the given node </summary>
-		/// <returns> Number of child nodes </returns>
-		public int ExpandNode(RemoteTreePath path)
-		{
-			RemoteTreeNode treeNode = GetNode(path);
-			
-			// Remove all current children
-			treeNode.Clear();
-			
-			// Add childs
-			AbstractNode[] childs;
-			try {
-				childs = ((AbstractNode)treeNode.Tag).ChildNodes;
-			} catch {
-				AppendNode(treeNode, new ErrorNode(String.Empty, "Can not get child nodes"));
-				return 1;
-			}
-			foreach(AbstractNode child in childs) {
-				AppendNode(treeNode, child);
-			}
-			return childs.Length;
 		}
 	}
 }
