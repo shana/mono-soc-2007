3d200aa5fdf9439f2c95d7432866726023b2e224
dsrbecky <dsrbecky@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-30 15:27:36 +0000

    Added a RemoteTreeStore - a tree model to be used in debugger process

---
+++ david/mdb-gui/RemoteTreeStore/RemoteTreeNode.cs
@@ -0,0 +1,164 @@
+using System;
+using System.Collections;
+
+namespace Mono.Debugger.Frontend
+{
+	/// <summary>
+	/// A node of RemoteTreeStore
+	/// </summary>
+	public class RemoteTreeNode
+	{
+		RemoteTreeStore remoteTreeStore;
+		RemoteTreeNode parent;
+		object[] values;
+		ArrayList childs = new ArrayList();
+		
+		public RemoteTreeStore RemoteTreeStore {
+			get { return remoteTreeStore; }
+		}
+		
+		public RemoteTreeNode Parent {
+			get { return parent; }
+		}
+		
+		public object[] Values {
+			get { return values; }
+		}
+		
+		public int ChildCount {
+			get { return childs.Count; }
+		}
+		
+		int myIndexHint = 0;
+		
+		/// <summary> Zero based index of this node in the parent </summary>
+		public int MyIndex {
+			get {
+				if (parent == null) {
+					// This is the root node
+					throw new Exception("Root node does not have an index");
+				} else {
+					if (Parent.GetChild(myIndexHint) == this) {
+						return myIndexHint;
+					} else {
+						myIndexHint = Parent.childs.IndexOf(this);
+						if (myIndexHint == -1) {
+							throw new Exception("Inconsistent data.  Child not found.");
+						}
+						return myIndexHint;
+					}
+				}
+			}
+		}
+		
+		public RemoteTreePath Path {
+			get {
+				if (parent == null) {
+					// This is the root node
+					return RemoteTreePath.Empty;
+				} else {
+					return Parent.Path.AppendIndex(MyIndex);
+				}
+			}
+		}
+		
+		public RemoteTreeNode(RemoteTreeStore remoteTreeStore, RemoteTreeNode parent)
+		{
+			this.remoteTreeStore = remoteTreeStore;
+			this.parent = parent;
+			this.values = new object[remoteTreeStore.ColumnTypes.Length];
+		}
+		
+		/// <summary> Insert a new node so that it is located at the given index </summary>
+		public void InsertNode(int index)
+		{
+			remoteTreeStore.AddModification(new RemoteTreeModification.InsertNode(this.Path, index));
+			
+			childs.Insert(index, new RemoteTreeNode(remoteTreeStore, this));
+		}
+		
+		/// <summary> Remove this node </summary>
+		public void Remove()
+		{
+			remoteTreeStore.AddModification(new RemoteTreeModification.RemoveNode(this.Path));
+			
+			parent.childs.RemoveAt(MyIndex);
+		}
+		
+		/// <summary> Set one of the values in this node </summary>
+		public void SetValue(int columnIndex, object value)
+		{
+			// Do the change only if the value is different
+			if (!value.Equals(GetValue(columnIndex))) {
+				remoteTreeStore.AddModification(new RemoteTreeModification.UpdateNode(this.Path, columnIndex, value));
+				
+				values[columnIndex] = value;
+			}
+		}
+		
+		// Convenience methods:
+		
+		/// <summary> Get a child with given index </summary>
+		public RemoteTreeNode GetChild(int index)
+		{
+			return (RemoteTreeNode)childs[index];
+		}
+		
+		/// <summary> Get a value in the given column </summary>
+		public object GetValue(int columnIndex)
+		{
+			return this.Values[columnIndex];
+		}
+		
+		/// <summary> Insert a node and set its values </summary>
+		public void InsertNode(int index, params object[] values)
+		{
+			InsertNode(index);
+			GetChild(index).SetValues(values);
+		}
+		
+		/// <summary> Add a node at the end </summary>
+		public void AppendNode()
+		{
+			InsertNode(childs.Count);
+		}
+		
+		/// <summary> Add a node at the end and set its values </summary>
+		public void AppendNode(params object[] values)
+		{
+			InsertNode(childs.Count, values);
+		}
+		
+		/// <summary> Add a node at the start </summary>
+		public void PrependNode()
+		{
+			InsertNode(0);
+		}
+		
+		/// <summary> Add a node at the start and set its values </summary>
+		public void PrependNode(params object[] values)
+		{
+			InsertNode(0, values);
+		}
+		
+		/// <summary> Set all values of this node </summary>
+		public void SetValues(params object[] newValues)
+		{
+			if (newValues.Length != this.Values.Length) {
+				throw new ArgumentException(String.Format("Incorrect number of values, {0} seen, {1} expected.", newValues.Length, this.Values.Length));
+			}
+			
+			for(int i = 0; i < this.Values.Length; i++) {
+				SetValue(i, newValues[i]);
+			}
+		}
+		
+		/// <summary> Remove all child nodes </summary>
+		public void Clear()
+		{
+			while (ChildCount > 0) {
+				GetChild(0).Remove();
+			}
+		}
+	}
+}
---
+++ david/mdb-gui/RemoteTreeStore/RemoteTreeStore.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+
+namespace Mono.Debugger.Frontend
+{
+	/// <summary>
+	/// A tree store designed to be mantained in one process and shown in other.
+	/// </summary>
+	public class RemoteTreeStore
+	{
+		Type[] columnTypes;
+		RemoteTreeNode rootNode;
+		ArrayList modifications = new ArrayList();
+		
+		public Type[] ColumnTypes {
+			get { return columnTypes; }
+		}
+		
+		public RemoteTreeNode RootNode {
+			get { return rootNode; }
+		}
+		
+		public RemoteTreeStore(params Type[] columnTypes)
+		{
+			this.columnTypes = columnTypes;
+			this.rootNode = new RemoteTreeNode(this, null);
+		}
+		
+		public RemoteTreeNode GetNode(RemoteTreePath path)
+		{
+			RemoteTreeNode current = RootNode;
+			foreach(int index in path.Indices) {
+				current = current.GetChild(index);
+			}
+			return current;
+		}
+		
+		internal void AddModification(RemoteTreeModification modification)
+		{
+			modifications.Add(modification);
+		}
+		
+		/// <summary>
+		/// Get a list of modifications done to the tree
+		/// </summary>
+		public RemoteTreeModification[] GetModifications()
+		{
+			return (RemoteTreeModification[])modifications.ToArray(typeof(RemoteTreeModification));
+		}
+		
+		/// <summary>
+		/// Clear the list of modifications.  This does not change the tree.
+		/// </summary>
+		public void ClearModifications()
+		{
+			modifications.Clear();
+		}
+	}
+}
