7f8fe9601d27bfd084014d2a2f128e211e2395a6
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-07-31 21:27:04 +0000

    2007-08-01  George Giolfan  <georgegiolfan@yahoo.com>
    	* luna.normalcolor.xaml: Added the ContentControl style.
    	* ContentControl.cs: Added.
    	* ContentControlTest.cs: Added.

---
+++ george/Source/PresentationFramework/System.Windows.Controls/ContentControl.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Markup;
+#if Implementation
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Mono.System.Windows.Controls {
+#else
+namespace System.Windows.Controls {
+#endif
+	[Localizability(LocalizationCategory.None, Readability=Readability.Unreadable)] 
+	[ContentProperty("Content")] 
+	public class ContentControl : Control, IAddChild {
+		#region Public Fields
+		#region Dependency Properties
+		public static readonly DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(object), typeof(ContentControl), new FrameworkPropertyMetadata(null, delegate(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			ContentControl i = (ContentControl)d;
+			i.should_serialize_content = true;
+			i.OnContentChanged(e.OldValue, e.NewValue);
+		}));
+		public static readonly DependencyProperty ContentTemplateProperty = DependencyProperty.Register("ContentTemplate", typeof(DataTemplate), typeof(ContentControl), new FrameworkPropertyMetadata(null, delegate(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			((ContentControl)d).OnContentTemplateChanged((DataTemplate)e.OldValue, (DataTemplate)e.NewValue);
+		}));
+		public static readonly DependencyProperty ContentTemplateSelectorProperty = DependencyProperty.Register("ContentTemplateSelector", typeof(DataTemplateSelector), typeof(ContentControl), new FrameworkPropertyMetadata(null, delegate(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+			((ContentControl)d).OnContentTemplateSelectorChanged((DataTemplateSelector)e.OldValue, (DataTemplateSelector)e.NewValue);
+		}));
+		static readonly DependencyPropertyKey HasContentPropertyKey = DependencyProperty.RegisterReadOnly("HasContent", typeof(bool), typeof(ContentControl), new PropertyMetadata());
+		public static readonly DependencyProperty HasContentProperty = HasContentPropertyKey.DependencyProperty;
+		#endregion
+		#endregion
+
+		#region Private Fields
+		bool should_serialize_content;
+		#endregion
+
+		#region Static Constructor
+		static ContentControl() {
+#if Implementation
+			Theme.Load();
+#endif
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ContentControl), new FrameworkPropertyMetadata(typeof(ContentControl)));
+		}
+		#endregion
+
+		#region Public Constructors
+		public ContentControl() {
+		}
+		#endregion
+
+		#region Public Properties
+		#region Dependency Properties
+		[Bindable(true)]
+		public object Content {
+			get { return GetValue(ContentProperty); }
+			set {
+				//HACK: It should not probably be done this way.
+				//FIXME: It does not solve the problem if SetValue is called directly.
+				should_serialize_content = true;
+				SetValue(ContentProperty, value); 
+			}
+		}
+
+		[Bindable(true)]
+		public DataTemplate ContentTemplate {
+			get { return (DataTemplate)GetValue(ContentTemplateProperty); }
+			set { SetValue(ContentTemplateProperty, value); }
+		}
+
+		[Bindable(true)]
+		public DataTemplateSelector ContentTemplateSelector {
+			get { return (DataTemplateSelector)GetValue(ContentTemplateSelectorProperty); }
+			set { SetValue(ContentTemplateSelectorProperty, value); }
+		}
+
+		public bool HasContent {
+			get { return (bool)GetValue(HasContentProperty); }
+			private set { SetValue(HasContentPropertyKey, value); }
+		}
+		#endregion
+		#endregion
+
+		#region Protected Properties
+		protected override IEnumerator LogicalChildren {
+			get {
+				object content = Content;
+				return (content == null ? new object[] { } : new object[] { content }).GetEnumerator();
+			}
+		}
+		#endregion
+
+		#region Public Methods
+		public virtual bool ShouldSerializeContent() {
+			return should_serialize_content;
+		}
+		#endregion
+
+		#region Protected Methods
+		protected virtual void AddChild(object value) {
+			CheckContent();
+			Content = value;
+		}
+
+		protected virtual void AddText(string text) {
+			CheckContent();
+			Content = text;
+		}
+
+		protected virtual void OnContentChanged(object oldContent, object newContent) {
+		}
+
+		protected virtual void OnContentTemplateChanged(DataTemplate oldContentTemplate, DataTemplate newContentTemplate) {
+		}
+
+		protected virtual void OnContentTemplateSelectorChanged(DataTemplateSelector oldContentTemplateSelector, DataTemplateSelector newContentTemplateSelector) {
+		}
+		#endregion
+		
+		#region Explicit Interface Implementations
+		#region IAddChild
+		void IAddChild.AddChild(object value) {
+			AddChild(value);
+		}
+
+		void IAddChild.AddText(string text) {
+			AddText(text);
+		}
+		#endregion
+		#endregion
+
+		#region Private Methods
+		void CheckContent() {
+			if (Content != null)
+				throw new InvalidOperationException("Content of a ContentControl must be a single element.");
+		}
+		#endregion
+	}
+}
---
+++ george/Source/PresentationFramework/Test/System.Windows.Controls/ContentControlTest.cs
@@ -0,0 +1,145 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Windows.Markup;
+#if Implementation
+using System;
+using System.Windows;
+using System.Windows.Controls;
+namespace Mono.System.Windows.Controls {
+#else
+namespace System.Windows.Controls {
+#endif
+	[TestFixture]
+	public class ContentControlTest {
+		#region LogicalChildren
+		[Test]
+		public void LogicalChildren() {
+			new LogicalChildrenContentControl();
+		}
+
+		class LogicalChildrenContentControl : ContentControl {
+			public LogicalChildrenContentControl() {
+				IEnumerator c = LogicalChildren;
+				//LAMESPEC
+				Assert.IsNotNull(c, "1");
+				Assert.IsFalse(c.MoveNext(), "2");
+				Content = "1";
+				c = LogicalChildren;
+				Assert.IsTrue(c.MoveNext(), "3");
+				Assert.AreEqual(c.Current, "1", "4");
+				Assert.IsFalse(c.MoveNext(), "5");
+				Content = "2";
+				c = LogicalChildren;
+				Assert.IsTrue(c.MoveNext(), "6");
+				Assert.AreEqual(c.Current, "2", "7");
+				Assert.IsFalse(c.MoveNext(), "8");
+				Content = null;
+				c = LogicalChildren;
+				Assert.IsFalse(c.MoveNext(), "9");
+			}
+		}
+		#endregion
+
+		#region IAddChildMembersCallTheOtherMembers
+		[Test]
+		public void IAddChildMembersCallTheOtherMembers() {
+			new IAddChildMembersCallTheOtherMembersContentControl();
+		}
+
+		class IAddChildMembersCallTheOtherMembersContentControl : ContentControl {
+			int add_child_calls;
+			int add_text_calls;
+
+			public IAddChildMembersCallTheOtherMembersContentControl() {
+				IAddChild add_child = (IAddChild)this;
+				Assert.AreEqual(add_child_calls, 0, "1");
+				add_child.AddChild(1);
+				Assert.AreEqual(add_child_calls, 1, "2");
+				Content = null;
+				Assert.AreEqual(add_text_calls, 0, "3");
+				add_child.AddText("1");
+				Assert.AreEqual(add_text_calls, 1, "4");
+			}
+
+			protected override void AddChild(object value) {
+				add_child_calls++;
+				base.AddChild(value);
+			}
+
+			protected override void AddText(string text) {
+				add_text_calls++;
+				base.AddText(text);
+			}
+		}
+		#endregion
+
+		[Test]
+		public void ShouldSerializeContent() {
+			ContentControl c = new ContentControl();
+			Assert.IsFalse(c.ShouldSerializeContent(), "1");
+			Assert.IsFalse(c.ShouldSerializeContent(), "1 1");
+			c.Content = null;
+			Assert.IsTrue(c.ShouldSerializeContent(), "2");
+			c.Content = "";
+			Assert.IsTrue(c.ShouldSerializeContent(), "3");
+			c.Content = null;
+			Assert.IsTrue(c.ShouldSerializeContent(), "4");
+			c.Content = 1;
+			Assert.IsTrue(c.ShouldSerializeContent(), "5");
+		}
+
+		[Test]
+		public void ShouldSerializeContent2() {
+			ContentControl c = new ContentControl();
+			Assert.IsFalse(c.ShouldSerializeContent(), "1");
+			Assert.IsFalse(c.ShouldSerializeContent(), "1 1");
+			c.SetValue(ContentControl.ContentProperty, null);
+			Assert.IsTrue(c.ShouldSerializeContent(), "2");
+			c.SetValue(ContentControl.ContentProperty, "");
+			Assert.IsTrue(c.ShouldSerializeContent(), "3");
+			c.SetValue(ContentControl.ContentProperty, null);
+			Assert.IsTrue(c.ShouldSerializeContent(), "4");
+			c.SetValue(ContentControl.ContentProperty, 1);
+			Assert.IsTrue(c.ShouldSerializeContent(), "5");
+		}
+
+		[Test]
+		public void ContentProperty() {
+			Assert.IsNull(ContentControl.ContentProperty.ValidateValueCallback, "1");
+			PropertyMetadata metadata = ContentControl.ContentProperty.GetMetadata(typeof(ContentControl));
+			Assert.IsNull(metadata.CoerceValueCallback, "2");
+			Assert.IsNotNull(metadata.PropertyChangedCallback, "3");
+		}
+
+		#region AddChildAddText
+		[Test]
+		public void AddChildAddText() {
+			new AddChildAddTextContentControl();
+		}
+
+		class AddChildAddTextContentControl : ContentControl {
+			public AddChildAddTextContentControl() {
+				Assert.IsNull(Content, "1");
+				AddChild(1);
+				Assert.AreEqual(Content, 1, "2");
+				Content = null;
+				Assert.IsNull(Content, "3");
+				AddText("1");
+				Assert.AreEqual(Content, "1", "4");
+				try {
+					AddChild(2);
+					Assert.Fail("5");
+				} catch (InvalidOperationException ex) {
+					Assert.AreEqual(ex.Message, "Content of a ContentControl must be a single element.", "6");
+				}
+				try {
+					AddText("2");
+					Assert.Fail("7");
+				} catch (InvalidOperationException ex) {
+					Assert.AreEqual(ex.Message, "Content of a ContentControl must be a single element.", "8");
+				}
+			}
+		}
+		#endregion
+	}
+}
