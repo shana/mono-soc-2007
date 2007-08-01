d5ddfa5e3d94abebd239082262c2824d2d5bdf4f
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-01 13:23:03 +0000

    added some scripts to make installation easier

---
+++ ben/patch.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/configure.patch > temp.patch
+patch -p0 -i temp.patch
+rm -f temp.patch
+
+cd Extras/
+
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/makefile.patch > temp.patch
+patch -p0 -i temp.patch
+rm -f temp.patch
+
+svn co https://mono-soc-2007.googlecode.com/svn/trunk/ben/MonoDevelop.Database MonoDevelop.Database
+cd ..
+
+cd contrib
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/MySql.Data.dll > MySql.Data.dll
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/FirebirdSql.Data.Firebird.dll > FirebirdSql.Data.Firebird.dll
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/FirebirdSql.Data.Firebird.license.txt > FirebirdSql.Data.Firebird.license.txt
+
+cd ..
+
+echo "patched!"
+echo "use --enable-database when running configure"
