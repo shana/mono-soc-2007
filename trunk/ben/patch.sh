7828438776f4d56b8a81cf3029e326b73a3ffaea
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-01 13:46:09 +0000

    added patch for the .mds solution

--- trunk/ben/patch.sh
+++ trunk/ben/patch.sh
@@ -8,6 +8,9 @@ cd Extras/
 
 svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/makefile.patch > temp.patch
 patch -p0 -i temp.patch
+
+svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/solution.patch > temp.patch
+patch -p0 -i temp.patch
 rm -f temp.patch
 
 svn co https://mono-soc-2007.googlecode.com/svn/trunk/ben/MonoDevelop.Database MonoDevelop.Database
