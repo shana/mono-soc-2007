ff0db310e78f6f2e8eedc81f827a19e81c45faa9
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-01 15:25:19 +0000

    copy the .dll files in the contrib dir is no longer needed

--- trunk/ben/patch.sh
+++ trunk/ben/patch.sh
@@ -16,13 +16,5 @@ rm -f temp.patch
 svn co https://mono-soc-2007.googlecode.com/svn/trunk/ben/MonoDevelop.Database MonoDevelop.Database
 cd ..
 
-cd contrib
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/MySql.Data.dll > MySql.Data.dll
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/Mono.Data.Sqlite.dll > Mono.Data.Sqlite.dll
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/FirebirdSql.Data.Firebird.dll > FirebirdSql.Data.Firebird.dll
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/contrib/FirebirdSql.Data.Firebird.license.txt > FirebirdSql.Data.Firebird.license.txt
-
-cd ..
-
 echo "patched!"
 echo "use --enable-database when running configure"
