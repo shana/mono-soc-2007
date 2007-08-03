95a9da7bfb614557154298b5db6c911cb8eece16
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-03 18:51:03 +0000

    2007-08-03  George Giolfan  <georgegiolfan@yahoo.com>
    	* BorderGapMaskConverter.cs: Implemented.
    	* BorderGapMaskConverterTest.cs: Added some tests.

--- george/Source/PresentationFramework/System.Windows.Controls/BorderGapMaskConverter.cs
+++ george/Source/PresentationFramework/System.Windows.Controls/BorderGapMaskConverter.cs
@@ -1,9 +1,11 @@
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Shapes;
 #if Implementation
 using System;
-using System.Windows;namespace Mono.System.Windows.Controls {
+using System.Windows;
+namespace Mono.System.Windows.Controls {
 #else
 namespace System.Windows.Controls {
 #endif
@@ -19,9 +21,44 @@ namespace System.Windows.Controls {
 				double header_width = (double)values[0];
 				double width = (double)values[1];
 				double height = (double)values[2];
-				double visible_line_width = (double)parameter;
-				VisualBrush result = new VisualBrush();
-				return result;
+				double visible_line_width;
+				if (parameter is double)
+					visible_line_width = (double)parameter;
+				else {
+					string parameter_string = parameter as string;
+					if (parameter == null)
+						return DependencyProperty.UnsetValue;
+					visible_line_width = double.Parse(parameter_string);
+				}
+				Grid grid = new Grid();
+				grid.Height = height;
+				grid.Width = width;
+				ColumnDefinition column_definition = new ColumnDefinition();
+				column_definition.Width = new GridLength(visible_line_width);
+				grid.ColumnDefinitions.Add(column_definition);
+				column_definition = new ColumnDefinition();
+				column_definition.Width = new GridLength(header_width);
+				grid.ColumnDefinitions.Add(column_definition);
+				grid.ColumnDefinitions.Add(new ColumnDefinition());
+				RowDefinition row_definition = new RowDefinition();
+				row_definition.Height = new GridLength(width);
+				grid.RowDefinitions.Add(row_definition);
+				grid.RowDefinitions.Add(new RowDefinition());
+				Rectangle rectangle = new Rectangle();
+				rectangle.Fill = Brushes.Black;
+				Grid.SetRowSpan(rectangle, 2);
+				grid.Children.Add(rectangle);
+				rectangle = new Rectangle();
+				rectangle.Fill = Brushes.Black;
+				Grid.SetColumn(rectangle, 1);
+				Grid.SetRow(rectangle, 1);
+				grid.Children.Add(rectangle);
+				rectangle = new Rectangle();
+				rectangle.Fill = Brushes.Black;
+				Grid.SetColumn(rectangle, 2);
+				Grid.SetRowSpan(rectangle, 2);
+				grid.Children.Add(rectangle);
+				return new VisualBrush(grid);
 			} catch {
 				return DependencyProperty.UnsetValue;
 			}
--- george/Source/PresentationFramework/Test/System.Windows.Controls/BorderGapMaskConverterTest.cs
+++ george/Source/PresentationFramework/Test/System.Windows.Controls/BorderGapMaskConverterTest.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
+using System.Windows.Markup;
 using System.Windows.Media;
+using System.Windows.Shapes;
 #if Implementation
+using System.Windows;
 namespace Mono.System.Windows.Controls {
 #else
 namespace System.Windows.Controls {
@@ -11,9 +14,57 @@ namespace System.Windows.Controls {
 		public void Test() {
 			Assert.AreEqual(new BorderGapMaskConverter().Convert(new object[] { 100, 500, 1000 }, null, 7, null), DependencyProperty.UnsetValue, "1");
 			Assert.AreEqual(new BorderGapMaskConverter().Convert(new object[] { 100D, 500D, 1000D }, null, 7, null), DependencyProperty.UnsetValue, "2");
-			VisualBrush result = (VisualBrush) new BorderGapMaskConverter().Convert(new object[] { 100D, 500D, 1000D }, null, 7D, null);
+			VisualBrush result = (VisualBrush)new BorderGapMaskConverter().Convert(new object[] { 100D, 500D, 1000D }, null, 7D, null);
 			Grid grid = (Grid)result.Visual;
 			Assert.AreEqual(grid.Children.Count, 3, "3");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[0]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "4");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[0]), 0, "5");
+			Assert.AreEqual(Grid.GetRow(grid.Children[0]), 0, "6");
+			Assert.AreEqual(Grid.GetRowSpan(grid.Children[0]), 2, "7");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[1]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "8");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[1]), 1, "9");
+			Assert.AreEqual(Grid.GetRow(grid.Children[1]), 1, "10");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[2]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "11");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[2]), 2, "12");
+			Assert.AreEqual(Grid.GetRow(grid.Children[0]), 0, "13");
+			Assert.AreEqual(Grid.GetRowSpan(grid.Children[2]), 2, "14");
+			Assert.AreEqual(grid.Height, 1000, "15");
+			Assert.AreEqual(grid.Width, 500, "16");
+			Assert.AreEqual(grid.ColumnDefinitions.Count, 3, "17");
+			Assert.AreEqual(grid.ColumnDefinitions[0].Width.Value, 7, "18");
+			Assert.AreEqual(grid.ColumnDefinitions[1].Width.Value, 100, "19");
+			Assert.IsTrue(grid.ColumnDefinitions[2].Width.IsStar, "20");
+			Assert.AreEqual(grid.RowDefinitions.Count, 2, "21");
+			Assert.AreEqual(grid.RowDefinitions[0].Height.Value, 500, "22");
+			Assert.IsTrue(grid.RowDefinitions[1].Height.IsStar, "23");
+		}
+
+		[Test]
+		public void Test2() {
+			Assert.AreEqual(new BorderGapMaskConverter().Convert(new object[] { 100, 500, 1000 }, null, "7", null), DependencyProperty.UnsetValue, "1");
+			VisualBrush result = (VisualBrush)new BorderGapMaskConverter().Convert(new object[] { 100D, 500D, 1000D }, null, "7", null);
+			Grid grid = (Grid)result.Visual;
+			Assert.AreEqual(grid.Children.Count, 3, "3");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[0]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "4");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[0]), 0, "5");
+			Assert.AreEqual(Grid.GetRow(grid.Children[0]), 0, "6");
+			Assert.AreEqual(Grid.GetRowSpan(grid.Children[0]), 2, "7");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[1]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "8");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[1]), 1, "9");
+			Assert.AreEqual(Grid.GetRow(grid.Children[1]), 1, "10");
+			Assert.AreEqual(((SolidColorBrush)((Rectangle)grid.Children[2]).Fill).Color, Color.FromArgb(0xFF, 0x00, 0x00, 0x00), "11");
+			Assert.AreEqual(Grid.GetColumn(grid.Children[2]), 2, "12");
+			Assert.AreEqual(Grid.GetRow(grid.Children[0]), 0, "13");
+			Assert.AreEqual(Grid.GetRowSpan(grid.Children[2]), 2, "14");
+			Assert.AreEqual(grid.Height, 1000, "15");
+			Assert.AreEqual(grid.Width, 500, "16");
+			Assert.AreEqual(grid.ColumnDefinitions.Count, 3, "17");
+			Assert.AreEqual(grid.ColumnDefinitions[0].Width.Value, 7, "18");
+			Assert.AreEqual(grid.ColumnDefinitions[1].Width.Value, 100, "19");
+			Assert.IsTrue(grid.ColumnDefinitions[2].Width.IsStar, "20");
+			Assert.AreEqual(grid.RowDefinitions.Count, 2, "21");
+			Assert.AreEqual(grid.RowDefinitions[0].Height.Value, 500, "22");
+			Assert.IsTrue(grid.RowDefinitions[1].Height.IsStar, "23");
 		}
 	}
 }
