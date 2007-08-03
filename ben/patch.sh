f416d254efd193564bb8a0318f734f811bd49952
ben.motmans <ben.motmans@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-03 12:32:39 +0000

    fixed urls in patch

--- ben/patch.sh
+++ ben/patch.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/configure.patch > temp.patch
+svn cat http://mono-soc-2007.googlecode.com/svn/trunk/ben/configure.patch > temp.patch
 patch -p0 -i temp.patch
 rm -f temp.patch
 
 cd Extras/
 
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/makefile.patch > temp.patch
+svn cat http://mono-soc-2007.googlecode.com/svn/trunk/ben/makefile.patch > temp.patch
 patch -p0 -i temp.patch
 
-svn cat https://mono-soc-2007.googlecode.com/svn/trunk/ben/solution.patch > temp.patch
+svn cat http://mono-soc-2007.googlecode.com/svn/trunk/ben/solution.patch > temp.patch
 patch -p0 -i temp.patch
 rm -f temp.patch
 
-svn co https://mono-soc-2007.googlecode.com/svn/trunk/ben/MonoDevelop.Database MonoDevelop.Database
+svn co http://mono-soc-2007.googlecode.com/svn/trunk/ben/MonoDevelop.Database MonoDevelop.Database
 cd ..
 
 echo "patched!"
