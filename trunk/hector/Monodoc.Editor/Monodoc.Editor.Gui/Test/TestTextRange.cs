598c892f547ecedfa307aebcd535499a08ae6461
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-10 18:22:08 +0000

    * Test/TestDocumentBufferArchiver.cs: Minor name changes of tests. * Test/TestTextRange.cs: Added several test units for TextRange helper   class. * DocumentBuffer.cs: Added some small change for default handler of   DeleteRange event.

--- trunk/hector/Monodoc.Editor/Monodoc.Editor.Gui/Test/TestTextRange.cs
+++ trunk/hector/Monodoc.Editor/Monodoc.Editor.Gui/Test/TestTextRange.cs
@@ -14,9 +14,77 @@ namespace Monodoc.Editor.Gui {
 [TestFixture()]
 public class TestTextRange {
 
-	[Test()]
-	public void TestCase()
+	[SetUp()]
+	public void Initialize ()
 	{
+		Application.Init ();
+	}
+	
+	[TearDown()]
+	public void Dispose ()
+	{
+		Application.Quit ();
+	}
+	
+	[Test()]
+	public void TestText ()
+	{
+		DocumentEditor editor = new DocumentEditor ();
+		TextBuffer buffer = editor.Buffer;
+		
+		TextTag summaryTag = buffer.TagTable.Lookup ("summary");
+		TextTag textTag = buffer.TagTable.Lookup ("summary:Text");
+		
+		TextIter insertAt = buffer.StartIter;
+		buffer.InsertWithTags (ref insertAt, "Test: ", summaryTag);
+		
+		int startIndex = insertAt.Offset;
+		buffer.InsertWithTags (ref insertAt, "Testing TextRange", textTag);
+
+		TextRange textRange = new TextRange (buffer.GetIterAtOffset (startIndex), insertAt);
+		Assert.AreEqual ("Testing TextRange", textRange.Text, "TRT");
+	}
+	
+	[Test()]
+	public void TestErase ()
+	{
+		DocumentEditor editor = new DocumentEditor ();
+		TextBuffer buffer = editor.Buffer;
+		
+		TextTag summaryTag = buffer.TagTable.Lookup ("summary");
+		TextTag textTag = buffer.TagTable.Lookup ("summary:Text");
+		
+		TextIter insertAt = buffer.StartIter;
+		buffer.InsertWithTags (ref insertAt, "Test: ", summaryTag);
+		
+		int startIndex = insertAt.Offset;
+		buffer.InsertWithTags (ref insertAt, "Testing TextRange", textTag);
+
+		TextRange textRange = new TextRange (buffer.GetIterAtOffset (startIndex), insertAt);
+		Assert.AreEqual ("Test: Testing TextRange", buffer.Text, "TRE01");
+		textRange.Erase ();
+		Assert.AreEqual ("Test: ", buffer.Text, "TRE", "TRE02");
+	}
+	
+	[Test()]
+	public void TestRemoveTag ()
+	{
+		DocumentEditor editor = new DocumentEditor ();
+		TextBuffer buffer = editor.Buffer;
+		
+		TextTag summaryTag = buffer.TagTable.Lookup ("summary");
+		TextTag textTag = buffer.TagTable.Lookup ("summary:Text");
+		
+		TextIter insertAt = buffer.StartIter;
+		buffer.InsertWithTags (ref insertAt, "Test: ", summaryTag);
+		
+		int startIndex = insertAt.Offset;
+		buffer.InsertWithTags (ref insertAt, "Testing TextRange", textTag);
+
+		TextRange textRange = new TextRange (buffer.GetIterAtOffset (startIndex), insertAt);
+		Assert.AreEqual (1, textRange.Start.Tags.Length, "TRRT01");
+		textRange.RemoveTag (textTag);
+		Assert.AreEqual (0, textRange.Start.Tags.Length, "TRRT02");
 	}
 }
 }
