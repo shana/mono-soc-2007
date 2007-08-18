c7154f11adddabcb17bffe504be09fbd2c1e3789
georgegiolfan <georgegiolfan@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-18 19:23:37 +0000

    2007-08-18  George Giolfan  <georgegiolfan@yahoo.com>
    	* AssemblyInfo.cs: Added some comments.

--- george/Source/PresentationFramework/Properties/AssemblyInfo.cs
+++ george/Source/PresentationFramework/Properties/AssemblyInfo.cs
@@ -3,4 +3,8 @@ using System.Windows;
 [assembly: AssemblyTitle("PresentationFramework")]
 [assembly: AssemblyFileVersion("3.0.0.0")]
 [assembly: AssemblyVersion("3.0.0.0")]
+// To change the way the controls look, enable one of the following lines.
+// The following line causes the modified Luna theme to be used. (It expect the Mono.PresentationFramework.Luna assembly to be available, generally in the same directory as this one.)
 [assembly: ThemeInfo(ResourceDictionaryLocation.ExternalAssembly, ResourceDictionaryLocation.SourceAssembly)]
+// The following line causes the simple internal theme to be used. (It only contains resources for some of the controls.)
+// [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
