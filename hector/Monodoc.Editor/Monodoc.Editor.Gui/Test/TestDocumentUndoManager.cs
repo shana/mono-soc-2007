76f841d8daf072845977ffb55a849fdfac9caeb5
hectoregm <hectoregm@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-23 19:13:14 +0000

    * Test/TestDocumentActions.cs, Test/TestDocumentUndoManager.cs,   Monodoc.Editor.Gui.mdp: Added stub files for test units for   DocumentAction and DocumentUndoManager classes.

---
+++ hector/Monodoc.Editor/Monodoc.Editor.Gui/Test/TestDocumentUndoManager.cs
@@ -0,0 +1,20 @@
+//
+// TestDocumentUndoManager: Test unit for DocumentUndoManager class.
+//
+// Author:
+//   Hector E. Gomez M (hectorgm@ciencias.unam.mx)
+//
+// (C) 2007 Hector E. Gomez M
+
+using System;
+using NUnit.Framework;
+
+namespace Monodoc.Editor.Gui {
+[TestFixture()]
+public class TestDocumentUndoManager {
+	[Test()]
+	public void TestCase()
+	{
+	}
+}
+}
