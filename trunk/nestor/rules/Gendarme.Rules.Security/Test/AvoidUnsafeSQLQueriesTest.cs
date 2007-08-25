e416fa7ebaf19c03a5696bc445503d91309b73f8
nestor.salceda <nestor.salceda@34f1089e-e42d-0410-9919-1f33f7df61a6>
2007-08-25 01:25:57 +0000

    Added Gendarme.Rules.Security directory.

---
+++ trunk/nestor/rules/Gendarme.Rules.Security/Test/AvoidUnsafeSQLQueriesTest.cs
@@ -0,0 +1,66 @@
+//
+// Unit Test for AvoidUnsafeSQLQueries Rule.
+//
+// Authors:
+//      Néstor Salceda <nestor.salceda@gmail.com>
+//
+//      (C) 2007 Néstor Salceda
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Reflection;
+
+using Mono.Cecil;
+using NUnit.Framework;
+using Gendarme.Framework;
+using Gendarme.Rules.Security;
+
+namespace Test.Rules.Security {
+
+	[TestFixture]
+	public class AvoidUnsafeSQLQueriesTest {
+		private IMethodRule rule;
+		private AssemblyDefinition assembly;
+		private MethodDefinition method;
+		private MessageCollection messageCollection;
+
+		[TestFixtureSetUp]
+		public void FixtureSetUp () 
+		{
+			string unit = Assembly.GetExecutingAssembly ().Location;
+			assembly = AssemblyFactory.GetAssembly (unit);
+			rule = new AvoidUnsafeSQLQueriesRule ();
+			messageCollection = null;
+		}
+
+		private MethodDefinition GetMethodForTest (string methodName) 
+		{
+			TypeDefinition type = assembly.MainModule.Types ["Test.Rules.Security.AvoidUnsafeSQLQueriesTest"];
+			foreach (MethodDefinition method in type.Methods) {
+				if (method.Name == methodName)
+					return method;
+			}
+			return null;
+		}
+
+	}
+}
